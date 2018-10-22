fc540746dae03f68aae6cc900447bbcb13dc73f9
Mats Bolstad <mbolstad@gmail.com>
2018-10-22 15:44:35 +0200

    Endepunkt for helsesjekken NAIS behøver.

---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/HealthcheckController.java
@@ -0,0 +1,23 @@
+package no.nav.tag.tiltaksgjennomforing;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthcheckController {
+
+    @RequestMapping(value = "/isAlive", method = RequestMethod.GET)
+    @ResponseBody
+    public String isAlive() {
+        return "ok";
+    }
+
+    @RequestMapping(value = "/isReady", method = RequestMethod.GET)
+    @ResponseBody
+    public String isReady() {
+        return "ok";
+    }
+
+}
