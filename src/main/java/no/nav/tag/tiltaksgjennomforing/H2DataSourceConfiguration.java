33088c1e83d5124c31fa362d94416ce51fd68ebc
Mats Bolstad <mbolstad@gmail.com>
2018-11-13 13:59:13 +0100

    Ny DataSource for Postgres-database. Henter forsøksvis secrets fra Vault.

--- src/main/java/no/nav/tag/tiltaksgjennomforing/H2DataSourceConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/H2DataSourceConfiguration.java
@@ -1,14 +1,16 @@
 package no.nav.tag.tiltaksgjennomforing;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class DataSourceConfiguration {
+@Profile("dev")
+public class H2DataSourceConfiguration {
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -16,15 +18,6 @@ public class DataSourceConfiguration {
         dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1;MODE=PostgreSQL");
         dataSource.setUsername("sa");
         dataSource.setPassword("sa");
-        migrer(dataSource);
         return dataSource;
-    }
-
-    private static void migrer(DataSource dataSource) {
-        Flyway
-                .configure()
-                .dataSource(dataSource)
-                .load()
-                .migrate();
     }
 }
---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/PostgresDataSourceConfiguration.java
@@ -0,0 +1,30 @@
+package no.nav.tag.tiltaksgjennomforing;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("!dev")
+public class PostgresDataSourceConfiguration {
+    @Value("db.url")
+    private String url;
+    @Value("db.username")
+    private String username;
+    @Value("db.password")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
