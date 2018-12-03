5352da2022bce652f448b22ab45848c13861c377
andrmos <mossige.a@gmail.com>
2018-12-03 16:39:42 +0100

    TAG-112: Implementer endepunkt for å oppdatere en avtale

--- src/main/java/no/nav/tag/tiltaksgjennomforing/AvtaleController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/AvtaleController.java
@@ -6,15 +6,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AvtaleController {
 
-    @Autowired
     private AvtaleRepository repository;
 
-    @RequestMapping("/avtaler/{id}")
+    @Autowired
+    public AvtaleController(AvtaleRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/avtaler/{id}")
     public Avtale hent(@PathVariable("id") Integer id) {
         return repository.findById(id).orElseThrow(ResourceNotFoundException::new);
     }
 
-    @RequestMapping("/avtaler")
+    @GetMapping("/avtaler")
     public Iterable<Avtale> hentAlle() {
         return repository.findAll();
     }
@@ -23,5 +27,13 @@ public class AvtaleController {
     public Integer opprett(@RequestBody Avtale avtale) {
         Avtale opprettetAvtale = repository.save(avtale);
         return opprettetAvtale.getId();
+    }
+
+    @PutMapping("/avtaler/{id}")
+    public void settAvtale(@PathVariable("id") Integer id, @RequestBody Avtale avtale) {
+        if (repository.existsById(id)) {
+            avtale.setId(id);
+        }
+        repository.save(avtale);
     }
 }
