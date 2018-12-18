ea931e9c87bd29a21265addb0fd960bb4b03d814
Mats Bolstad <mbolstad@gmail.com>
2018-12-18 16:36:04 +0100

    Lager enhetstest som reflekterer feilsituasjon.

--- src/test/java/no/nav/tag/tiltaksgjennomforing/AvtaleRepositoryTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/AvtaleRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -16,10 +18,11 @@ public class AvtaleRepositoryTest {
     private AvtaleRepository avtaleRepository;
 
     @Test
-    public void tomAvtaleSkalKunneLagresAvRepository() {
+    public void nyAvtaleSkalKunneLagreOgReturneresAvRepository() {
         Fnr deltakerFnr = new Fnr("12345678901");
-        Avtale avtale = Avtale.nyAvtale(deltakerFnr);
-        Avtale lagretAvtale = avtaleRepository.save(avtale);
-        assertThat(lagretAvtale).isNotNull();
+        Avtale lagretAvtale = avtaleRepository.save(Avtale.nyAvtale(deltakerFnr));
+
+        Optional<Avtale> avtaleOptional = avtaleRepository.findById(lagretAvtale.getId());
+        assertThat(avtaleOptional).isPresent();
     }
 }
