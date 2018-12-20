3dbb61d48899a0f330176cfa0aebb9a75b97c7f1
Mats Bolstad <mbolstad@gmail.com>
2018-12-20 10:42:40 +0100

    Kommenterte ut tokenvalidering ved en feil.

--- src/main/java/no/nav/tag/tiltaksgjennomforing/TiltaksgjennomforingApplication.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/TiltaksgjennomforingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableOIDCTokenValidation
+@EnableOIDCTokenValidation
 public class TiltaksgjennomforingApplication {
     public static void main(String[] args) {
         SpringApplication.run(TiltaksgjennomforingApplication.class, args);
