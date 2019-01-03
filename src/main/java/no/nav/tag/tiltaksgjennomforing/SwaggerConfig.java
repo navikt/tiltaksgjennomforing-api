ed7402ec53438238d3dafb44feb59de022744875
Lars Andreas Tveiten <lat@forskningsradet.no>
2019-01-03 15:45:10 +0100

    TAG-175 Bare bruk swagger lokalt

--- src/main/java/no/nav/tag/tiltaksgjennomforing/SwaggerConfig.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/SwaggerConfig.java
@@ -3,6 +3,7 @@ package no.nav.tag.tiltaksgjennomforing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -11,14 +12,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Profile("dev")
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build();
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
+
