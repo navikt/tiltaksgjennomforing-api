20d92b92c38ed11bdd9e1478b9a73e5e1b3a64f2
André Mossige <mossige.a@gmail.com>
2019-01-08 16:09:55 +0100

    TAG-203: Lag test som sjekker at man kan lagre mål på to avtaler etterhverandre

--- src/test/java/no/nav/tag/tiltaksgjennomforing/AvtaleRepositoryTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/AvtaleRepositoryTest.java
@@ -1,12 +1,15 @@
 package no.nav.tag.tiltaksgjennomforing;
 
 import no.nav.tag.tiltaksgjennomforing.domene.Avtale;
+import no.nav.tag.tiltaksgjennomforing.domene.EndreAvtale;
+import no.nav.tag.tiltaksgjennomforing.domene.Maal;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,5 +27,32 @@ public class AvtaleRepositoryTest {
 
         Optional<Avtale> avtaleOptional = avtaleRepository.findById(lagretAvtale.getId());
         assertThat(avtaleOptional).isPresent();
+    }
+
+    @Test
+    public void skalKunneLagreMaalFlereGanger() {
+        // Lage avtale
+        Avtale lagretAvtale = avtaleRepository.save(TestData.minimalAvtale());
+
+        // Lagre maal skal fungere
+        EndreAvtale endreAvtale = new EndreAvtale();
+        Maal maal = new Maal();
+        maal.setKategori("Kategori");
+        maal.setBeskrivelse("Beskrivelse");
+        endreAvtale.setMaal(List.of(maal));
+        lagretAvtale.endreAvtale(1, endreAvtale);
+        avtaleRepository.save(lagretAvtale);
+
+        // Lage ny avtale
+        Avtale lagretAvtale2 = avtaleRepository.save(TestData.minimalAvtale());
+
+        // Lagre maal skal enda fungere
+        EndreAvtale endreAvtale2 = new EndreAvtale();
+        Maal maal2 = new Maal();
+        maal2.setKategori("Kategori");
+        maal2.setBeskrivelse("Beskrivelse");
+        endreAvtale2.setMaal(List.of(maal2));
+        lagretAvtale2.endreAvtale(1, endreAvtale2);
+        avtaleRepository.save(lagretAvtale2);
     }
 }
