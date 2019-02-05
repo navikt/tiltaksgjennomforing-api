b5350ecd591de623f6c5c672778dd8026ff1329d
Mats Bolstad <mbolstad@gmail.com>
2019-02-05 19:25:58 +0100

    TAG-192: Egen Spring-profil 'heroku' for aktivering av falsk innlogging.

--- src/main/java/no/nav/tag/tiltaksgjennomforing/DevOidcConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/DevOidcConfiguration.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Import(TokenGeneratorConfiguration.class)
-@Profile("dev")
+@Profile({"dev", "heroku"})
 public class DevOidcConfiguration {
 }
