df28bf23427b213f190d1b9eef9377096017f023
Mats Bolstad <mbolstad@gmail.com>
2019-03-15 10:54:17 +0100

    Fikser feil i migreringsscript.

--- src/main/resources/db/migration/V2__expandInputFields.sql
+++ src/main/resources/db/migration/V2__expandInputFields.sql
@@ -1,5 +1,5 @@
-ALTER TABLE avtale ALTER COLUMN oppfolging varchar(1000);
-ALTER TABLE avtale ALTER COLUMN tilrettelegging varchar(1000);
-ALTER TABLE maal ALTER COLUMN beskrivelse varchar(1000);
-ALTER TABLE oppgave ALTER COLUMN beskrivelse varchar(1000);
-ALTER TABLE oppgave ALTER COLUMN opplaering varchar(1000);
+ALTER TABLE avtale ALTER COLUMN oppfolging TYPE varchar(1000);
+ALTER TABLE avtale ALTER COLUMN tilrettelegging TYPE varchar(1000);
+ALTER TABLE maal ALTER COLUMN beskrivelse TYPE varchar(1000);
+ALTER TABLE oppgave ALTER COLUMN beskrivelse TYPE varchar(1000);
+ALTER TABLE oppgave ALTER COLUMN opplaering TYPE varchar(1000);
