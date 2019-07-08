999c5198193573fa6a06696f2d24e72b84ba825a
Mats Bolstad <mbolstad@gmail.com>
2019-07-08 09:29:20 +0200

    Eliminerer advarsel ifm. equals/hashcode

--- src/main/java/no/nav/tag/tiltaksgjennomforing/domene/varsel/VarslbarHendelse.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/domene/varsel/VarslbarHendelse.java
@@ -1,6 +1,7 @@
 package no.nav.tag.tiltaksgjennomforing.domene.varsel;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import no.nav.tag.tiltaksgjennomforing.domene.Avtale;
 import no.nav.tag.tiltaksgjennomforing.domene.events.VarslbarHendelseOppstaatt;
 import org.springframework.data.annotation.Id;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class VarslbarHendelse extends AbstractAggregateRoot {
     @Id
     private UUID id;
