a9aa44d888c869d505db9b89f51268e5bf06acb4
Mats Bolstad <mbolstad@gmail.com>
2019-08-13 16:53:02 +0200

    Konverterer enum til string

--- src/main/java/no/nav/tag/tiltaksgjennomforing/integrasjon/kafka/SmsVarselResultatConsumer.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/integrasjon/kafka/SmsVarselResultatConsumer.java
@@ -19,7 +19,7 @@ public class SmsVarselResultatConsumer {
         // Bryter prinsippet om aggregate root her ved å gå direkte på sms_varsel og ikke bruke repository til varslbar_hendelse.
         // Grunnen til at sms_varsel oppdateres direkte er å unngå situasjon der resultat av to SMS-varslinger kommer inn fra Kafka samtidig,
         // og at den andre overskriver den første. Mulig at dette bør løses på annen måte.
-        int oppdaterteRader = jdbcTemplate.update("update sms_varsel set status = ? where id = ?", resultatMelding.getStatus(), resultatMelding.getSmsVarselId());
+        int oppdaterteRader = jdbcTemplate.update("update sms_varsel set status = ? where id = ?", resultatMelding.getStatus().toString(), resultatMelding.getSmsVarselId());
         if (oppdaterteRader == 1) {
             log.info("Oppdatert SmsVarsel med smsVarselId={} til status={}", resultatMelding.getSmsVarselId(), resultatMelding.getStatus());
         } else {
