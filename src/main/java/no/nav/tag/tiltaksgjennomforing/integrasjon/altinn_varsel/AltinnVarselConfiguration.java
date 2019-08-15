1ab05fb5b7b39910880274a921ee17b4b1502ba1
Mats Bolstad <mbolstad@gmail.com>
2019-08-15 10:47:46 +0200

    Konfigurerer STS litt annerledes

--- src/main/java/no/nav/tag/tiltaksgjennomforing/integrasjon/altinn_varsel/AltinnVarselConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/integrasjon/altinn_varsel/AltinnVarselConfiguration.java
@@ -5,24 +5,11 @@ import no.altinn.services.serviceengine.notification._2010._10.INotificationAgen
 import no.nav.tag.tiltaksgjennomforing.integrasjon.WsClient;
 import no.nav.tag.tiltaksgjennomforing.integrasjon.configurationProperties.AltinnVarselProperties;
 import no.nav.tag.tiltaksgjennomforing.integrasjon.configurationProperties.StsProperties;
-import org.apache.cxf.binding.soap.Soap12;
-import org.apache.cxf.binding.soap.SoapMessage;
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.feature.LoggingFeature;
-import org.apache.cxf.frontend.ClientProxy;
-import org.apache.cxf.ws.policy.PolicyBuilder;
-import org.apache.cxf.ws.policy.PolicyEngine;
-import org.apache.cxf.ws.policy.attachment.reference.RemoteReferenceResolver;
-import org.apache.cxf.ws.security.SecurityConstants;
-import org.apache.cxf.ws.security.trust.STSClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Map;
-
-import static org.apache.cxf.rt.security.SecurityConstants.CACHE_ISSUED_TOKEN_IN_ENDPOINT;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -34,36 +21,38 @@ public class AltinnVarselConfiguration {
 
     @Bean
     public INotificationAgencyExternalBasic iNotificationAgencyExternalBasic() {
-        var port = new WsClient<INotificationAgencyExternalBasic>().createPort(varselProperties.getUri().toString(), INotificationAgencyExternalBasic.class, Collections.singletonList(new LogErrorHandler()), true);
-        configureSTSFor(port);
+        var port = new WsClient<INotificationAgencyExternalBasic>().createPort(varselProperties.getUri().toString(), INotificationAgencyExternalBasic.class, List.of(new LogErrorHandler()), false);
+        new STSClientConfigurer(stsProperties.getUri().toString(), stsProperties.getUsername(), stsProperties.getPassword())
+                .configureRequestSamlToken(port);
+        // configureSTSFor(port);
         return port;
     }
-
-    private STSClient createSystemUserSTSClient(Client client) {
-        STSClient stsClient = new STSClient(client.getBus());
-        stsClient.setLocation(stsProperties.getUri().toString());
-        stsClient.setProperties(Map.of(
-                SecurityConstants.USERNAME, stsProperties.getUsername(),
-                SecurityConstants.PASSWORD, stsProperties.getPassword()
-        ));
-        stsClient.setEnableAppliesTo(false);
-        stsClient.setAllowRenewing(false);
-        stsClient.setFeatures(Arrays.asList(new LoggingFeature()));
-        stsClient.setPolicy(UNT_POLICY);
-        stsClient.getRequestContext().put(CACHE_ISSUED_TOKEN_IN_ENDPOINT, true);
-
-        var policyEngine = client.getBus().getExtension(PolicyEngine.class);
-        var endpointInfo = client.getEndpoint().getEndpointInfo();
-        var soapMessage = new SoapMessage(Soap12.getInstance());
-        var endpointPolicy = policyEngine.getClientEndpointPolicy(endpointInfo, null, soapMessage);
-        var policy = new RemoteReferenceResolver("", client.getBus().getExtension(PolicyBuilder.class)).resolveReference(SAML_POLICY);
-        policyEngine.setClientEndpointPolicy(endpointInfo, endpointPolicy.updatePolicy(policy, soapMessage));
-        return stsClient;
-    }
-
-    private void configureSTSFor(INotificationAgencyExternalBasic port) {
-        var client = ClientProxy.getClient(port);
-        var stsClient = createSystemUserSTSClient(client);
-        client.getRequestContext().put(SecurityConstants.STS_CLIENT, stsClient);
-    }
+//
+//    private STSClient createSystemUserSTSClient(Client client) {
+//        STSClient stsClient = new STSClient(client.getBus());
+//        stsClient.setLocation(stsProperties.getUri().toString());
+//        stsClient.setProperties(Map.of(
+//                SecurityConstants.USERNAME, stsProperties.getUsername(),
+//                SecurityConstants.PASSWORD, stsProperties.getPassword()
+//        ));
+//        stsClient.setEnableAppliesTo(false);
+//        stsClient.setAllowRenewing(false);
+//        stsClient.setFeatures(Arrays.asList(new LoggingFeature()));
+//        stsClient.setPolicy(UNT_POLICY);
+//        stsClient.getRequestContext().put(CACHE_ISSUED_TOKEN_IN_ENDPOINT, true);
+//
+//        var policyEngine = client.getBus().getExtension(PolicyEngine.class);
+//        var endpointInfo = client.getEndpoint().getEndpointInfo();
+//        var soapMessage = new SoapMessage(Soap12.getInstance());
+//        var endpointPolicy = policyEngine.getClientEndpointPolicy(endpointInfo, null, soapMessage);
+//        var policy = new RemoteReferenceResolver("", client.getBus().getExtension(PolicyBuilder.class)).resolveReference(SAML_POLICY);
+//        policyEngine.setClientEndpointPolicy(endpointInfo, endpointPolicy.updatePolicy(policy, soapMessage));
+//        return stsClient;
+//    }
+//
+//    private void configureSTSFor(INotificationAgencyExternalBasic port) {
+//        var client = ClientProxy.getClient(port);
+//        var stsClient = createSystemUserSTSClient(client);
+//        client.getRequestContext().put(SecurityConstants.STS_CLIENT, stsClient);
+//    }
 }
