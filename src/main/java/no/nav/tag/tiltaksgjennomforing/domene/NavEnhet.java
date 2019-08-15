5db14c40f25089c34599e478ab1773cb8215b62f
Sindre D Løken <sindre.loken@soprasteria.com>
2019-08-15 13:39:11 +0200

    Fjernet gyldighetssjekk pa NavEnhet som ikke gjorde noen ting

--- src/main/java/no/nav/tag/tiltaksgjennomforing/domene/NavEnhet.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/domene/NavEnhet.java
@@ -2,21 +2,13 @@ package no.nav.tag.tiltaksgjennomforing.domene;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Value;
-import no.nav.tag.tiltaksgjennomforing.domene.exceptions.TiltaksgjennomforingException;
 
 @Value
 public class NavEnhet implements Identifikator{
     private final String enhetId;
 
     public NavEnhet(String enhetId) {
-        if (!erGyldigNavEnhet(enhetId)) {
-            throw new TiltaksgjennomforingException("Ugyldig format på NAV Enhet");
-        }
         this.enhetId = enhetId;
-    }
-
-    public static boolean erGyldigNavEnhet(String id) {
-        return true;
     }
 
 
