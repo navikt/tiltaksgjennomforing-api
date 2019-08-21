31c38738ea42f112687b2ad0d992dfa624da6b53
Sindre D Løken <sindre.loken@soprasteria.com>
2019-08-21 17:34:03 +0200

    Lagt til test true og false

--- src/test/java/no/nav/tag/tiltaksgjennomforing/domene/MetrikkRegistreringTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/domene/MetrikkRegistreringTest.java
@@ -3,6 +3,7 @@ package no.nav.tag.tiltaksgjennomforing.domene;
 import io.micrometer.core.instrument.MeterRegistry;
 import no.nav.tag.tiltaksgjennomforing.domene.events.AvtaleOpprettet;
 import no.nav.tag.tiltaksgjennomforing.integrasjon.configurationProperties.PilotProperties;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.boot.test.rule.OutputCapture;
@@ -17,16 +18,25 @@ public class MetrikkRegistreringTest {
 
     @Rule
     public OutputCapture capture = new OutputCapture();
+    private PilotProperties pilotProperties;
+    private MetrikkRegistrering metrikkRegistrering;
+
+    @Before
+    public void setUp() throws Exception {
+        pilotProperties = new PilotProperties();
+        pilotProperties.setIdenter(List.of(new NavIdent("X123456")));
+        metrikkRegistrering = new MetrikkRegistrering(mock(MeterRegistry.class, RETURNS_DEEP_STUBS), pilotProperties);
+    }
 
     @Test
     public void opprettAvtale__skal_logge_pilotfylke() {
-        PilotProperties pilotProperties = new PilotProperties();
-        pilotProperties.setIdenter(List.of(new NavIdent("X123456")));
-        MeterRegistry meterRegistry = mock(MeterRegistry.class, RETURNS_DEEP_STUBS);
-        MetrikkRegistrering metrikkRegistrering = new MetrikkRegistrering(meterRegistry, pilotProperties);
         metrikkRegistrering.avtaleOpprettet(new AvtaleOpprettet(TestData.enAvtale(), new NavIdent("X123459")));
-
         assertThat(capture.toString()).contains("PilotFylke=true");
     }
 
+    @Test
+    public void opprettAvtale__skal_ikke_logge_pilotfylke() {
+        metrikkRegistrering.avtaleOpprettet(new AvtaleOpprettet(TestData.enAvtale(), new NavIdent("X123456")));
+        assertThat(capture.toString()).contains("PilotFylke=false");
+    }
 }
