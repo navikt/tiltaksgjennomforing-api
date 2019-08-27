0d6eb9ae52ef8a42ee6776cbeb46dd4eb2b78765
Sindre Dahl Løken <sindre.loken@soprasteria.com>
2019-08-27 16:28:55 +0200

    Fjernet pilotFylke fra counteren (#63)

--- src/main/java/no/nav/tag/tiltaksgjennomforing/domene/MetrikkRegistrering.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/domene/MetrikkRegistrering.java
@@ -23,52 +23,49 @@ public class MetrikkRegistrering {
     @EventListener
     public void avtaleOpprettet(AvtaleOpprettet event) {
         log.info("Avtale opprettet, avtaleId={} ident={}, PilotFylke={}", event.getAvtale().getId(), event.getUtfortAv(), pilotFylke(event.getUtfortAv()));
-        counter("avtale.opprettet", Avtalerolle.VEILEDER, pilotFylke(event.getUtfortAv())).increment();
+        counter("avtale.opprettet", Avtalerolle.VEILEDER).increment();
     }
 
     @EventListener
     public void avtaleEndret(AvtaleEndret event) {
         log.info("Avtale endret, avtaleId={} avtalepart={}", event.getAvtale().getId(), event.getUtfortAv());
-        counter("avtale.endret", event.getUtfortAv(), null).increment();
+        counter("avtale.endret", event.getUtfortAv()).increment();
     }
 
     @EventListener
     public void godkjenningerOpphevet(GodkjenningerOpphevet event) {
         log.info("Avtalens godkjenninger opphevet, avtaleId={} avtalepart={}", event.getAvtale().getId(), event.getUtfortAv());
-        counter("avtale.godkjenning.opphevet", event.getUtfortAv(), null).increment();
+        counter("avtale.godkjenning.opphevet", event.getUtfortAv()).increment();
     }
 
     @EventListener
     public void godkjentAvDeltaker(GodkjentAvDeltaker event) {
         log.info("Avtale godkjent, avtaleId={} avtalepart=DELTAKER", event.getAvtale().getId());
-        counter("avtale.godkjenning.godkjent", Avtalerolle.DELTAKER, null).increment();
+        counter("avtale.godkjenning.godkjent", Avtalerolle.DELTAKER).increment();
     }
 
     @EventListener
     public void godkjentAvArbeidsgiver(GodkjentAvArbeidsgiver event) {
         log.info("Avtale godkjent, avtaleId={} avtalepart=ARBEIDSGIVER", event.getAvtale().getId());
-        counter("avtale.godkjenning.godkjent", Avtalerolle.ARBEIDSGIVER, null).increment();
+        counter("avtale.godkjenning.godkjent", Avtalerolle.ARBEIDSGIVER).increment();
     }
 
     @EventListener
     public void godkjentAvVeileder(GodkjentAvVeileder event) {
         log.info("Avtale godkjent, avtaleId={} avtalepart=VEILEDER, PilotFylke={}", event.getAvtale().getId(), pilotFylke(event.getUtfortAv()));
-        counter("avtale.godkjenning.godkjent", Avtalerolle.VEILEDER, pilotFylke(event.getUtfortAv())).increment();
+        counter("avtale.godkjenning.godkjent", Avtalerolle.VEILEDER).increment();
     }
 
     @EventListener
     public void godkjentPaVegneAv(GodkjentPaVegneAv event) {
         log.info("Avtale godkjent på vegne av deltaker, avtaleId={} avtalepart=VEILEDER, PilotFylke={}", event.getAvtale().getId(), pilotFylke(event.getUtfortAv()));
-        counter("avtale.godkjenning.godkjentPaVegneAv", Avtalerolle.VEILEDER, pilotFylke(event.getUtfortAv())).increment();
+        counter("avtale.godkjenning.godkjentPaVegneAv", Avtalerolle.VEILEDER).increment();
     }
 
-    private Counter counter(String navn, Avtalerolle avtalerolle, Boolean erPilotFylke) {
+    private Counter counter(String navn, Avtalerolle avtalerolle) {
         var builder = Counter.builder("tiltaksgjennomforing." + navn)
                 .tag("tiltak", Tiltaktype.ARBEIDSTRENING.name())
                 .tag("avtalepart", avtalerolle.name());
-        if (erPilotFylke != null) {
-            builder.tag("pilotfylke", erPilotFylke.toString());
-        }
         return builder.register(meterRegistry);
     }
 
