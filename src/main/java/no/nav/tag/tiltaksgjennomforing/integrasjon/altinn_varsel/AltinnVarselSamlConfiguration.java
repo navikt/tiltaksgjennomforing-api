ef1ef59b82caff8ec2a76b28488ed7bec59a1e9b
Mats Bolstad <mbolstad@gmail.com>
2019-08-30 13:49:02 +0200

    Gjør de to beanene uavhengige,  retter feil i preprod

--- src/main/java/no/nav/tag/tiltaksgjennomforing/integrasjon/altinn_varsel/AltinnVarselSamlConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/integrasjon/altinn_varsel/AltinnVarselSamlConfiguration.java
@@ -2,8 +2,9 @@ package no.nav.tag.tiltaksgjennomforing.integrasjon.altinn_varsel;
 
 import lombok.RequiredArgsConstructor;
 import no.altinn.services.serviceengine.notification._2010._10.INotificationAgencyExternalBasic;
+import no.nav.tag.tiltaksgjennomforing.integrasjon.WsClient;
+import no.nav.tag.tiltaksgjennomforing.integrasjon.configurationProperties.AltinnVarselProperties;
 import no.nav.tag.tiltaksgjennomforing.integrasjon.configurationProperties.StsProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -13,11 +14,15 @@ import org.springframework.context.annotation.Profile;
 @Profile({"preprod", "prod"})
 @RequiredArgsConstructor
 public class AltinnVarselSamlConfiguration {
+    private final AltinnVarselProperties varselProperties;
     private final StsProperties stsProperties;
 
-    @Bean("iNotificationAgencyExternalBasicSaml")
+    @Bean
     @Primary
-    public INotificationAgencyExternalBasic iNotificationAgencyExternalBasic(@Autowired INotificationAgencyExternalBasic port) {
+    public INotificationAgencyExternalBasic iNotificationAgencyExternalBasic() {
+        var port = new WsClient<INotificationAgencyExternalBasic>().createPort(varselProperties.getUri().toString(),
+                INotificationAgencyExternalBasic.class,
+                true);
         STSClientConfigurer configurer = new STSClientConfigurer(stsProperties.getUri(), stsProperties.getUsername(), stsProperties.getPassword());
         configurer.configureRequestSamlToken(port);
         return port;
