1b743b3672bd1c024da7848ba357d9da637d23dd
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-09-16 11:52:29 +0200

    Unleash for pilottilgang (#76)
    * Forenkler unleash-initialisering litt
    
    * ByEnetStrategy som henter veileders enheter fra axsys
    
    * Støtter bruk av unleash for å styre pilottilgang
    
    * Lar AxsysService returnere Optional.empty i stedet for TiltaksgjennomforingException ved feil
    
    * Endrer isEqualTo(false) -> isFalse()
    
    * Endrer fra map() til flatMap()

--- src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/pilottilgang/TilgangUnderPilotering.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/pilottilgang/TilgangUnderPilotering.java
@@ -3,9 +3,9 @@ package no.nav.tag.tiltaksgjennomforing.autorisasjon.pilottilgang;
 import lombok.Data;
 import no.nav.tag.tiltaksgjennomforing.avtale.NavIdent;
 import no.nav.tag.tiltaksgjennomforing.exceptions.TilgangskontrollException;
-import org.springframework.stereotype.Component;
+import no.nav.tag.tiltaksgjennomforing.featuretoggles.FeatureToggleService;
 
-import java.util.List;
+import org.springframework.stereotype.Component;
 
 import static java.util.Collections.disjoint;
 
@@ -14,16 +14,29 @@ import static java.util.Collections.disjoint;
 @Component
 public class TilgangUnderPilotering {
 
+    static final String TAG_TILTAK_PILOTTILGANG = "tag.tiltak.pilottilgang";
+    static final String TAG_TILTAK_BRUK_UNLEASH_FOR_PILOTTILGANG = "tag.tiltak.bruk.unleash.for.pilottilgang";
+
     private final PilotProperties pilotProperties;
     private final AxsysService axsysService;
+    private final FeatureToggleService featureToggleService;
 
     public void sjekkTilgang(NavIdent ident) {
-        if (pilotProperties.isEnabled() && !pilotProperties.getIdenter().contains(ident)) {
-            List<NavEnhet> enheterInnloggetVeilederHarTilgangTil = axsysService.hentEnheterVeilederHarTilgangTil(ident);
-            if (disjoint(pilotProperties.getEnheter(), enheterInnloggetVeilederHarTilgangTil)) {
-                throw new TilgangskontrollException("Ident " + ident.asString() + " er ikke lagt til i lista over brukere med tilgang.");
-            }
+        boolean tilgangOk = featureToggleService.isEnabled(TAG_TILTAK_BRUK_UNLEASH_FOR_PILOTTILGANG) ? sjekkPilotTilgangMedUnleash() : sjekkPilottilgangMedVault(ident);
+        if(!tilgangOk) {
+            throw new TilgangskontrollException("Ident " + ident.asString() + " er ikke lagt til i lista over brukere med tilgang.");
         }
+    }
+
+    private boolean sjekkPilotTilgangMedUnleash() {
+        return featureToggleService.isEnabled(TAG_TILTAK_PILOTTILGANG);
+    }
+
+    private boolean sjekkPilottilgangMedVault(NavIdent ident) {
+        if (pilotProperties.isEnabled() && !pilotProperties.getIdenter().contains(ident)) {
+            return axsysService.hentEnheterVeilederHarTilgangTil(ident).map(enheter -> !disjoint(pilotProperties.getEnheter(), enheter)).orElse(false);
+        }
+        return true;
     }
 
 }
