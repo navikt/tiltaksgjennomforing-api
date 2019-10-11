c2ab3677a9a2442b32abb575e2f647d9da6d15c6
Mats Bolstad <mbolstad@gmail.com>
2019-10-11 15:11:14 +0200

    Retter opp feilaktig merge

--- src/main/java/no/nav/tag/tiltaksgjennomforing/TiltaksgjennomforingApplication.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/TiltaksgjennomforingApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -16,7 +15,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 @EnableConfigurationProperties
 @EnableJpaRepositories
-@EnableCaching
 public class TiltaksgjennomforingApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplicationBuilder(TiltaksgjennomforingApplication.class)
