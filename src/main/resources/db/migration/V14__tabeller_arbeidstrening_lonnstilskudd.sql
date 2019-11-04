e5791a244c16c52daee5d001cf09590db0235cfb
Mats Bolstad <mbolstad@gmail.com>
2019-11-04 15:24:50 +0100

    Kolonnen tiltakstype settes til not null og default ARBEIDSTRENING

--- src/main/resources/db/migration/V14__tabeller_arbeidstrening_lonnstilskudd.sql
+++ src/main/resources/db/migration/V14__tabeller_arbeidstrening_lonnstilskudd.sql
@@ -1,4 +1,4 @@
-alter table avtale add column tiltakstype                   varchar;
+alter table avtale add column tiltakstype                   varchar not null default 'ARBEIDSTRENING';
 alter table avtale add column arbeidsgiver_kontonummer      varchar(11);
 alter table avtale add column stillingtype                  varchar;
 alter table avtale add column stillingbeskrivelse           varchar;
@@ -6,5 +6,3 @@ alter table avtale add column lonnstilskudd_prosent         integer;
 alter table avtale add column manedslonn                    integer;
 alter table avtale add column feriepengesats                decimal;
 alter table avtale add column arbeidsgiveravgift            decimal;
-
-update avtale set tiltakstype = 'ARBEIDSTRENING';
