cc6732ba24ad642b5a614dd97e99365b723e762f
Mats Bolstad <mbolstad@gmail.com>
2019-11-18 10:23:30 +0100

    Fjerne en-til-en-relasjon og embedde de i avtale-tabellen (#104)

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/GodkjentPaVegneGrunn.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/GodkjentPaVegneGrunn.java
@@ -1,34 +1,20 @@
 package no.nav.tag.tiltaksgjennomforing.avtale;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import lombok.ToString;
 import no.nav.tag.tiltaksgjennomforing.exceptions.TiltaksgjennomforingException;
 
-import javax.persistence.*;
-import java.util.UUID;
+import javax.persistence.Embeddable;
 
 @Data
-@Entity
+@Embeddable
 public class GodkjentPaVegneGrunn {
-    @Id
-    @Column(name = "avtale")
-    @JsonIgnore
-    private UUID id;
-
-    @OneToOne
-    @JoinColumn(name = "avtale")
-    @JsonIgnore
-    @ToString.Exclude
-    private Avtale avtale;
-
     private boolean ikkeBankId;
     private boolean reservert;
     private boolean digitalKompetanse;
 
     public void valgtMinstEnGrunn() {
         if (!ikkeBankId && !reservert && !digitalKompetanse) {
-            throw new TiltaksgjennomforingException("Minst èn grunn må være valgt");
+            throw new TiltaksgjennomforingException("Minst én grunn må være valgt");
         }
     }
 }
