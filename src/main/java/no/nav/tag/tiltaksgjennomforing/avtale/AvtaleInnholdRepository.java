fe47cfedfd483ecacdf8a78e2e5a524a51d53957
bjartty <bjarte.tynning@nav.no>
2019-12-12 13:50:05 +0100

    POC: Hente avtaleversjoner til journalføring logging 2

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleInnholdRepository.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleInnholdRepository.java
@@ -8,7 +8,8 @@ import java.util.UUID;
 
 public interface AvtaleInnholdRepository extends JpaRepository<AvtaleInnhold, UUID> {
 
-    @Query(value = "select * from public.avtale_innhold ai where ai.journalpost_id is null and ai.godkjent_av_veileder is not null", nativeQuery = true)
+    @Query(value = "select ai from AvtaleInnhold ai where ai.journalpostId is null and ai.godkjentAvVeileder is not null")
+   // @Query(value = "select * from public.avtale_innhold ai where ai.journalpost_id is null and ai.godkjent_av_veileder is not null", nativeQuery = true)
     List<AvtaleInnhold> finnAvtaleVersjonerTilJournalfoering();
 }
 
