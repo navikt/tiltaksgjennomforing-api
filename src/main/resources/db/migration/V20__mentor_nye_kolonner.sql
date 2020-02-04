9cf7605365c08a743bdca0f6401436265631bb1c
bjartty <bjarte.tynning@nav.no>
2020-02-04 09:31:22 +0100

    Mentor: endringer e. pr.

--- src/main/resources/db/migration/V20__mentor_nye_kolonner.sql
+++ src/main/resources/db/migration/V20__mentor_nye_kolonner.sql
@@ -1,5 +1,5 @@
-alter table avtale_innhold add column mentor_fornavn varchar(255);
-alter table avtale_innhold add column mentor_etternavn varchar(255);
-alter table avtale_innhold add column mentor_oppgaver varchar(1000);
+alter table avtale_innhold add column mentor_fornavn varchar;
+alter table avtale_innhold add column mentor_etternavn varchar;
+alter table avtale_innhold add column mentor_oppgaver varchar;
 alter table avtale_innhold add column mentor_antall_timer integer;
 alter table avtale_innhold add column mentor_timelonn integer;
