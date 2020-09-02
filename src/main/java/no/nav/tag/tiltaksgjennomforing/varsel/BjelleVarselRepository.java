3e4efc6d3a07c7832e71113f3c4c4354a589edcf
Mats Bolstad <mbolstad@gmail.com>
2020-09-02 11:26:50 +0200

    Returnerer bare varsler som er nyere enn 24t (#191)

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/BjelleVarselRepository.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/BjelleVarselRepository.java
@@ -3,6 +3,8 @@ package no.nav.tag.tiltaksgjennomforing.varsel;
 import io.micrometer.core.annotation.Timed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,4 +12,7 @@ public interface BjelleVarselRepository extends JpaRepository<BjelleVarsel, UUID
     @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
     @Override
     List<BjelleVarsel> findAll();
+
+    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    List<BjelleVarsel> findAllByTidspunktAfter(LocalDateTime tidspunkt);
 }
