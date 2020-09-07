0471cee862ec8e969934016484644f6f8518b97f
Sindre Dahl Løken <sindre.loken@nav.no>
2020-09-07 17:56:32 +0200

    laget ikke valgt part exception som handteres i frontend med cookies Co-Authored-By: Mats Bolstad <mats.bolstad@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/InnloggetBrukerController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/InnloggetBrukerController.java
@@ -1,6 +1,7 @@
 package no.nav.tag.tiltaksgjennomforing.autorisasjon;
 
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtalerolle;
+import no.nav.tag.tiltaksgjennomforing.exceptions.IkkeValgtPartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -24,7 +25,7 @@ public class InnloggetBrukerController {
     }
 
     @GetMapping
-    public ResponseEntity<InnloggetBruker<?>> hentInnloggetBruker(@CookieValue("innlogget-part") Avtalerolle innloggetPart) {
-        return ResponseEntity.ok(innloggingService.hentInnloggetBruker(innloggetPart));
+    public ResponseEntity<InnloggetBruker<?>> hentInnloggetBruker(@CookieValue("innlogget-part") Optional<Avtalerolle> innloggetPart) {
+        return ResponseEntity.ok(innloggingService.hentInnloggetBruker(innloggetPart.orElseThrow(IkkeValgtPartException::new)));
     }
 }
---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/exceptions/FeilkodeException.java
@@ -0,0 +1,6 @@
+package no.nav.tag.tiltaksgjennomforing.exceptions;
+
+public abstract class FeilkodeException extends RuntimeException {
+    abstract String feilkode();
+
+}
