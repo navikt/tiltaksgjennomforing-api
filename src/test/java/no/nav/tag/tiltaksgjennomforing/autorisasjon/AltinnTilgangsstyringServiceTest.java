1358791e9235173f4bcec410930b710efae98f08
Mats Bolstad <mbolstad@gmail.com>
2020-09-25 09:15:45 +0200

    Fikser test

--- src/test/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/AltinnTilgangsstyringServiceTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/AltinnTilgangsstyringServiceTest.java
@@ -3,14 +3,14 @@ package no.nav.tag.tiltaksgjennomforing.autorisasjon;
 import no.nav.tag.tiltaksgjennomforing.Miljø;
 import no.nav.tag.tiltaksgjennomforing.TestData;
 import no.nav.tag.tiltaksgjennomforing.autorisasjon.altinntilgangsstyring.AltinnOrganisasjon;
+import no.nav.tag.tiltaksgjennomforing.autorisasjon.altinntilgangsstyring.AltinnTilgangsstyringProperties;
+import no.nav.tag.tiltaksgjennomforing.autorisasjon.altinntilgangsstyring.AltinnTilgangsstyringService;
 import no.nav.tag.tiltaksgjennomforing.avtale.BedriftNr;
 import no.nav.tag.tiltaksgjennomforing.avtale.Fnr;
 import no.nav.tag.tiltaksgjennomforing.avtale.Tiltakstype;
 import no.nav.tag.tiltaksgjennomforing.exceptions.TiltaksgjennomforingException;
 import no.nav.tag.tiltaksgjennomforing.featuretoggles.FeatureToggleService;
 import no.nav.tag.tiltaksgjennomforing.orgenhet.ArbeidsgiverOrganisasjon;
-import no.nav.tag.tiltaksgjennomforing.autorisasjon.altinntilgangsstyring.AltinnTilgangsstyringService;
-import no.nav.tag.tiltaksgjennomforing.autorisasjon.altinntilgangsstyring.AltinnTilgangsstyringProperties;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,12 +27,11 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles({ Miljø.LOCAL, "wiremock"})
+@ActiveProfiles({ Miljø.LOCAL, "wiremock" })
 @DirtiesContext
 public class AltinnTilgangsstyringServiceTest {
     @Autowired
@@ -51,32 +50,34 @@ public class AltinnTilgangsstyringServiceTest {
     }
 
     @Test
-    public void hentOrganisasjoner__gyldig_fnr_en_bedrift_pa_hvert_titlak() {
+    public void hentOrganisasjoner__gyldig_fnr_en_bedrift_på_hvert_tiltak() {
         List<ArbeidsgiverOrganisasjon> organisasjoner = altinnTilgangsstyringService.hentOrganisasjoner(new Fnr("10000000000"));
-        assertThat(organisasjoner).extracting("bedriftNr").containsOnly(new BedriftNr("999999999"), new BedriftNr("910712314"), new BedriftNr("910712306"), new BedriftNr("910825550"));
+        assertThat(organisasjoner).extracting(ArbeidsgiverOrganisasjon.Fields.bedriftNr).containsOnly(new BedriftNr("999999999"), new BedriftNr("910712314"), new BedriftNr("910712306"), new BedriftNr("910825550"));
     }
 
     @Test
-    public void hentOrganisasjoner__fnr_skal_ha_tilgang_pa_arbeidstrening() {
-        List<ArbeidsgiverOrganisasjon> organisasjoner =altinnTilgangsstyringService.hentOrganisasjoner(new Fnr("20000000000"));
-        assertThat(organisasjoner).flatExtracting(ArbeidsgiverOrganisasjon.Fields.tilgangstyper).containsOnly(Tiltakstype.ARBEIDSTRENING);
+    public void hentOrganisasjoner__fnr_skal_ha_tilgang_på_arbeidstrening_og_varig() {
+        List<ArbeidsgiverOrganisasjon> organisasjoner = altinnTilgangsstyringService.hentOrganisasjoner(new Fnr("20000000000"));
+        assertThat(organisasjoner).flatExtracting(ArbeidsgiverOrganisasjon.Fields.tilgangstyper).containsOnly(Tiltakstype.ARBEIDSTRENING, Tiltakstype.VARIG_LONNSTILSKUDD);
     }
+
     @Test
-    public void hentOrganisasjoner__fnr_skal_ha_tilgang_pa_midlertidig_lonnstilskudd() {
+    public void hentOrganisasjoner__fnr_skal_ha_tilgang_på_midlertidig_lonnstilskudd() {
         List<ArbeidsgiverOrganisasjon> organisasjoner = altinnTilgangsstyringService.hentOrganisasjoner(new Fnr("26000000000"));
         assertThat(organisasjoner).flatExtracting(ArbeidsgiverOrganisasjon.Fields.tilgangstyper).containsOnly(Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD);
     }
+
     @Test
-    public void hentOrganisasjoner__fnr_skal_ha_tilgang_pa_varig_lonnstilskudd() {
+    public void hentOrganisasjoner__fnr_skal_ha_tilgang_på_varig_lonnstilskudd() {
         List<ArbeidsgiverOrganisasjon> organisasjoner = altinnTilgangsstyringService.hentOrganisasjoner(new Fnr("27000000000"));
         assertThat(organisasjoner).flatExtracting(ArbeidsgiverOrganisasjon.Fields.tilgangstyper).containsOnly(Tiltakstype.VARIG_LONNSTILSKUDD);
     }
+
     @Test
-    public void hentOrganisasjoner__fnr_skal_ha_tilgang_pa_alle_tiltakstyper() {
+    public void hentOrganisasjoner__fnr_skal_ha_tilgang_på_alle_tiltakstyper() {
         List<ArbeidsgiverOrganisasjon> organisasjoner = altinnTilgangsstyringService.hentOrganisasjoner(new Fnr("10000000000"));
         assertThat(organisasjoner).flatExtracting(ArbeidsgiverOrganisasjon.Fields.tilgangstyper).contains(Tiltakstype.ARBEIDSTRENING, Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD, Tiltakstype.VARIG_LONNSTILSKUDD);
     }
-
 
     @Test
     public void hentOrganisasjoner__gyldig_fnr_en_andre_bedrift() {
@@ -90,19 +91,19 @@ public class AltinnTilgangsstyringServiceTest {
         assertThat(organisasjoner).hasSize(0);
     }
 
-    @Test (expected = TiltaksgjennomforingException.class)
+    @Test(expected = TiltaksgjennomforingException.class)
     public void hentOrganisasjoner__ugyldig_fnr_skal_kaste_feil() {
-        List<ArbeidsgiverOrganisasjon> organisasjoner = altinnTilgangsstyringService.hentOrganisasjoner(TestData.enIdentifikator());
+        altinnTilgangsstyringService.hentOrganisasjoner(TestData.enIdentifikator());
     }
 
-    @Test (expected = TiltaksgjennomforingException.class)
+    @Test(expected = TiltaksgjennomforingException.class)
     public void hentOrganisasjoner__feilkonfigurasjon_skal_kaste_feil() {
         AltinnTilgangsstyringProperties altinnTilgangsstyringProperties = new AltinnTilgangsstyringProperties();
         altinnTilgangsstyringProperties.setUri(URI.create("http://foobar"));
         new AltinnTilgangsstyringService(altinnTilgangsstyringProperties, tokenUtils, featureToggleService).hentOrganisasjoner(TestData.enIdentifikator());
     }
 
-    @Test (expected = TiltaksgjennomforingException.class)
+    @Test(expected = TiltaksgjennomforingException.class)
     public void hentOrganisasjoner__manglende_serviceCode_skal_kaste_feil() {
         AltinnTilgangsstyringProperties altinnTilgangsstyringProperties = new AltinnTilgangsstyringProperties();
         new AltinnTilgangsstyringService(altinnTilgangsstyringProperties, tokenUtils, featureToggleService);
@@ -111,7 +112,7 @@ public class AltinnTilgangsstyringServiceTest {
     @Test
     public void hentOrganisasjoner__skal_ha_parentOrganization() {
         Set<AltinnOrganisasjon> organisasjoner = altinnTilgangsstyringService.hentAltinnOrganisasjoner(new Fnr("27999999999"));
-        AltinnOrganisasjon organisasjon = organisasjoner.stream().filter(org -> org.getOrganizationNumber().equals("910825607")).findFirst().get();
+        AltinnOrganisasjon organisasjon = organisasjoner.stream().filter(org -> org.getOrganizationNumber().equals("910825607")).findFirst().orElseThrow();
 
         assertThat(organisasjon.getOrganizationForm()).isEqualTo("BEDR");
         assertThat(organisasjon.getStatus()).isEqualTo("Active");
@@ -125,6 +126,5 @@ public class AltinnTilgangsstyringServiceTest {
         Set<AltinnOrganisasjon> altinnOrganisasjoner = altinnTilgangsstyringService.hentAltinnOrganisasjoner(new Fnr("30000000000"));
         assertThat(altinnOrganisasjoner).filteredOn(a -> a.getOrganizationNumber().equals("910825607")).hasSize(1);
     }
-
 
 }
