4210ea346b5e1384de1fb9d1876585955a957e6c
bjartty <bjarte.tynning@nav.no>
2020-10-09 09:17:34 +0200

    Mindre testkode. Laget innerclass av oppgaveresponse.
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/oppgave/OppgaveResponse.java
+++
@@ -1,8 +0,0 @@
-package no.nav.tag.tiltaksgjennomforing.varsel.oppgave;
-
-import lombok.Data;
-
-@Data
-public class OppgaveResponse {
-    private String id;
-}
--- src/test/java/no/nav/tag/tiltaksgjennomforing/varsel/oppgave/OppgaveVarselServiceTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/varsel/oppgave/OppgaveVarselServiceTest.java
@@ -1,11 +1,14 @@
 package no.nav.tag.tiltaksgjennomforing.varsel.oppgave;
 
+import no.nav.tag.tiltaksgjennomforing.avtale.Tiltakstype;
 import no.nav.tag.tiltaksgjennomforing.exceptions.GosysFeilException;
 import no.nav.tag.tiltaksgjennomforing.infrastruktur.sts.STSClient;
 import no.nav.tag.tiltaksgjennomforing.infrastruktur.sts.STSToken;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
@@ -16,7 +19,7 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.util.UUID;
 
-import static no.nav.tag.tiltaksgjennomforing.avtale.Tiltakstype.*;
+import static no.nav.tag.tiltaksgjennomforing.avtale.Tiltakstype.VARIG_LONNSTILSKUDD;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -28,7 +31,7 @@ import static org.mockito.Mockito.when;
 public class OppgaveVarselServiceTest {
 
     private URI uri = URI.create("test");
-    private OppgaveResponse response = new OppgaveResponse();
+    private OppgaveVarselService.OppgaveResponse oppgaveResponse = new OppgaveVarselService.OppgaveResponse();
     private OppgaveProperties oppgaveProperties = new OppgaveProperties();
 
     @Captor
@@ -44,65 +47,26 @@ public class OppgaveVarselServiceTest {
 
     @BeforeEach
     public void setUp() {
-        response.setId("oppgaveId");
+        oppgaveResponse.setId("oppgaveId");
         oppgaveProperties.setOppgaveUri(uri);
         oppgaveVarselService = new OppgaveVarselService(oppgaveProperties, restTemplate, stsClient);
     }
 
-    @Test
-    public void oppretterOppgaveRequestForArbeidstrening(){
-
+    @ParameterizedTest
+    @EnumSource(Tiltakstype.class)
+    public void oppretterOppgaveRequestForTiltak(Tiltakstype tiltakstype){
         when(stsClient.hentSTSToken()).thenReturn(new STSToken());
-        when(restTemplate.postForObject(any(URI.class), any(), any(Class.class))).thenReturn(response);
+        when(restTemplate.postForObject(any(URI.class), any(), any(Class.class))).thenReturn(oppgaveResponse);
 
-        oppgaveVarselService.opprettOppgave("aktørId", ARBEIDSTRENING, UUID.randomUUID());
-        verify(restTemplate).postForObject(eq(uri), requestCaptor.capture(), eq(OppgaveResponse.class));
+        oppgaveVarselService.opprettOppgave("aktørId", tiltakstype, UUID.randomUUID());
+        verify(restTemplate).postForObject(eq(uri), requestCaptor.capture(), eq(OppgaveVarselService.OppgaveResponse.class));
         OppgaveRequest request = requestCaptor.getValue().getBody();
 
         assertThat(request.getAktivDato()).isToday();
         assertThat(request.getAktoerId()).isEqualTo("aktørId");
-        assertThat(request.getBehandlingstema()).isEqualTo(ARBEIDSTRENING.getBehandlingstema());
-        assertThat(request.getBeskrivelse()).isEqualTo("Avtale er opprettet av arbeidsgiver på tiltak ARBEIDSTRENING");
-        assertThat(request.getBehandlingstype()).isEqualTo("ae0034");
-        assertThat(request.getOppgavetype()).isEqualTo("VURD_HENV");
-        assertThat(request.getPrioritet()).isEqualTo("HOY");
-        assertThat(request.getTema()).isEqualTo("TIL");
-    }
-
-    @Test
-    public void oppretterOppgaveRequestForMidlertidigLønnstilskudd(){
-
-        when(stsClient.hentSTSToken()).thenReturn(new STSToken());
-        when(restTemplate.postForObject(any(URI.class), any(), any(Class.class))).thenReturn(response);
-
-        oppgaveVarselService.opprettOppgave("aktørId", MIDLERTIDIG_LONNSTILSKUDD, UUID.randomUUID());
-        verify(restTemplate).postForObject(eq(uri), requestCaptor.capture(), eq(OppgaveResponse.class));
-        OppgaveRequest request = requestCaptor.getValue().getBody();
-
-        assertThat(request.getAktivDato()).isToday();
-        assertThat(request.getAktoerId()).isEqualTo("aktørId");
-        assertThat(request.getBehandlingstema()).isEqualTo(MIDLERTIDIG_LONNSTILSKUDD.getBehandlingstema());
-        assertThat(request.getBeskrivelse()).isEqualTo("Avtale er opprettet av arbeidsgiver på tiltak MIDLERTIDIG_LONNSTILSKUDD");
-        assertThat(request.getBehandlingstype()).isEqualTo("ae0034");
-        assertThat(request.getOppgavetype()).isEqualTo("VURD_HENV");
-        assertThat(request.getPrioritet()).isEqualTo("HOY");
-        assertThat(request.getTema()).isEqualTo("TIL");
-    }
-
-    @Test
-    public void oppretterOppgaveRequestForVarigLønnstilskudd(){
-
-        when(stsClient.hentSTSToken()).thenReturn(new STSToken());
-        when(restTemplate.postForObject(any(URI.class), any(), any(Class.class))).thenReturn(response);
-
-        oppgaveVarselService.opprettOppgave("aktørId", VARIG_LONNSTILSKUDD, UUID.randomUUID());
-        verify(restTemplate).postForObject(eq(uri), requestCaptor.capture(), eq(OppgaveResponse.class));
-        OppgaveRequest request = requestCaptor.getValue().getBody();
-
-        assertThat(request.getAktivDato()).isToday();
-        assertThat(request.getAktoerId()).isEqualTo("aktørId");
-        assertThat(request.getBehandlingstema()).isEqualTo(VARIG_LONNSTILSKUDD.getBehandlingstema());
-        assertThat(request.getBeskrivelse()).isEqualTo("Avtale er opprettet av arbeidsgiver på tiltak VARIG_LONNSTILSKUDD");
+        assertThat(request.getBehandlingstema()).isEqualTo(tiltakstype.getBehandlingstema());
+        assertThat(request.getBeskrivelse()).contains(tiltakstype.name());
+        assertThat(request.getBeskrivelse()).contains("Avtale er opprettet av arbeidsgiver på tiltak ");
         assertThat(request.getBehandlingstype()).isEqualTo("ae0034");
         assertThat(request.getOppgavetype()).isEqualTo("VURD_HENV");
         assertThat(request.getPrioritet()).isEqualTo("HOY");
@@ -111,7 +75,6 @@ public class OppgaveVarselServiceTest {
 
     @Test
     public void oppretterOppgaveRequestFeiler() {
-
         when(stsClient.hentSTSToken()).thenReturn(new STSToken());
         when(restTemplate.postForObject(any(URI.class), any(), any(Class.class))).thenThrow(RuntimeException.class);
 
