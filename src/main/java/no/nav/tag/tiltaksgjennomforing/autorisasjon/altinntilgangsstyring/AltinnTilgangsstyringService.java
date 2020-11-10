8b89670237aea4a6bc401e7feeb99223af453c96
Sindre Dahl Løken <sindre.loken@nav.no>
2020-11-10 16:30:09 +0100

    Altinn proxy klient (#297)
    * wip test
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    
    * kortere proxyUri
    
    * logger henting av org
    
    * gjor gammel henting i else
    
    * oppdaterer fallbackUri og logger mer
    
    * logger mer
    
    * Tester med enFrom og ny discovery url for loginservice utside
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    
    * Skiller pa kall med og uten serviceCode og serviceEditionCode
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    
    * mapper reportees respons til AltinnOrganisasjon
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    
    * mapper reportees respons til AltinnOrganisasjon pa en annen mate
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    
    * WIP Byttet til AltinnReportee type og fjernet egen handtering av fallback
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    
    * Setter lokal proxyUri til noe annet slik at klienten gar mot fallback lokalt sa vi enklere kan matche i wiremock
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    
    * Oppdtatert mappings til a ogsa forvente ingen sc og sec
    
    * Retter opp prority i mappings og kaster riktig AltinnException
    
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    
    * Ryddet imports og fjernet autowired
    
    * Henter applicationName fra spring property og fjerner ubrukt toggle
    
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    
    * Passer inn applicationName i test
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    
    * ryddet imports, dummifisert proxyUri lokalt og pa labs og revertet en test
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/altinntilgangsstyring/AltinnTilgangsstyringService.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/altinntilgangsstyring/AltinnTilgangsstyringService.java
@@ -3,40 +3,36 @@ package no.nav.tag.tiltaksgjennomforing.autorisasjon.altinntilgangsstyring;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.AltinnConfig;
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.AltinnrettigheterProxyKlient;
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.AltinnrettigheterProxyKlientConfig;
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.ProxyConfig;
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.error.exceptions.AltinnrettigheterProxyKlientFallbackException;
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.model.*;
 import no.nav.tag.tiltaksgjennomforing.autorisasjon.TokenUtils;
 import no.nav.tag.tiltaksgjennomforing.avtale.BedriftNr;
 import no.nav.tag.tiltaksgjennomforing.avtale.Fnr;
-import no.nav.tag.tiltaksgjennomforing.avtale.Identifikator;
 import no.nav.tag.tiltaksgjennomforing.avtale.Tiltakstype;
 import no.nav.tag.tiltaksgjennomforing.exceptions.AltinnFeilException;
 import no.nav.tag.tiltaksgjennomforing.exceptions.TiltaksgjennomforingException;
-import no.nav.tag.tiltaksgjennomforing.featuretoggles.FeatureToggleService;
 import no.nav.tag.tiltaksgjennomforing.utils.Utils;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.util.*;
 
 @Component
 @Slf4j
 public class AltinnTilgangsstyringService {
-    private static final int ALTINN_ORG_PAGE_SIZE = 999;
     private final AltinnTilgangsstyringProperties altinnTilgangsstyringProperties;
-    private final RestTemplate restTemplate;
     private final TokenUtils tokenUtils;
-    private final FeatureToggleService featureToggleService;
+    private final AltinnrettigheterProxyKlient klient;
 
     public AltinnTilgangsstyringService(
             AltinnTilgangsstyringProperties altinnTilgangsstyringProperties,
             TokenUtils tokenUtils,
-            FeatureToggleService featureToggleService
-    ) {
+            @Value("${spring.application.name}") String applicationName) {
+
         if (Utils.erNoenTomme(altinnTilgangsstyringProperties.getArbtreningServiceCode(),
                 altinnTilgangsstyringProperties.getArbtreningServiceEdition(),
                 altinnTilgangsstyringProperties.getLtsMidlertidigServiceCode(),
@@ -47,87 +43,67 @@ public class AltinnTilgangsstyringService {
         }
         this.altinnTilgangsstyringProperties = altinnTilgangsstyringProperties;
         this.tokenUtils = tokenUtils;
-        this.featureToggleService = featureToggleService;
-        restTemplate = new RestTemplate();
-    }
 
-    private URI lagAltinnUrl(Integer serviceCode, Integer serviceEdition, Identifikator fnr) {
-        return UriComponentsBuilder.fromUri(altinnTilgangsstyringProperties.getUri())
-                .queryParam("ForceEIAuthentication")
-                .queryParam("subject", fnr.asString())
-                .queryParam("serviceCode", serviceCode)
-                .queryParam("serviceEdition", serviceEdition)
-                .queryParam("$filter", "Type+ne+'Person'")
-                .queryParam("$top", ALTINN_ORG_PAGE_SIZE)
-                .build()
-                .toUri();
-    }
 
-    private URI lagAltinnProxyUrl(Integer serviceCode, Integer serviceEdition) {
-        return UriComponentsBuilder.fromUri(altinnTilgangsstyringProperties.getProxyUri())
-                .queryParam("ForceEIAuthentication")
-                .queryParam("serviceCode", serviceCode)
-                .queryParam("serviceEdition", serviceEdition)
-                .queryParam("$filter", "Type+ne+'Person'")
-                .queryParam("$top", ALTINN_ORG_PAGE_SIZE)
-                .build()
-                .toUri();
+        String altinnProxyUrl = altinnTilgangsstyringProperties.getProxyUri().toString();
+        String altinnProxyFallbackUrl = altinnTilgangsstyringProperties.getUri().toString();
+
+        AltinnrettigheterProxyKlientConfig proxyKlientConfig = new AltinnrettigheterProxyKlientConfig(
+                new ProxyConfig(applicationName, altinnProxyUrl),
+                new AltinnConfig(
+                        altinnProxyFallbackUrl,
+                        altinnTilgangsstyringProperties.getAltinnApiKey(),
+                        altinnTilgangsstyringProperties.getApiGwApiKey()
+                )
+        );
+        this.klient = new AltinnrettigheterProxyKlient(proxyKlientConfig);
+
     }
 
     public Map<BedriftNr, Collection<Tiltakstype>> hentTilganger(Fnr fnr) {
         Multimap<BedriftNr, Tiltakstype> tilganger = HashMultimap.create();
 
-        AltinnOrganisasjon[] arbeidstreningOrger = kallAltinn(altinnTilgangsstyringProperties.getArbtreningServiceCode(), altinnTilgangsstyringProperties.getArbtreningServiceEdition(), fnr);
+        AltinnReportee[] arbeidstreningOrger = kallAltinn(altinnTilgangsstyringProperties.getArbtreningServiceCode(), altinnTilgangsstyringProperties.getArbtreningServiceEdition(), fnr);
         leggTil(tilganger, arbeidstreningOrger, Tiltakstype.ARBEIDSTRENING);
 
-        AltinnOrganisasjon[] varigLtsOrger = kallAltinn(altinnTilgangsstyringProperties.getLtsVarigServiceCode(), altinnTilgangsstyringProperties.getLtsVarigServiceEdition(), fnr);
+        AltinnReportee[] varigLtsOrger = kallAltinn(altinnTilgangsstyringProperties.getLtsVarigServiceCode(), altinnTilgangsstyringProperties.getLtsVarigServiceEdition(), fnr);
         leggTil(tilganger, varigLtsOrger, Tiltakstype.VARIG_LONNSTILSKUDD);
 
-        AltinnOrganisasjon[] midlLtsOrger = kallAltinn(altinnTilgangsstyringProperties.getLtsMidlertidigServiceCode(), altinnTilgangsstyringProperties.getLtsMidlertidigServiceEdition(), fnr);
+        AltinnReportee[] midlLtsOrger = kallAltinn(altinnTilgangsstyringProperties.getLtsMidlertidigServiceCode(), altinnTilgangsstyringProperties.getLtsMidlertidigServiceEdition(), fnr);
         leggTil(tilganger, midlLtsOrger, Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD);
 
         return tilganger.asMap();
     }
 
-    private void leggTil(Multimap<BedriftNr, Tiltakstype> tilganger, AltinnOrganisasjon[] arbeidstreningOrger, Tiltakstype tiltakstype) {
-        for (AltinnOrganisasjon altinnOrganisasjon : arbeidstreningOrger) {
-            if (!altinnOrganisasjon.getType().equals("Enterprise")) {
-                tilganger.put(new BedriftNr(altinnOrganisasjon.getOrganizationNumber()), tiltakstype);
+    private void leggTil(Multimap<BedriftNr, Tiltakstype> tilganger, AltinnReportee[] arbeidstreningOrger, Tiltakstype tiltakstype) {
+        for (AltinnReportee altinnReportee : arbeidstreningOrger) {
+            if (!altinnReportee.getType().equals("Enterprise")) {
+                tilganger.put(new BedriftNr(altinnReportee.getOrganizationNumber()), tiltakstype);
             }
         }
     }
 
-    public Set<AltinnOrganisasjon> hentAltinnOrganisasjoner(Fnr fnr) {
+    public Set<AltinnReportee> hentAltinnOrganisasjoner(Fnr fnr) {
         return new HashSet<>(List.of(kallAltinn(null, null, fnr)));
     }
 
-    private AltinnOrganisasjon[] kallAltinn(Integer serviceCode, Integer serviceEdition, Fnr fnr) {
+    private AltinnReportee[] kallAltinn(Integer serviceCode, Integer serviceEdition, Fnr fnr) {
         try {
-            boolean brukProxy = featureToggleService.isEnabled("arbeidsgiver.tiltaksgjennomforing-api.bruk-altinn-proxy");
-            HttpEntity<HttpHeaders> headers = brukProxy ? getAuthHeadersForInnloggetBruker() : getAuthHeadersForAltinn();
-            URI uri = brukProxy ? lagAltinnProxyUrl(serviceCode, serviceEdition) : lagAltinnUrl(serviceCode, serviceEdition, fnr);
-            return restTemplate.exchange(
-                    uri,
-                    HttpMethod.GET,
-                    headers,
-                    AltinnOrganisasjon[].class
-            ).getBody();
-        } catch (RestClientException exception) {
+                List<AltinnReportee> reportees;
+                if (serviceCode != null && serviceEdition != null) {
+                    reportees = klient.hentOrganisasjoner(
+                            new SelvbetjeningToken(tokenUtils.hentSelvbetjeningToken()),
+                            new Subject(fnr.asString()), new ServiceCode(serviceCode.toString()), new ServiceEdition(serviceEdition.toString()),
+                            true
+                    );
+                } else {
+                    reportees = klient.hentOrganisasjoner(new SelvbetjeningToken(tokenUtils.hentSelvbetjeningToken()), new Subject(fnr.asString()), true);
+                }
+                return reportees.toArray(new AltinnReportee[0]);
+
+        } catch (AltinnrettigheterProxyKlientFallbackException exception) {
             log.warn("Feil ved kall mot Altinn.", exception);
             throw new AltinnFeilException();
         }
-    }
-
-    private HttpEntity<HttpHeaders> getAuthHeadersForInnloggetBruker() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(tokenUtils.hentSelvbetjeningToken());
-        return new HttpEntity<>(headers);
-    }
-
-    private HttpEntity<HttpHeaders> getAuthHeadersForAltinn() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-NAV-APIKEY", altinnTilgangsstyringProperties.getApiGwApiKey());
-        headers.set("APIKEY", altinnTilgangsstyringProperties.getAltinnApiKey());
-        return new HttpEntity<>(headers);
     }
 }
