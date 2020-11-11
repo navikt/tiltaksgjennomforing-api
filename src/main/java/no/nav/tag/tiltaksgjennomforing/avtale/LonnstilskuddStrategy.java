62f7e114df855ee099e4e6f3a0c9626fd6dba15e
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-11-11 12:28:41 +0100

    Oppdatert beregning paa manedslonn fullstilling
    Co-authored-by: Sindre Dahl Løken <sindre.loken@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/LonnstilskuddStrategy.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/LonnstilskuddStrategy.java
@@ -35,7 +35,7 @@ public class LonnstilskuddStrategy extends BaseAvtaleInnholdStrategy {
             nyAvtale.getArbeidsgiveravgift());
         Integer sumLonnsutgifter = getSumLonnsutgifter(nyAvtale.getManedslonn(), feriepengerBelop, obligTjenestepensjon, arbeidsgiveravgiftBelop);
         Integer sumlønnTilskudd = getSumLonnsTilskudd(sumLonnsutgifter, nyAvtale.getLonnstilskuddProsent());
-        Integer månedslønnFullStilling = getLønnVedFullStilling(nyAvtale.getManedslonn(), nyAvtale.getStillingprosent());
+        Integer månedslønnFullStilling = getLønnVedFullStilling(sumLonnsutgifter, nyAvtale.getStillingprosent());
 
         avtaleInnhold.setFeriepengerBelop(feriepengerBelop);
         avtaleInnhold.setOtpBelop(obligTjenestepensjon);
