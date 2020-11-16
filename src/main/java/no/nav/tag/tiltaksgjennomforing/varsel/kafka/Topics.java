b92a555a452f2b01ceca77da79b823da6008221b
Millad Dagdoni <millad.dagdoni@nav.no>
2020-11-16 12:21:56 +0100

    rettet på kafka kønavn

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/Topics.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/Topics.java
@@ -1,7 +1,7 @@
 package no.nav.tag.tiltaksgjennomforing.varsel.kafka;
 
 public class Topics {
-    public static final String STATISTIKKFORMIDLING = "privat-tiltaksgjennomforing-statistikk-formidling";
+    public static final String STATISTIKKFORMIDLING = "privat-tiltaksgjennomforing-statistikkformidling";
     public static final String SMS_VARSEL = "privat-tiltaksgjennomforing-smsVarsel";
     public static final String SMS_VARSEL_RESULTAT = "privat-tiltaksgjennomforing-smsVarselResultat";
 
