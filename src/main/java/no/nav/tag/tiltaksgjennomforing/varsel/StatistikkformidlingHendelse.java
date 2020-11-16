0d36cd51cc80c0ee16b1ce6a49e2913d6c2b1601
Millad Dagdoni <millad.dagdoni@nav.no>
2020-11-16 13:15:18 +0100

    Sende StatistikkformidlingHendelse for både Godkjent av veilder og på vegne av andre

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/StatistikkformidlingHendelse.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/StatistikkformidlingHendelse.java
@@ -2,6 +2,7 @@ package no.nav.tag.tiltaksgjennomforing.varsel;
 
 import lombok.RequiredArgsConstructor;
 import no.nav.tag.tiltaksgjennomforing.avtale.events.GodkjentAvVeileder;
+import no.nav.tag.tiltaksgjennomforing.avtale.events.GodkjentPaVegneAv;
 import no.nav.tag.tiltaksgjennomforing.varsel.kafka.StatistikkformidlingProducer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,11 @@ public class StatistikkformidlingHendelse {
 
     @TransactionalEventListener
     public void godkjentAvVeileder(GodkjentAvVeileder event) {
+        statistikkformidlingProducer.publiserStatistikkformidlingMelding(event.getAvtale());
+    }
+
+    @TransactionalEventListener
+    public void godkjentPaVegneAv(GodkjentPaVegneAv event) {
         statistikkformidlingProducer.publiserStatistikkformidlingMelding(event.getAvtale());
     }
 
