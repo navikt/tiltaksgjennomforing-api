50b893e07db06774450266b712172f91aa12bb7f
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-11-19 16:36:01 +0100

    Beregner tilskuddsperioder med tester.
    Co-authored-by: Bjarte Tynning <bjarte.tynning@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/TilskuddForPeriode.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/TilskuddForPeriode.java
@@ -5,9 +5,7 @@ import java.math.MathContext;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.YearMonth;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static java.time.temporal.ChronoUnit.DAYS;
@@ -17,68 +15,40 @@ public class TilskuddForPeriode {
     private final static int dagerIMåned = 30;
     private final static int antallMånederIEnPeriode = 3;
 
-    public List<TilskuddPeriode> beregnTilskuddForPeriode(Integer lønnPrMåned, LocalDate datoFom, LocalDate datoTom){
+    public List<TilskuddPeriode> beregnTilskuddForPeriode(final Integer lønnPrMåned, final LocalDate datoFraOgMed, final LocalDate datoTilOgMed){
 
-//        return opprettTilskuddsPerioder(lønnPrMåned, datoFom, datoTom);
-
-        Period period = Period.between(datoFom, datoTom);
+        Period period = Period.between(datoFraOgMed, datoTilOgMed.plusDays(1));
         List<TilskuddPeriode> tilskuddPerioder = new ArrayList<>();
-        LocalDate nyDatoFom = datoFom;
-        LocalDate nyDatoTom;
+        LocalDate nyDatoFraOgMed = datoFraOgMed;
+        LocalDate nyDatoTilOgMed;
         TilskuddPeriode nyTsPeriode;
 
-        while(period.getMonths() >= 3){
-            nyDatoTom = nyDatoFom.plusMonths(antallMånederIEnPeriode);
-            nyTsPeriode = new TilskuddPeriode(lønnPrMåned * antallMånederIEnPeriode, nyDatoFom, nyDatoTom, false);
+        while(period.getMonths() >= antallMånederIEnPeriode){
+            nyDatoTilOgMed = nyDatoFraOgMed.plusMonths(antallMånederIEnPeriode);
+            nyTsPeriode = new TilskuddPeriode(lønnPrMåned * antallMånederIEnPeriode, nyDatoFraOgMed, nyDatoTilOgMed.minusDays(1), false);
             tilskuddPerioder.add(nyTsPeriode);
-            nyDatoFom = nyTsPeriode.getDatoTom().plusDays(1);
+            nyDatoFraOgMed = nyDatoTilOgMed;
             period = period.minusMonths(antallMånederIEnPeriode);
         }
 
         if(!period.isZero()){
-            nyDatoTom = nyDatoFom.plus(period);
-            nyTsPeriode = beregnTilsagnPåSistePeriode(lønnPrMåned, nyDatoFom, nyDatoTom);
+            nyTsPeriode = beregnTilsagnPåSistePeriode(lønnPrMåned, nyDatoFraOgMed, datoTilOgMed);
             tilskuddPerioder.add(nyTsPeriode);
         }
         return tilskuddPerioder;
     }
 
-    private TilskuddPeriode beregnTilsagnPåSistePeriode(Integer lønnPrMåned, LocalDate datoFom, LocalDate datoTom){ //TODO Avklare beregning av resterende periode: Skal vi ta  med helgene?
+    long beregnetilskuddsperiode(LocalDate fraDato, LocalDate tilDato){
+        return DAYS.between(fraDato, tilDato.plusDays(1));
+    }
 
-//        case: periode = 1 jan - 15 feb:
-
-//        1. Finn antall_dager i hele perioden (1 jan - 31 mars)
-        int antallDagerIHelePerioden = dagerIMåned * antallMånederIEnPeriode;
-
-//        2. Finn lønn for hele perioden (1 jan - 31 mars): lønnPrMåned * 3,
+    private TilskuddPeriode beregnTilsagnPåSistePeriode(Integer lønnPrMåned, LocalDate datoFom, LocalDate datoTom){
+        int antallDagerITilskuddsPerioden = dagerIMåned * antallMånederIEnPeriode;
         BigDecimal totalLønnIPerioden = BigDecimal.valueOf(lønnPrMåned).multiply(BigDecimal.valueOf(antallMånederIEnPeriode));
-
-//        3. Del max periodelønn (1 jan - 31 mars) => dagslønn
-//        int dagslønn = totalLønnIPerioden / antallDagerIHelePerioden;
-
-        BigDecimal dagslønn = totalLønnIPerioden.divide(BigDecimal.valueOf(antallDagerIHelePerioden), MathContext.DECIMAL32).setScale(3, RoundingMode.HALF_UP);
-
-
-//        4. Finn antall_dager i perioden 1 jan - 15 feb
-        long dagerISistePerioden = DAYS.between(datoFom, datoTom.plusDays(1));
-
-//        5. multipliser dagslønn med antall antall_dager fra 3.
-        Integer lønnISistePeriode = dagslønn.multiply(BigDecimal.valueOf(dagerISistePerioden)).setScale(0, RoundingMode.HALF_UP).intValue();
+        BigDecimal dagslønn = totalLønnIPerioden.divide(BigDecimal.valueOf(antallDagerITilskuddsPerioden), MathContext.DECIMAL32).setScale(3, RoundingMode.HALF_UP);
+        long dagerISisteTilskuddsPerioden = beregnetilskuddsperiode(datoFom, datoTom);
+        Integer lønnISistePeriode = dagslønn.multiply(BigDecimal.valueOf(dagerISisteTilskuddsPerioden)).setScale(0, RoundingMode.HALF_UP).intValue();
 
         return new TilskuddPeriode(lønnISistePeriode, datoFom, datoTom, true);
     }
-
-    private boolean erEnHelMåned(LocalDate datoFom, LocalDate datoTom) {
-        return datoFom.getDayOfMonth() == 1 && datoTom.equals(YearMonth.from(datoTom).atEndOfMonth());
-    }
-
 }
-
-
-
-
-
-
-//        BigDecimal dagLønn = månedsBeløp.divide(dagerIMåned, MathContext.DECIMAL32).setScale(3, RoundingMode.HALF_DOWN);
-//        Integer beløp = dagLønn.multiply(BigDecimal.valueOf(dagerIPerioden)).intValue();
-
--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/TilskuddForPeriodeTest.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/TilskuddForPeriodeTest.java
@@ -47,9 +47,6 @@ public class TilskuddForPeriodeTest {
         List<TilskuddPeriode> list  = tilskuddForPeriode.beregnTilskuddForPeriode(månedslønn, datoFom, datoTom);
         assertEquals(11, (DAYS.between(list.get(0).getDatoFom(), list.get(0).getDatoTom()) + 1L));
         assertEquals(7333, list.get(0).getBeløp());
-
-//        int refusjonForPeriode = tilskuddForPeriode.beregnTilskuddForPeriode(månedslønn, datoFom, datoTom);
-//        assertEquals(6667, refusjonForPeriode);
     }
 
     @Test
@@ -65,26 +62,73 @@ public class TilskuddForPeriodeTest {
     }
 
     @Test
+    public void sjekkAtBeregnettilskuddsperiodeReturnerer45dager() {
+        LocalDate avtaleStartDato = LocalDate.of(2020, 4, 1);
+        LocalDate avtaleSluttDato = LocalDate.of(2020, 5, 15);
+        long antalltilskuddsdager = tilskuddForPeriode.beregnetilskuddsperiode(avtaleStartDato, avtaleSluttDato);
+
+        assertEquals(45, antalltilskuddsdager);
+
+        avtaleStartDato = LocalDate.of(2020, 5, 1);
+        avtaleSluttDato = LocalDate.of(2020, 5, 15);
+        antalltilskuddsdager = tilskuddForPeriode.beregnetilskuddsperiode(avtaleStartDato, avtaleSluttDato);
+
+        assertEquals(15, antalltilskuddsdager);
+    }
+
+    @Test
+    public void lag2RefusjonerOgEndag() {
+        LocalDate avtaleStartDato = LocalDate.of(2020, 3, 1);
+        LocalDate avtaleSluttDato = LocalDate.of(2020, 9, 1);
+        assertEquals(185, tilskuddForPeriode.beregnetilskuddsperiode(avtaleStartDato, avtaleSluttDato));
+        LocalDate _31_MAI = LocalDate.of(2020, 5, 31);
+        LocalDate _1_JUNI = LocalDate.of(2020, 6, 1);
+        LocalDate _31_AUG = LocalDate.of(2020, 8, 31);
+
+        List<TilskuddPeriode> perioder = tilskuddForPeriode.beregnTilskuddForPeriode(20000, avtaleStartDato, avtaleSluttDato);
+
+        assertEquals(3, perioder.size());
+
+        TilskuddPeriode tilskuddPeriode = perioder.get(0);
+        assertEquals(avtaleStartDato, tilskuddPeriode.getDatoFom());
+        assertEquals(_31_MAI, tilskuddPeriode.getDatoTom());
+        assertEquals(60000, tilskuddPeriode.getBeløp());
+
+        tilskuddPeriode = perioder.get(1);
+        assertEquals(60000, tilskuddPeriode.getBeløp());
+        assertEquals(_1_JUNI, tilskuddPeriode.getDatoFom());
+        assertEquals(_31_AUG, tilskuddPeriode.getDatoTom());
+
+        tilskuddPeriode = perioder.get(2);
+        assertEquals(avtaleSluttDato, tilskuddPeriode.getDatoTom());
+        assertEquals(1, tilskuddForPeriode.beregnetilskuddsperiode(tilskuddPeriode.getDatoFom(), tilskuddPeriode.getDatoTom()));
+        assertEquals(667, tilskuddPeriode.getBeløp());
+    }
+
+
+    @Test
     public void lager1ogEnHalvRefusjonsperioder() {
-        LocalDate datoFom = LocalDate.of(2020, 1, 1);
-        LocalDate datoTom = LocalDate.of(2020, 5, 15);
+        LocalDate avtaleStartDato = LocalDate.of(2020, 1, 1);
+        LocalDate avtaleSluttDato = LocalDate.of(2020, 5, 15);
+        assertEquals(136, tilskuddForPeriode.beregnetilskuddsperiode(avtaleStartDato, avtaleSluttDato));
         LocalDate _31_MARS = LocalDate.of(2020, 3, 31);
         LocalDate _1_APRIL = LocalDate.of(2020, 4, 1);
 
-        List<TilskuddPeriode> perioder = tilskuddForPeriode.beregnTilskuddForPeriode(20000, datoFom, datoTom);
+        List<TilskuddPeriode> perioder = tilskuddForPeriode.beregnTilskuddForPeriode(20000, avtaleStartDato, avtaleSluttDato);
 
         assertEquals(2, perioder.size());
         TilskuddPeriode tilskuddPeriode = perioder.get(0);
-        assertEquals(datoFom, tilskuddPeriode.getDatoFom());
+        assertEquals(avtaleStartDato, tilskuddPeriode.getDatoFom());
         assertEquals(_31_MARS, tilskuddPeriode.getDatoTom());
 
         assertEquals(60000, tilskuddPeriode.getBeløp());
 
         tilskuddPeriode = perioder.get(1);
         assertEquals(_1_APRIL, tilskuddPeriode.getDatoFom());
-        assertEquals(datoTom, tilskuddPeriode.getDatoTom());
+        assertEquals(avtaleSluttDato, tilskuddPeriode.getDatoTom());
 
-        assertEquals(45, DAYS.between(tilskuddPeriode.getDatoFom(), tilskuddPeriode.getDatoTom()) + 1L);
+
+        assertEquals(45, tilskuddForPeriode.beregnetilskuddsperiode(tilskuddPeriode.getDatoFom(), tilskuddPeriode.getDatoTom()));
 
         assertEquals(30000, tilskuddPeriode.getBeløp());
     }
