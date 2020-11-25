48e74faf64550d626cf4118903fb3cccae7456d7
Millad Dagdoni <millad.dagdoni@nav.no>
2020-11-25 11:16:46 +0100

    lagt til ssl konfig for kafka aiven i application.yaml og application-dev-fss.yaml og application-local.yaml

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/avien/AivenKafkaConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/avien/AivenKafkaConfiguration.java
@@ -1,19 +1,10 @@
 package no.nav.tag.tiltaksgjennomforing.varsel.kafka.avien;
 
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 
 @ConditionalOnProperty("tiltaksgjennomforing.kafka.enabled")
 @Configuration
@@ -23,15 +14,33 @@ public class AivenKafkaConfiguration {
 
   @Value("${no.nav.gcp.kafka.aiven.bootstrap-servers}")
   private String bootstrapServers;
-
+  private final String javaKeystore = "jks";
+  private final String pkcs12 = "PKCS12";
+  @Value("${no.nav.gcp.kafka.aiven.truststore-path}")
+  private String sslTruststoreLocationEnvKey;
+  @Value("${no.nav.gcp.kafka.aiven.truststore-password}")
+  private String sslTruststorePasswordEnvKey;
+  @Value("${no.nav.gcp.kafka.aiven.keystore-path}")
+  private String sslKeystoreLocationEnvKey;
+  @Value("${no.nav.gcp.kafka.aiven.keystore-password}")
+  private String sslKeystorePasswordEnvKey;
+/*
   @Bean
   Map<String, Object> producerConfigs() {
     Map<String, Object> props = new HashMap<>();
-  //  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name);
+    props.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "");
+    props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG , javaKeystore);
+    props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG , pkcs12);
+    props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG , sslTruststoreLocationEnvKey);
+    props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG , sslTruststorePasswordEnvKey);
+    props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG , sslKeystoreLocationEnvKey);
+    props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG , sslKeystorePasswordEnvKey);
     return props;
   }
-/*
+
   @Bean
   ProducerFactory<String, String> producerFactory() {
     return new DefaultKafkaProducerFactory<>(producerConfigs());
