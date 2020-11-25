da09bad7814553f0969b6b21ccc9b74c3c74b0d0
Millad Dagdoni <millad.dagdoni@nav.no>
2020-11-25 16:45:30 +0100

    riktig topic name ihh nais.io

--- src/main/java/no/nav/tag/tiltaksgjennomforing/refusjon/Topics.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/refusjon/Topics.java
@@ -1,5 +1,5 @@
 package no.nav.tag.tiltaksgjennomforing.refusjon;
 
 public class Topics {
-    public static final String REFUSJON = "refusjon";
+    public static final String REFUSJON = "arbeidsgiver.refusjon";
 }
