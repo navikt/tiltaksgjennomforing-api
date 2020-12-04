12c8c5dc8ace1622f2bb2f6ef6be0d6369524d51
Millad Dagdoni <millad.dagdoni@nav.no>
2020-12-04 11:42:13 +0100

    fjernet ref til avtale_innhold på resten av spørringen
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    Co-authored-by: Bjarte Tynning <bjarte.tynning@nav.no>

--- src/main/resources/db/migration/V34__otp_sats_avtaleinnhold.sql
+++ src/main/resources/db/migration/V34__otp_sats_avtaleinnhold.sql
@@ -1,6 +1,6 @@
 alter table avtale_innhold add column otp_sats decimal;
 
-update avtale_innhold set avtale_innhold.otp_sats = 0.02
-where avtale_innhold.avtale
+update avtale_innhold set otp_sats = 0.02
+where avtale
 in (select id from avtale where avtale.tiltakstype = 'MIDLERTIDIG_LONNSTILSKUDD' OR tiltakstype = 'VARIG_LONNSTILSKUDD' )
- and avtale_innhold.otp_sats is null;
+ and otp_sats is null;
