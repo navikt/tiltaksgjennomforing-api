204802254929f2ee43c162b54db8f912a69bbc9a
Millad Dagdoni <millad.dagdoni@nav.no>
2020-12-04 11:31:00 +0100

    linjeshift på sql kommando
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    Co-authored-by: Bjarte Tynning <bjarte.tynning@nav.no>

--- src/main/resources/db/migration/V34__otp_sats_avtaleinnhold.sql
+++ src/main/resources/db/migration/V34__otp_sats_avtaleinnhold.sql
@@ -1,3 +1,6 @@
 alter table avtale_innhold add column otp_sats decimal;
 
-update avtale_innhold set avtale_innhold.otp_sats = 0.02 where avtale_innhold.avtale in (select id from avtale where avtale.tiltakstype = 'MIDLERTIDIG_LONNSTILSKUDD' OR tiltakstype = 'VARIG_LONNSTILSKUDD' ) and avtale_innhold.otp_sats is null;
+update avtale_innhold set avtale_innhold.otp_sats = 0.02
+where avtale_innhold.avtale
+in (select id from avtale where avtale.tiltakstype = 'MIDLERTIDIG_LONNSTILSKUDD' OR tiltakstype = 'VARIG_LONNSTILSKUDD' )
+ and avtale_innhold.otp_sats is null;
