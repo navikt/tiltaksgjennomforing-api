7dfa5791c9e99fd0f475f3e66d9b370a5eec5e18
Millad Dagdoni <millad.dagdoni@nav.no>
2020-12-04 13:02:41 +0100

    fjernet V34__otp_sats_avtaleinnhold.sql
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    Co-authored-by: Bjarte Tynning <bjarte.tynning@nav.no>

--- src/main/resources/db/migration/V34__otp_sats_avtaleinnhold.sql
+++
@@ -1,7 +0,0 @@
-alter table avtale_innhold add column otp_sats decimal;
-
-UPDATE avtale_innhold
-SET avtale_innhold.otp_sats = 0.02
-WHERE avtale_innhold.avtale
-IN (SELECT id from avtale WHERE avtale.tiltakstype = 'MIDLERTIDIG_LONNSTILSKUDD' OR tiltakstype = 'VARIG_LONNSTILSKUDD' )
-AND avtale_innhold.otp_sats is NULL;
