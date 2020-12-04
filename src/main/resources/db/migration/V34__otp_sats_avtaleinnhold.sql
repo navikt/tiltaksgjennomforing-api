439547bf7a723a353ec906e4feaf9219167f9457
Millad Dagdoni <millad.dagdoni@nav.no>
2020-12-04 11:30:18 +0100

    små lowercased på sql commandoer
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    Co-authored-by: Bjarte Tynning <bjarte.tynning@nav.no>

--- src/main/resources/db/migration/V34__otp_sats_avtaleinnhold.sql
+++ src/main/resources/db/migration/V34__otp_sats_avtaleinnhold.sql
@@ -1,7 +1,3 @@
 alter table avtale_innhold add column otp_sats decimal;
 
-UPDATE avtale_innhold
-SET avtale_innhold.otp_sats = 0.02
-WHERE avtale_innhold.avtale
-IN (SELECT id from avtale WHERE avtale.tiltakstype = 'MIDLERTIDIG_LONNSTILSKUDD' OR tiltakstype = 'VARIG_LONNSTILSKUDD' )
-AND avtale_innhold.otp_sats is NULL;
+update avtale_innhold set avtale_innhold.otp_sats = 0.02 where avtale_innhold.avtale in (select id from avtale where avtale.tiltakstype = 'MIDLERTIDIG_LONNSTILSKUDD' OR tiltakstype = 'VARIG_LONNSTILSKUDD' ) and avtale_innhold.otp_sats is null;
