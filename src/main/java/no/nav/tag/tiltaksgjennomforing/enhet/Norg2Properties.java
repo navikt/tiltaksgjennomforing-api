872d5cf84afa97f8137df5df42d0d428b2887555
bjartty <bjarte.tynning@nav.no>
2020-12-22 11:19:12 +0100

    Korrigere feil

--- src/main/java/no/nav/tag/tiltaksgjennomforing/enhet/Norg2Properties.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/enhet/Norg2Properties.java
@@ -1,8 +1,10 @@
 package no.nav.tag.tiltaksgjennomforing.enhet;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
 @ConfigurationProperties(prefix = "tiltaksgjennomforing.norg2")
 public class Norg2Properties {
