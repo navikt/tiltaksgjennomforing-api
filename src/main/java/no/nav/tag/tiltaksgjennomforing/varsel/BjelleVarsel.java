931c1d28c0eb139a9ddeef40fd238bf85f6b87b7
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-01-29 10:02:17 +0100

    Co-authored-by: Sindre Dahl Løken <sindre.loken@nav.no>
    opprettet metode for getvarslbarHendelse type

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/BjelleVarsel.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/BjelleVarsel.java
@@ -4,13 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
+import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
 import no.nav.tag.tiltaksgjennomforing.avtale.Identifikator;
 import no.nav.tag.tiltaksgjennomforing.avtale.IdentifikatorConverter;
+import no.nav.tag.tiltaksgjennomforing.avtale.TilskuddPeriode;
 import org.springframework.data.domain.AbstractAggregateRoot;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Locale;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
@@ -30,13 +34,29 @@ public class BjelleVarsel extends AbstractAggregateRoot<BjelleVarsel> {
     private UUID avtaleId;
     private LocalDateTime tidspunkt;
 
-    public static BjelleVarsel nyttVarsel(Identifikator identifikator, VarslbarHendelse varslbarHendelse) {
+    public static String getVarslbarHEndelseType(VarslbarHendelse varslbarHendelse, Avtale avtale) {
+        if (varslbarHendelse.getVarslbarHendelseType() == VarslbarHendelseType.TILSKUDDSPERIODE_AVSLATT) {
+            TilskuddPeriode gjeldendePeriode = avtale.gjeldendeTilskuddsperiode();
+            String avslagÅrsaker = gjeldendePeriode.getAvslagsårsaker().stream()
+                    .map(type ->  type.getTekst().toLowerCase(Locale.ROOT).concat(" ")).collect(Collectors.joining());
+            return varslbarHendelse.getVarslbarHendelseType().getTekst()
+                    .concat(gjeldendePeriode.getAvslåttAvNavIdent().toString().substring(20, 27))
+                    .concat(". Årsak til retur: ")
+                    .concat(avslagÅrsaker)
+                    .concat("Forklaring: ")
+                    .concat(gjeldendePeriode.getAvslagsforklaring());
+
+        }
+        return varslbarHendelse.getVarslbarHendelseType().getTekst();
+    }
+
+    public static BjelleVarsel nyttVarsel(Identifikator identifikator, VarslbarHendelse varslbarHendelse, Avtale avtale) {
         BjelleVarsel varsel = new BjelleVarsel();
         varsel.id = UUID.randomUUID();
         varsel.tidspunkt = LocalDateTime.now();
         varsel.identifikator = identifikator;
         varsel.varslbarHendelse = varslbarHendelse.getId();
-        varsel.varslingstekst = varslbarHendelse.getVarslbarHendelseType().getTekst();
+        varsel.varslingstekst = getVarslbarHEndelseType(varslbarHendelse, avtale);
         varsel.varslbarHendelseType = varslbarHendelse.getVarslbarHendelseType();
         varsel.avtaleId = varslbarHendelse.getAvtaleId();
         return varsel;
