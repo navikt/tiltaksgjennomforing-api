5f97293ab1a824e107de97a6c2294b05c8638a7c
Millad Dagdoni <millad.dagdoni@nav.no>
2021-02-03 12:47:23 +0100

    Sperre på maks lønnstilskudd prosent (#486)

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/VarigLonnstilskuddStrategy.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/VarigLonnstilskuddStrategy.java
@@ -1,8 +1,20 @@
 package no.nav.tag.tiltaksgjennomforing.avtale;
 
+import no.nav.tag.tiltaksgjennomforing.exceptions.FeilLonnstilskuddsprosentException;
+
 public class VarigLonnstilskuddStrategy extends LonnstilskuddStrategy {
 
     public VarigLonnstilskuddStrategy(AvtaleInnhold avtaleInnhold) {
         super(avtaleInnhold);
     }
+
+    @Override
+    public void endre(EndreAvtale endreAvtale) {
+        if (endreAvtale.getLonnstilskuddProsent() != null && (
+            endreAvtale.getLonnstilskuddProsent() < 0 || endreAvtale.getLonnstilskuddProsent() > 75)) {
+            throw new FeilLonnstilskuddsprosentException();
+        }
+        super.endre(endreAvtale);
+    }
+
 }
