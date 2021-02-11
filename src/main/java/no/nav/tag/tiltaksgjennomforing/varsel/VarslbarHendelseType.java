00722a867e214c7c83f0c4053905116b67806234
Mats Bolstad <mbolstad@gmail.com>
2021-02-11 09:53:59 +0100

    Tar inn endringer på VarslbarHendelse

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/VarslbarHendelseType.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/VarslbarHendelseType.java
@@ -6,24 +6,24 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum VarslbarHendelseType {
-    OPPRETTET("Avtale er opprettet av NAV-veileder"),
+    OPPRETTET("Avtale er opprettet av veileder"),
     GODKJENT_AV_ARBEIDSGIVER("Avtale er godkjent av arbeidsgiver"),
-    GODKJENT_AV_VEILEDER("Avtale er godkjent av NAV-veileder"),
+    GODKJENT_AV_VEILEDER("Avtale er godkjent av veileder"),
     GODKJENT_AV_DELTAKER("Avtale er godkjent av deltaker"),
-    GODKJENT_PAA_VEGNE_AV("Avtale er godkjent av NAV-veileder"),
+    GODKJENT_PAA_VEGNE_AV("Avtalen ble godkjent på vegne av deltaker"),
     GODKJENNINGER_OPPHEVET_AV_ARBEIDSGIVER("Avtalens godkjenninger er opphevet av arbeidsgiver"),
-    GODKJENNINGER_OPPHEVET_AV_VEILEDER("Avtalens godkjenninger er opphevet av NAV-veileder"),
-    SMS_VARSLING_FEILET("Varsling på SMS har feilet"),
-    ENDRET("Avtale endret"),
+    GODKJENNINGER_OPPHEVET_AV_VEILEDER("Avtalens godkjenninger er opphevet av veileder"),
     DELT_MED_DELTAKER("Avtale delt med deltaker"),
     DELT_MED_ARBEIDSGIVER("Avtale delt med arbeidsgiver"),
+    ENDRET("Avtale endret"),
     AVBRUTT("Avtale avbrutt av veileder"),
     LÅST_OPP("Avtale låst opp av veileder"),
     GJENOPPRETTET("Avtale gjenopprettet"),
     OPPRETTET_AV_ARBEIDSGIVER("Avtale er opprettet av arbeidsgiver"),
     NY_VEILEDER("Avtale tildelt ny veileder"),
     AVTALE_FORDELT("Avtale tildelt veileder"),
-    TILSKUDDSPERIODE_AVSLATT("Avtalen har blitt sendt i retur av ");
+    TILSKUDDSPERIODE_AVSLATT("Tilskuddsperioden har blitt sendt i retur av "),
+    TILSKUDDSPERIODE_GODKJENT("Tilskuddsperioden har blitt godkjent av beslutter");
 
     private final String tekst;
 
