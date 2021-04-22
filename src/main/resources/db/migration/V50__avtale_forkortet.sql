2dff6ca745e2b4d8da15d3c16bd918ec0dd79b9b
Mats Bolstad <mbolstad@gmail.com>
2021-04-22 11:05:17 +0200

    Ikke references avtale_innhold, siden det mangler primary key der også

--- src/main/resources/db/migration/V50__avtale_forkortet.sql
+++ src/main/resources/db/migration/V50__avtale_forkortet.sql
@@ -1,7 +1,7 @@
 create table avtale_forkortet(
     id uuid primary key,
     avtale_id uuid references avtale(id),
-    avtale_innhold_id uuid references avtale_innhold(id),
+    avtale_innhold_id uuid,
     tidspunkt timestamp,
     ny_slutt_dato date,
     grunn varchar,
