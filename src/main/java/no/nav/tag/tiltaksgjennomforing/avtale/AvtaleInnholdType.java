6a59a78620a52ffb357ad2c99e6b37ae78611a6c
Mats Bolstad <mats.bolstad@nav.no>
2021-05-19 16:49:15 +0200

    Type på AvtaleInnhold (#647)
    * Type på avtaleinnhold
    
    * Fikser testdata
    
    * Fikser test

---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleInnholdType.java
@@ -0,0 +1,5 @@
+package no.nav.tag.tiltaksgjennomforing.avtale;
+
+public enum AvtaleInnholdType {
+    INNGÅ, LÅSE_OPP, FORLENGE, FORKORTE, ENDRE_TILSKUDDSBEREGNING, ENDRE_STILLING, ENDRE_KONTAKTINFO, ENDRE_OPPFØLGING_OG_TILRETTELEGGING, ANNULLERE
+}
