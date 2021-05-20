0d40deef8cef6bfbd14b3b973c659f44ed6b987e
Mats Bolstad <mbolstad@gmail.com>
2021-05-20 11:01:34 +0200

    Ikke @TransactionalEventListener, siden kafka-produsenten tar hensyn til det

--- src/main/java/no/nav/tag/tiltaksgjennomforing/datavarehus/DvhAvtalehendelseLytter.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/datavarehus/DvhAvtalehendelseLytter.java
@@ -3,8 +3,8 @@ package no.nav.tag.tiltaksgjennomforing.datavarehus;
 import lombok.RequiredArgsConstructor;
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
 import no.nav.tag.tiltaksgjennomforing.avtale.events.*;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,43 +15,43 @@ public class DvhAvtalehendelseLytter {
     private final DvhMeldingEntitetRepository repository;
     private final DvhMeldingFilter dvhMeldingFilter;
 
-    @TransactionalEventListener
+    @EventListener
     public void avtaleInngått(AvtaleInngått event) {
         Avtale avtale = event.getAvtale();
         lagHendelse(avtale, DvhHendelseType.INNGÅTT);
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void avtaleForlenget(AvtaleForlenget event) {
         lagHendelse(event.getAvtale(), DvhHendelseType.FORLENGET);
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void avtaleForkortet(AvtaleForkortet event) {
         lagHendelse(event.getAvtale(), DvhHendelseType.FORKORTET);
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void avtaleAnnullert(AnnullertAvVeileder event) {
         lagHendelse(event.getAvtale(), DvhHendelseType.ANNULLERT);
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void tilskuddsberegningEndret(TilskuddsberegningEndret event) {
         lagHendelse(event.getAvtale(), DvhHendelseType.ENDRET);
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void stillingsbeskrivelseEndret(StillingsbeskrivelseEndret event) {
         lagHendelse(event.getAvtale(), DvhHendelseType.ENDRET);
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void kontaktinformasjonEndret(KontaktinformasjonEndret event) {
         lagHendelse(event.getAvtale(), DvhHendelseType.ENDRET);
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void oppfølgingOgTilretteleggingEndret(OppfølgingOgTilretteleggingEndret event) {
         lagHendelse(event.getAvtale(), DvhHendelseType.ENDRET);
     }
