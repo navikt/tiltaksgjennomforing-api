a36d32afc78938eb734f62d35e7d223753846974
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-06-09 17:37:38 +0200

    lagt til filtrering paa veilederAvtaler pr enhet

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleRepository.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleRepository.java
@@ -11,39 +11,45 @@ import java.util.Set;
 import java.util.UUID;
 
 public interface AvtaleRepository extends JpaRepository<Avtale, UUID>, JpaSpecificationExecutor {
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     @Override
     Optional<Avtale> findById(UUID id);
 
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     @Override
     List<Avtale> findAllById(Iterable<UUID> ids);
 
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     List<Avtale> findAllByBedriftNr(BedriftNr bedriftNr);
 
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     List<Avtale> findAllByBedriftNrIn(Set<BedriftNr> bedriftNrList);
 
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     List<Avtale> findAllByDeltakerFnr(Fnr deltakerFnr);
 
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     List<Avtale> findAllByVeilederNavIdent(NavIdent veilederNavIdent);
 
 
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     @Query("FROM Avtale "
             + "where veilederNavIdent is null "
             + "and (enhetOppfolging in (?1) or enhetGeografisk in (?1))")
     List<Avtale> findAllUfordelteByEnhet(String navEnhet);
 
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
+    @Query("FROM Avtale "
+            + "where veilederNavIdent in (?1) "
+            + "and (enhetOppfolging in (?2) or enhetGeografisk in (?2))")
+    List<Avtale> findAllfordelteByEnhet(NavIdent ident, String navEnhet);
 
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     @Override
     List<Avtale> findAll();
 
-    @Timed(percentiles = { 0.5d, 0.75d, 0.9d, 0.99d, 0.999d })
+    @Timed(percentiles = {0.5d, 0.75d, 0.9d, 0.99d, 0.999d})
     @Override
     Avtale save(Avtale entity);
 
