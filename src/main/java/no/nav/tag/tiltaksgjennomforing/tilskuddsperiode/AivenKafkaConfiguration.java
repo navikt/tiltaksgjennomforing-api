991613994b83bd710e0e0aee4e0ad45131b914fc
Sindre Dahl Løken <sindre.loken@nav.no>
2021-06-30 14:03:02 +0200

    import rydding Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/tilskuddsperiode/AivenKafkaConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/tilskuddsperiode/AivenKafkaConfiguration.java
@@ -1,26 +1,18 @@
 package no.nav.tag.tiltaksgjennomforing.tilskuddsperiode;
 
-import kafka.tools.ConsoleConsumer;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
