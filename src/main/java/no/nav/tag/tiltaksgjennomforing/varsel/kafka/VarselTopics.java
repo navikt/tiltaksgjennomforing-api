ec57c22079ee6f898d523d6b0d6dc4a0901f6956
Sindre Dahl Løken <sindre.loken@nav.no>
2021-06-30 14:06:33 +0200

    tar bort ubrukt topic og konfigurasjon Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/VarselTopics.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/VarselTopics.java
@@ -2,5 +2,4 @@ package no.nav.tag.tiltaksgjennomforing.varsel.kafka;
 
 public class VarselTopics {
     public static final String TILTAK_VARSEL = "arbeidsgiver.tiltak-varsel";
-    public static final String TILTAK_VARSEL_RESULTAT = "arbeidsgiver.tiltak-varsel-resultat";
 }
