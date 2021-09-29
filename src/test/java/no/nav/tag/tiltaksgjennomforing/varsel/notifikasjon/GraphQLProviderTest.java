ccf3fdf5f406f4937b33c0415f394d4c3181d752
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-09-29 16:00:29 +0200

    WIP
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/test/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/GraphQLProviderTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/GraphQLProviderTest.java
@@ -1,11 +1,25 @@
 package no.nav.tag.tiltaksgjennomforing.varsel.notifikasjon;
 
-
+import graphql.ExecutionInput;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import no.nav.tag.tiltaksgjennomforing.Miljø;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles({ Miljø.LOCAL, "wiremock"})
 public class GraphQLProviderTest {
-
+    @Autowired
+    GraphQLProvider graphQLProvider;
 
 
 
@@ -13,9 +27,17 @@ public class GraphQLProviderTest {
     public void init() {}
 
     @Test
-    public void schemaParserTest() {
+    public void schemaParserTest() throws IOException {
+        graphQLProvider.init();
+        GraphQL build = graphQLProvider.graphQL(); // bookById
+        ExecutionInput executionInput = ExecutionInput.newExecutionInput().query("query { bookById(id: book-1) { id name pageCount } }")
+                .build();
+        ExecutionResult executionResult = build.execute(executionInput);
+        Object data = executionResult.getData();
 
-
+        if(data != null) {
+            System.out.println(data.toString());
+        }
 
     }
 }
