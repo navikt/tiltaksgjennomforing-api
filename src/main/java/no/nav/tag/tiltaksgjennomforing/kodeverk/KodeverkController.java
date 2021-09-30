25e5d504a63157757116d2b9dd382429263c5e41
Mats Bolstad <mbolstad@gmail.com>
2021-09-30 12:51:52 +0200

    Egne endepunkt for status og tiltakstype

--- src/main/java/no/nav/tag/tiltaksgjennomforing/kodeverk/KodeverkController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/kodeverk/KodeverkController.java
@@ -1,5 +1,6 @@
 package no.nav.tag.tiltaksgjennomforing.kodeverk;
 
+import io.micrometer.core.annotation.Timed;
 import no.nav.security.token.support.core.api.Unprotected;
 import no.nav.tag.tiltaksgjennomforing.avtale.Status;
 import no.nav.tag.tiltaksgjennomforing.avtale.Tiltakstype;
@@ -16,11 +17,30 @@ import java.util.stream.Collectors;
 @RestController
 @Unprotected
 @RequestMapping("/kodeverk")
+@Timed
 public class KodeverkController {
+
     @GetMapping
     public Map<String, List<Object>> get() {
         Map<String, List<Object>> map = new HashMap<>();
-        map.put("tiltakstype", Arrays.stream(Tiltakstype.values()).map(t -> {
+        map.put("tiltakstyper", tiltakstyper());
+        map.put("statuser", statuser());
+        return map;
+    }
+
+    @GetMapping("/statuser")
+    public List<Object> statuser() {
+        return Arrays.stream(Status.values()).map(s -> {
+            HashMap<String, String> statusMap = new HashMap<>();
+            statusMap.put("navn", s.name());
+            statusMap.put("beskrivelse", s.getBeskrivelse());
+            return statusMap;
+        }).collect(Collectors.toList());
+    }
+
+    @GetMapping("/tiltakstyper")
+    public List<Object> tiltakstyper() {
+        return Arrays.stream(Tiltakstype.values()).map(t -> {
             HashMap<String, String> tiltakMap = new HashMap<>();
             tiltakMap.put("navn", t.name());
             tiltakMap.put("beskrivelse", t.getBeskrivelse());
@@ -29,15 +49,6 @@ public class KodeverkController {
                 tiltakMap.put("tiltakskodeArena", t.getTiltakskodeArena());
             }
             return tiltakMap;
-        }).collect(Collectors.toList()));
-
-        map.put("status", Arrays.stream(Status.values()).map(s -> {
-            HashMap<String, String> statusMap = new HashMap<>();
-            statusMap.put("navn", s.name());
-            statusMap.put("beskrivelse", s.getBeskrivelse());
-            return statusMap;
-        }).collect(Collectors.toList()));
-
-        return map;
+        }).collect(Collectors.toList());
     }
 }
