70a55cd72895c78d4af3fdcce467712bcbabdd57
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-10-13 21:11:14 +0200

    slettet do kode.

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/request/ArbeidsgiverMutationRequestMedAvtale.java
+++
@@ -1,9 +0,0 @@
-package no.nav.tag.tiltaksgjennomforing.varsel.notifikasjon.request;
-
-import lombok.Value;
-
-@Value
-public class ArbeidsgiverMutationRequestMedAvtale {
-        String query;
-        VariablesMedAvtale variablesMedAvtale;
-}
