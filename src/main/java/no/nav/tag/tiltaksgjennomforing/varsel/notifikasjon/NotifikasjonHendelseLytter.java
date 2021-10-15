fec21b3287fe5e7a0d63deb4dcade833c38df08e
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-10-15 12:57:41 +0200

    fikset conditinal props i application-*.yaml
    Co-authored-by: Millad Dagdoni <millad.dagdoni@nav.no>
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/NotifikasjonHendelseLytter.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/NotifikasjonHendelseLytter.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@ConditionalOnProperty("tiltaksgjennomforing.notifikasjonHendelseLytter.enabled")
+@ConditionalOnProperty("tiltaksgjennomforing.notifikasjoner.enabled")
 public class NotifikasjonHendelseLytter {
 
     private final ArbeidsgiverNotifikasjonRepository arbeidsgiverNotifikasjonRepository;
