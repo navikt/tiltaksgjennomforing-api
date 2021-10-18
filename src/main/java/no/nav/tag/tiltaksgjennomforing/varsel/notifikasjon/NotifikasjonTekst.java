d580a289c089017b5e3fdc5ebf74eba70532a8af
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-10-18 11:03:52 +0200

    oppdatert NotifikasjonTekster
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/NotifikasjonTekst.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/NotifikasjonTekst.java
@@ -1,17 +1,16 @@
 package no.nav.tag.tiltaksgjennomforing.varsel.notifikasjon;
 
 public enum NotifikasjonTekst {
-    AVTALE_OPPRETTET("Ny arbeidstiltak avtale opprettet. Åpne avtale og fyll ut innholdet."),
-    TILTAK_AVTALE_INNGATT("Arbeidstiltak avtale inngått."),
-    TILTAK_AVTALE_KLAR_REFUSJON("Refusjon for arbeidstiltaksavtale er klar."),
-    TILTAK_AVTALE_GODKJENT_VEILEDER("Arbeidstiltaksavtale godkjent av veileder."),
-    TILTAK_STILLINGSBESKRIVELSE_ENDRET("Stillingsbeskrivelse i arbeidstiltak endret av veileder" ),
-    TILTAK_MÅL_ENDRET("Mål i arbeidstiltaksavtale endret av veileder"),
-    TILTAK_OPPFØLGING_OG_TILRETTELEGGING_ENDRET("Oppfølging og tilrettelegging rundt arbeidstiltak endret av veileder"),
-    TILTAK_AVTALE_FORKORTET("Arbeidstiltak avtale forkortet"),
-    TILTAK_AVTALE_FORLENGET("Arbeidstiltak avtale forlenget av veileder"),
-    TILTAK_TILSKUDDSBEREGNING_ENDRET("Tilskuddsberegning i arbeidstiltak endret av veileder"),
-    TILTAK_KONTAKTINFORMASJON_ENDRET("Kontaktinformasjon i arbeidstiltak endret av veileder");
+    AVTALE_OPPRETTET("Ny avtale om arbeidstiltak opprettet. Åpne avtale og fyll ut innholdet."),
+    TILTAK_AVTALE_INNGATT("Avtale om arbeidstiltak godkjent."),
+    TILTAK_AVTALE_KLAR_REFUSJON("Du kan nå søke om refusjon."),
+    TILTAK_STILLINGSBESKRIVELSE_ENDRET("Stillingsbeskrivelse i avtale endret av veileder" ),
+    TILTAK_MÅL_ENDRET("Mål i avtale endret av veileder"),
+    TILTAK_OPPFØLGING_OG_TILRETTELEGGING_ENDRET("Oppfølging og tilrettelegging i avtale endret av veileder"),
+    TILTAK_AVTALE_FORKORTET("Avtale forkortet"),
+    TILTAK_AVTALE_FORLENGET("Avtale forlenget av veileder"),
+    TILTAK_TILSKUDDSBEREGNING_ENDRET("Tilskuddsberegning i avtale endret av veileder"),
+    TILTAK_KONTAKTINFORMASJON_ENDRET("Kontaktinformasjon i avtale endret av veileder");
 
     private final String tekst;
 
