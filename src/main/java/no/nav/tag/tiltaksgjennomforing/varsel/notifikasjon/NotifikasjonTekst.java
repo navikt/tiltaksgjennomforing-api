0ec85d9f7a0719f1bd7c71f7e4bcbbeb1dfb73fa
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-10-21 11:18:45 +0200

    oppdatert Notifikasjontekster
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    Co-authored-by: Sindre Dahl Løken <sindre.loken@nav.no>
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/NotifikasjonTekst.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/NotifikasjonTekst.java
@@ -4,14 +4,14 @@ public enum NotifikasjonTekst {
     TILTAK_AVTALE_OPPRETTET("Ny avtale om arbeidstiltak opprettet. Åpne avtale og fyll ut innholdet."),
     TILTAK_AVTALE_INNGATT("Avtale om arbeidstiltak godkjent."),
     TILTAK_AVTALE_KLAR_REFUSJON("Du kan nå søke om refusjon."),
-    TILTAK_STILLINGSBESKRIVELSE_ENDRET("Stillingsbeskrivelse i avtale endret av veileder" ),
-    TILTAK_MÅL_ENDRET("Mål i avtale endret av veileder"),
-    TILTAK_OPPFØLGING_OG_TILRETTELEGGING_ENDRET("Oppfølging og tilrettelegging i avtale endret av veileder"),
-    TILTAK_AVTALE_FORKORTET("Avtale forkortet"),
-    TILTAK_AVTALE_FORLENGET("Avtale forlenget av veileder"),
-    TILTAK_TILSKUDDSBEREGNING_ENDRET("Tilskuddsberegning i avtale endret av veileder"),
-    TILTAK_GODKJENNINGER_OPPHEVET_AV_VEILEDER("Avtalens godkjenninger er opphevet av veileder"),
-    TILTAK_KONTAKTINFORMASJON_ENDRET("Kontaktinformasjon i avtale endret av veileder");
+    TILTAK_STILLINGSBESKRIVELSE_ENDRET("Stillingsbeskrivelse i avtale endret av veileder."),
+    TILTAK_MÅL_ENDRET("Mål i avtale endret av veileder."),
+    TILTAK_OPPFØLGING_OG_TILRETTELEGGING_ENDRET("Oppfølging og tilrettelegging i avtale endret av veileder."),
+    TILTAK_AVTALE_FORKORTET("Avtale forkortet."),
+    TILTAK_AVTALE_FORLENGET("Avtale forlenget av veileder."),
+    TILTAK_TILSKUDDSBEREGNING_ENDRET("Tilskuddsberegning i avtale endret av veileder."),
+    TILTAK_GODKJENNINGER_OPPHEVET_AV_VEILEDER("Avtalen må godkjennes på nytt."),
+    TILTAK_KONTAKTINFORMASJON_ENDRET("Kontaktinformasjon i avtale endret av veileder.");
 
     private final String tekst;
 
