f8daa09e61e567d84c46ec420f4f0c361e46d56b
Sindre Dahl Løken <sindre.loken@nav.no>
2021-10-25 16:27:11 +0200

    oppdatert docker compose oppsett med manglende abac mapping Co-authored-by: Mats Bolstad <mats.bolstad@nav.no> Co-authored-by: Millad Dagdoni <millad.dagdoni@nav.no>

--- src/test/java/no/nav/tag/tiltaksgjennomforing/DockerComposeTiltaksgjennomforingApplication.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/DockerComposeTiltaksgjennomforingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class DockerComposeTiltaksgjennomforingApplication extends TiltaksgjennomforingApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(TiltaksgjennomforingApplication.class)
-            .profiles("dockercompose", "testdata")
+            .profiles("dockercompose", "testdata", "wiremock")
             .build()
             .run();
     }
