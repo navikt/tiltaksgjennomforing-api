b10a00781516c860520928ac57b6446e0b993972
Millad Dagdoni <millad.dagdoni@nav.no>
2022-01-03 14:46:07 +0100

    endre RefusjonGodkjentMelding.java til å ignorere hele RefusjonGodkjentMelding og kun få med seg avtaleId og tilskuddsperiodeID

--- src/main/java/no/nav/tag/tiltaksgjennomforing/tilskuddsperiode/RefusjonGodkjentMelding.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/tilskuddsperiode/RefusjonGodkjentMelding.java
@@ -1,8 +1,10 @@
 package no.nav.tag.tiltaksgjennomforing.tilskuddsperiode;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.UUID;
 import lombok.Value;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Value
 public class RefusjonGodkjentMelding {
     UUID avtaleId;
