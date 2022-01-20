b90b0edfc13a6590a97cf3247b4bce0fb8d4fd2b
Mats Bolstad <mbolstad@gmail.com>
2022-01-20 10:59:39 +0100

    Deprekerer TilgangskontrollException, den bør ikke brukes lenger

--- src/main/java/no/nav/tag/tiltaksgjennomforing/exceptions/TilgangskontrollException.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/exceptions/TilgangskontrollException.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
+@Deprecated(since = "Bør ikke bruke denne exception lenger fordi det er vanskelig å se i browser hvilken exception som kastes. Det er kun feilkode 403 som vises, og message vises ikke noe sted. Bruk i stedet FeilkodeException.")
 public class TilgangskontrollException extends RuntimeException {
 
     public TilgangskontrollException(String message) {
