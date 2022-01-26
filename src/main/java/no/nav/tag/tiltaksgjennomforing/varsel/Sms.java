4af16a943681e18a0af91d5f75a46974d9ef4a9f
Mats Bolstad <mbolstad@gmail.com>
2022-01-26 12:00:50 +0100

    Ny entitet for SMS-sending, og oppsett for Aiven
    Co-authored-by: Sindre Løken <sindre.loken@nav.no>

---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/Sms.java
@@ -0,0 +1,50 @@
+package no.nav.tag.tiltaksgjennomforing.varsel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.tag.tiltaksgjennomforing.avtale.Identifikator;
+import no.nav.tag.tiltaksgjennomforing.avtale.IdentifikatorConverter;
+import no.nav.tag.tiltaksgjennomforing.utils.Now;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Slf4j
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Sms {
+    @Id
+    private UUID id;
+    private String telefonnummer;
+    @Convert(converter = IdentifikatorConverter.class)
+    private Identifikator identifikator;
+    private String meldingstekst;
+    private UUID avtaleId;
+    private LocalDateTime tidspunkt;
+    @Enumerated(EnumType.STRING)
+    private VarslbarHendelseType hendelseType;
+    private String avsenderApplikasjon;
+
+    public static Sms nyttVarsel(String telefonnummer,
+                                 Identifikator identifikator,
+                                 String meldingstekst,
+                                 VarslbarHendelseType hendelseType, UUID avtaleId) {
+        Sms sms = new Sms();
+        sms.id = UUID.randomUUID();
+        sms.telefonnummer = telefonnummer;
+        sms.identifikator = identifikator;
+        sms.meldingstekst = meldingstekst;
+        sms.hendelseType = hendelseType;
+        sms.tidspunkt = Now.localDateTime();
+        sms.avtaleId = avtaleId;
+        sms.avsenderApplikasjon = "tiltaksgjennomforing-api";
+        return sms;
+    }
+}
