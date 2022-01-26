5442abec1ad171566177e32fe461e86a52ba8734
Sindre Dahl Løken <sindre.loken@nav.no>
2022-01-26 17:19:16 +0100

    Brukte feil jsonSerializer Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/SmsAivenKafkaConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/SmsAivenKafkaConfiguration.java
@@ -6,7 +6,6 @@ import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.connect.json.JsonSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
