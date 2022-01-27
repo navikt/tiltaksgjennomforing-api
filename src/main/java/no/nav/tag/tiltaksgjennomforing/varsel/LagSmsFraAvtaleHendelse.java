5c92f680fe3a40e292b9e1c04d825e18fd7add3d
Sindre Dahl Løken <sindre.loken@nav.no>
2022-01-27 17:08:44 +0100

    Produsering av sms meldinger på ny aiven-kafka topic for alle avtalehendelser. Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/LagSmsFraAvtaleHendelse.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/LagSmsFraAvtaleHendelse.java
@@ -4,7 +4,8 @@ import lombok.RequiredArgsConstructor;
 import no.nav.tag.tiltaksgjennomforing.Miljø;
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtalerolle;
-import no.nav.tag.tiltaksgjennomforing.avtale.events.AvtaleDeltMedAvtalepart;
+import no.nav.tag.tiltaksgjennomforing.avtale.BedriftNr;
+import no.nav.tag.tiltaksgjennomforing.avtale.events.*;
 import no.nav.tag.tiltaksgjennomforing.varsel.kafka.SmsProducer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
@@ -14,10 +15,12 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @ConditionalOnProperty("tiltaksgjennomforing.kafka.enabled")
-@Profile({ Miljø.DEV_FSS, "dockercompose" })
+@Profile({ Miljø.DEV_FSS, "dockercompose", Miljø.LOCAL })
 public class LagSmsFraAvtaleHendelse {
     private final SmsRepository smsRepository;
     private final SmsProducer smsProducer;
+
+    private static final BedriftNr NAV_ORGNR = new BedriftNr("889640782");
 
     @EventListener
     public void avtaleDeltMedAvtalepart(AvtaleDeltMedAvtalepart event) {
@@ -29,6 +32,79 @@ public class LagSmsFraAvtaleHendelse {
             lagreOgSendKafkaMelding(sms);
         }
     }
+    @EventListener
+    public void avtaleGodkjentAvDeltaker(GodkjentAvDeltaker event) {
+        var sms = smsTilVeileder(event.getAvtale(), VarslbarHendelseType.GODKJENT_AV_DELTAKER);
+        lagreOgSendKafkaMelding(sms);
+    }
+    @EventListener
+    public void avtaleGodkjentAvArbeidsgiver(GodkjentAvArbeidsgiver event) {
+        var sms = smsTilVeileder(event.getAvtale(), VarslbarHendelseType.GODKJENT_AV_ARBEIDSGIVER);
+        lagreOgSendKafkaMelding(sms);
+    }
+    @EventListener
+    public void avtaleInngått(AvtaleInngått event) {
+        var smsTilDeltaker = smsTilDeltaker(event.getAvtale(), VarslbarHendelseType.AVTALE_INNGÅTT);
+        var smsTilArbeidsgiver = smsTilArbeidsgiver(event.getAvtale(), VarslbarHendelseType.AVTALE_INNGÅTT);
+        lagreOgSendKafkaMelding(smsTilDeltaker);
+        lagreOgSendKafkaMelding(smsTilArbeidsgiver);
+    }
+    @EventListener
+    public void godkjenningerOpphevetAvArbeidsgiver(GodkjenningerOpphevetAvArbeidsgiver event) {
+        if (event.getGamleVerdier().isGodkjentAvDeltaker()) {
+            var smsTilDeltaker = smsTilDeltaker(event.getAvtale(), VarslbarHendelseType.GODKJENNINGER_OPPHEVET_AV_ARBEIDSGIVER);
+            lagreOgSendKafkaMelding(smsTilDeltaker);
+        }
+        var smsTilVeileder = smsTilVeileder(event.getAvtale(), VarslbarHendelseType.OPPRETTET_AV_ARBEIDSGIVER);
+        lagreOgSendKafkaMelding(smsTilVeileder);
+    }
+    @EventListener
+    public void godkjenningerOpphevetAvVeileder(GodkjenningerOpphevetAvVeileder event) {
+        if (event.getGamleVerdier().isGodkjentAvDeltaker()) {
+            var smsTilDeltaker = smsTilDeltaker(event.getAvtale(), VarslbarHendelseType.GODKJENNINGER_OPPHEVET_AV_VEILEDER);
+            lagreOgSendKafkaMelding(smsTilDeltaker);
+        }
+        if (event.getGamleVerdier().isGodkjentAvArbeidsgiver()) {
+            var smsTilArbeidsgiver = smsTilArbeidsgiver(event.getAvtale(), VarslbarHendelseType.GODKJENNINGER_OPPHEVET_AV_VEILEDER);
+            lagreOgSendKafkaMelding(smsTilArbeidsgiver);
+        }
+    }
+    @EventListener
+    public void refusjonKlar(RefusjonKlar event) {
+        String smsTekst = String.format("Dere kan nå søke om refusjon for tilskudd til sommerjobb for avtale med nr: %s. Frist for å søke er om to måneder. Søk om refusjon her: https://tiltak-refusjon.nav.no. Hilsen NAV.", event.getAvtale().getAvtaleNr());
+        refusjonVarslingMedKontaktperson(event.getAvtale(), smsTekst, VarslbarHendelseType.REFUSJON_KLAR);
+    }
+    @EventListener
+    public void refusjonKlarRevarsel(RefusjonKlarRevarsel event) {
+        String smsTekst = String.format("Fristen nærmer seg for å søke om refusjon for tilskudd til sommerjobb for avtale med nr: %s. Søk om refusjon her: https://tiltak-refusjon.nav.no. Hilsen NAV.", event.getAvtale().getAvtaleNr());
+        refusjonVarslingMedKontaktperson(event.getAvtale(), smsTekst, VarslbarHendelseType.REFUSJON_KLAR_REVARSEL);
+    }
+    @EventListener
+    public void refusjonFristForlenget(RefusjonFristForlenget event) {
+        String smsTekst = String.format("Fristen for å godkjenne refusjon for avtale med nr: %s har blitt forlenget. Du kan sjekke fristen og søke om refusjon her: https://tiltak-refusjon.nav.no. Hilsen NAV.", event.getAvtale().getAvtaleNr());
+        refusjonVarslingMedKontaktperson(event.getAvtale(), smsTekst, VarslbarHendelseType.REFUSJON_FRIST_FORLENGET);
+
+    }
+    @EventListener
+    public void refusjonKorrigert(RefusjonKorrigert event) {
+        String smsTekst = String.format("Tidligere innsendt refusjon på avtale med nr %d er korrigert. Se detaljer her: https://tiltak-refusjon.nav.no. Hilsen NAV.", event.getAvtale().getAvtaleNr());
+        refusjonVarslingMedKontaktperson(event.getAvtale(), smsTekst, VarslbarHendelseType.REFUSJON_KORRIGERT);
+    }
+
+    private void refusjonVarslingMedKontaktperson(Avtale avtale, String smsTekst, VarslbarHendelseType varslbarHendelseType) {
+        if (avtale.getGjeldendeInnhold().getRefusjonKontaktperson() != null && avtale.getGjeldendeInnhold().getRefusjonKontaktperson().getRefusjonKontaktpersonTlf() != null) {
+            if (avtale.getGjeldendeInnhold().getRefusjonKontaktperson().getØnskerVarslingOmRefusjon()) {
+                Sms smsTilArbeidsgiver = Sms.nyttVarsel(avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), avtale.getBedriftNr(), smsTekst, varslbarHendelseType, avtale.getId());
+                lagreOgSendKafkaMelding(smsTilArbeidsgiver);
+            }
+            Sms smsTilKontaktpersonForRefusjon = Sms.nyttVarsel(avtale.getGjeldendeInnhold().getRefusjonKontaktperson().getRefusjonKontaktpersonTlf(), avtale.getBedriftNr(), smsTekst, varslbarHendelseType, avtale.getId());
+            lagreOgSendKafkaMelding(smsTilKontaktpersonForRefusjon);
+        } else {
+            Sms sms = Sms.nyttVarsel(avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), avtale.getBedriftNr(), smsTekst, varslbarHendelseType, avtale.getId());
+            lagreOgSendKafkaMelding(sms);
+        }
+    }
+
 
     private void lagreOgSendKafkaMelding(Sms sms) {
         smsRepository.save(sms);
@@ -41,5 +117,9 @@ public class LagSmsFraAvtaleHendelse {
 
     private static Sms smsTilArbeidsgiver(Avtale avtale, VarslbarHendelseType hendelse) {
         return Sms.nyttVarsel(avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), avtale.getBedriftNr(), "Du har mottatt et nytt varsel på https://arbeidsgiver.nav.no/tiltaksgjennomforing", hendelse, avtale.getId());
+    }
+
+    private static Sms smsTilVeileder(Avtale avtale, VarslbarHendelseType hendelse) {
+        return Sms.nyttVarsel(avtale.getGjeldendeInnhold().getVeilederTlf(), NAV_ORGNR, "Du har mottatt et nytt varsel på https://arbeidsgiver.nais.adeo.no/tiltaksgjennomforing", hendelse, avtale.getId());
     }
 }
---
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/varsel/LagSmsFraAvtaleHendelseTest.java
@@ -0,0 +1,200 @@
+package no.nav.tag.tiltaksgjennomforing.varsel;
+
+import no.nav.tag.tiltaksgjennomforing.Miljø;
+import no.nav.tag.tiltaksgjennomforing.avtale.*;
+import no.nav.tag.tiltaksgjennomforing.varsel.kafka.SmsProducer;
+import no.nav.tag.tiltaksgjennomforing.varsel.kafka.Topics;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(properties = { "tiltaksgjennomforing.kafka.enabled=true" })
+@DirtiesContext
+@ActiveProfiles(Miljø.LOCAL)
+@EmbeddedKafka(partitions = 1, topics = {Topics.SMS_AIVEN})
+class LagSmsFraAvtaleHendelseTest {
+    @Autowired
+    SmsRepository smsRepository;
+    @Autowired
+    AvtaleRepository avtaleRepository;
+    @SpyBean
+    SmsProducer smsProducer;
+
+    private static final String SELVBETJENINGSONE_VARSELTEKST = "Du har mottatt et nytt varsel på https://arbeidsgiver.nav.no/tiltaksgjennomforing";
+    private static final String FAGSYSTEMSONE_VARSELTEKST = "Du har mottatt et nytt varsel på https://arbeidsgiver.nais.adeo.no/tiltaksgjennomforing";
+
+
+
+    @Test
+    void avtaleDeltMedAvtalepart__skal_opprette_sms_til_riktig_mottaker() {
+        Avtale avtale = TestData.enAvtaleMedAltUtfylt();
+        avtale.getGjeldendeInnhold().setArbeidsgiverTlf("41234567");
+        avtale.getGjeldendeInnhold().setDeltakerTlf("42234567");
+        avtale.delMedAvtalepart(Avtalerolle.ARBEIDSGIVER);
+        avtaleRepository.save(avtale);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.DELT_MED_ARBEIDSGIVER, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), SELVBETJENINGSONE_VARSELTEKST);
+        avtale.delMedAvtalepart(Avtalerolle.DELTAKER);
+        avtaleRepository.save(avtale);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.DELT_MED_DELTAKER, avtale.getId(), avtale.getGjeldendeInnhold().getDeltakerTlf(), SELVBETJENINGSONE_VARSELTEKST);
+    }
+
+    @Test
+    void avtaleGodkjent() {
+        Avtale avtale = TestData.enAvtaleMedAltUtfylt();
+        Arbeidsgiver arbeidsgiver = TestData.enArbeidsgiver(avtale);
+        Deltaker deltaker = TestData.enDeltaker(avtale);
+        arbeidsgiver.godkjennAvtale(Instant.now(), avtale);
+        avtaleRepository.save(avtale);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.GODKJENT_AV_ARBEIDSGIVER, avtale.getId(), avtale.getGjeldendeInnhold().getVeilederTlf(), FAGSYSTEMSONE_VARSELTEKST);
+        deltaker.godkjennAvtale(Instant.now(), avtale);
+        avtaleRepository.save(avtale);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.GODKJENT_AV_DELTAKER, avtale.getId(), avtale.getGjeldendeInnhold().getVeilederTlf(), FAGSYSTEMSONE_VARSELTEKST);
+    }
+
+    @Test
+    void avtaleInngått() {
+        Avtale avtale = TestData.enAvtaleMedAltUtfylt();
+        Arbeidsgiver arbeidsgiver = TestData.enArbeidsgiver(avtale);
+        arbeidsgiver.godkjennAvtale(Instant.now(), avtale);
+        Veileder veileder = TestData.enVeileder(avtale);
+        GodkjentPaVegneGrunn godkjentPaVegneGrunn = new GodkjentPaVegneGrunn();
+        godkjentPaVegneGrunn.setIkkeBankId(true);
+        veileder.godkjennForVeilederOgDeltaker(godkjentPaVegneGrunn, avtale);
+        avtaleRepository.save(avtale);
+
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.AVTALE_INNGÅTT, avtale.getId(), avtale.getGjeldendeInnhold().getDeltakerTlf(), SELVBETJENINGSONE_VARSELTEKST);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.AVTALE_INNGÅTT, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), SELVBETJENINGSONE_VARSELTEKST);
+    }
+
+    @Test
+    void godkjenningerOpphevet() {
+        Avtale avtale = TestData.enAvtaleMedAltUtfylt();
+        Veileder veileder = TestData.enVeileder(avtale);
+        Arbeidsgiver arbeidsgiver = TestData.enArbeidsgiver(avtale);
+        Deltaker deltaker = TestData.enDeltaker(avtale);
+        deltaker.godkjennAvtale(Instant.now(), avtale);
+        //Arbeidsgiver opphever deltaker
+        arbeidsgiver.opphevGodkjenninger(avtale);
+        avtaleRepository.save(avtale);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.GODKJENNINGER_OPPHEVET_AV_ARBEIDSGIVER, avtale.getId(), avtale.getGjeldendeInnhold().getDeltakerTlf(), SELVBETJENINGSONE_VARSELTEKST);
+
+        deltaker.godkjennAvtale(Instant.now(), avtale);
+        arbeidsgiver.godkjennAvtale(Instant.now(), avtale);
+        //Veileder opphever arbeidsgiver og deltaker
+        veileder.opphevGodkjenninger(avtale);
+        avtaleRepository.save(avtale);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.GODKJENNINGER_OPPHEVET_AV_VEILEDER, avtale.getId(), avtale.getGjeldendeInnhold().getDeltakerTlf(), SELVBETJENINGSONE_VARSELTEKST);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.GODKJENNINGER_OPPHEVET_AV_VEILEDER, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), SELVBETJENINGSONE_VARSELTEKST);
+    }
+
+    @Test
+    void refusjonKlar() {
+        Avtale avtale = TestData.enSommerjobbAvtale();
+        avtale.getGjeldendeInnhold().setArbeidsgiverTlf("41234567");
+        // I et reelt scenario kan ikke refusjonKlar bli kalt uten at avtalen er godkjent av alle parter+beslutter ++
+        avtale.refusjonKlar();
+        avtaleRepository.save(avtale);
+
+        String meldingstekst = String.format("Dere kan nå søke om refusjon for tilskudd til sommerjobb for avtale med nr: %s. Frist for å søke er om to måneder. Søk om refusjon her: https://tiltak-refusjon.nav.no. Hilsen NAV.", avtale.getAvtaleNr());
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.REFUSJON_KLAR, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), meldingstekst);
+    }
+    @Test
+    void refusjonKlarRevarsel() {
+        Avtale avtale = TestData.enSommerjobbAvtale();
+        avtale.getGjeldendeInnhold().setArbeidsgiverTlf("41234567");
+        // I et reelt scenario kan ikke refusjonRevarsel bli kalt uten at avtalen er godkjent av alle parter+beslutter ++
+        avtale.refusjonRevarsel();
+        avtaleRepository.save(avtale);
+        String meldingstekst = String.format("Fristen nærmer seg for å søke om refusjon for tilskudd til sommerjobb for avtale med nr: %s. Søk om refusjon her: https://tiltak-refusjon.nav.no. Hilsen NAV.", avtale.getAvtaleNr());
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.REFUSJON_KLAR_REVARSEL, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), meldingstekst);
+    }
+    @Test
+    void refusjonFristForlenget() {
+        Avtale avtale = TestData.enSommerjobbAvtale();
+        avtale.getGjeldendeInnhold().setArbeidsgiverTlf("41234567");
+        // I et reelt scenario kan ikke refusjonFristForlenget bli kalt uten at avtalen er godkjent av alle parter+beslutter ++
+        avtale.refusjonFristForlenget();
+        avtaleRepository.save(avtale);
+        String meldingstekst = String.format("Fristen for å godkjenne refusjon for avtale med nr: %s har blitt forlenget. Du kan sjekke fristen og søke om refusjon her: https://tiltak-refusjon.nav.no. Hilsen NAV.", avtale.getAvtaleNr());
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.REFUSJON_FRIST_FORLENGET, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), meldingstekst);
+    }
+    @Test
+    void refusjonKorrigert() {
+        Avtale avtale = TestData.enSommerjobbAvtale();
+        avtale.getGjeldendeInnhold().setArbeidsgiverTlf("41234567");
+        // I et reelt scenario kan ikke refusjonKorrigert bli kalt uten at avtalen er godkjent av alle parter+beslutter ++
+        avtale.refusjonKorrigert();
+        avtaleRepository.save(avtale);
+        String meldingstekst = String.format("Tidligere innsendt refusjon på avtale med nr %d er korrigert. Se detaljer her: https://tiltak-refusjon.nav.no. Hilsen NAV.", avtale.getAvtaleNr());
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.REFUSJON_KORRIGERT, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), meldingstekst);
+    }
+
+
+    @Test
+    void refusjonKorrigertKontaktperson__begge_skal_få_sms() {
+        Avtale avtale = TestData.enSommerjobbAvtale();
+        avtale.getGjeldendeInnhold().setArbeidsgiverTlf("41234567");
+        RefusjonKontaktperson refusjonKontaktperson = new RefusjonKontaktperson("Per", "Persen", "49876543", true);
+        avtale.getGjeldendeInnhold().setRefusjonKontaktperson(refusjonKontaktperson);
+        // I et reelt scenario kan ikke refusjonKorrigert bli kalt uten at avtalen er godkjent av alle parter+beslutter ++
+        avtale.refusjonKorrigert();
+        avtaleRepository.save(avtale);
+        String meldingstekst = String.format("Tidligere innsendt refusjon på avtale med nr %d er korrigert. Se detaljer her: https://tiltak-refusjon.nav.no. Hilsen NAV.", avtale.getAvtaleNr());
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.REFUSJON_KORRIGERT, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), meldingstekst);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.REFUSJON_KORRIGERT, avtale.getId(), avtale.getGjeldendeInnhold().getRefusjonKontaktperson().getRefusjonKontaktpersonTlf(), meldingstekst);
+    }
+
+    @Test
+    void refusjonKorrigertKontaktperson__bare_kontaktperson_skal_få_sms() {
+        Avtale avtale = TestData.enSommerjobbAvtale();
+        avtale.getGjeldendeInnhold().setArbeidsgiverTlf("41234567");
+        RefusjonKontaktperson refusjonKontaktperson = new RefusjonKontaktperson("Per", "Persen", "49876543", false);
+        avtale.getGjeldendeInnhold().setRefusjonKontaktperson(refusjonKontaktperson);
+        // I et reelt scenario kan ikke refusjonKorrigert bli kalt uten at avtalen er godkjent av alle parter+beslutter ++
+        avtale.refusjonKorrigert();
+        avtaleRepository.save(avtale);
+        String meldingstekst = String.format("Tidligere innsendt refusjon på avtale med nr %d er korrigert. Se detaljer her: https://tiltak-refusjon.nav.no. Hilsen NAV.", avtale.getAvtaleNr());
+        assertSmsIkkeOpprettetEllerSendt(VarslbarHendelseType.REFUSJON_KORRIGERT, avtale.getId(), avtale.getGjeldendeInnhold().getArbeidsgiverTlf(), meldingstekst);
+        assertSmsOpprettetOgSendt(VarslbarHendelseType.REFUSJON_KORRIGERT, avtale.getId(), avtale.getGjeldendeInnhold().getRefusjonKontaktperson().getRefusjonKontaktpersonTlf(), meldingstekst);
+    }
+
+    private void assertSmsOpprettetOgSendt(VarslbarHendelseType varslbarHendelseType, UUID avtaleId, String telefonnummer, String meldingstekst) {
+        assertThat(smsRepository.findAll())
+                .filteredOn(sms -> sms.getHendelseType() == varslbarHendelseType
+                        && sms.getAvtaleId().equals(avtaleId)
+                        && sms.getTelefonnummer().equals(telefonnummer)
+                        && sms.getMeldingstekst().equals(meldingstekst))
+                .hasSize(1);
+        verify(smsProducer).sendSmsVarselMeldingTilKafka(argThat((Sms sms) ->
+                sms.getAvtaleId().equals(avtaleId)
+                && sms.getHendelseType().equals(varslbarHendelseType)
+                && sms.getTelefonnummer().equals(telefonnummer)
+                && sms.getMeldingstekst().equals(meldingstekst)));
+    }
+
+    private void assertSmsIkkeOpprettetEllerSendt(VarslbarHendelseType varslbarHendelseType, UUID avtaleId, String telefonnummer, String meldingstekst) {
+        assertThat(smsRepository.findAll())
+                .filteredOn(sms -> sms.getHendelseType() == varslbarHendelseType
+                        && sms.getAvtaleId().equals(avtaleId)
+                        && sms.getTelefonnummer().equals(telefonnummer)
+                        && sms.getMeldingstekst().equals(meldingstekst))
+                .hasSize(0);
+        verify(smsProducer, never()).sendSmsVarselMeldingTilKafka(argThat((Sms sms) ->
+                sms.getAvtaleId().equals(avtaleId)
+                        && sms.getHendelseType().equals(varslbarHendelseType)
+                        && sms.getTelefonnummer().equals(telefonnummer)
+                        && sms.getMeldingstekst().equals(meldingstekst)));
+    }
+}
