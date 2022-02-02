b772fa59e84b4f9b20a51d68b11a63798dbdf395
Millad Dagdoni <millad.dagdoni@nav.no>
2022-02-02 09:13:25 +0100

    sommerjobb 2022

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/startOgSluttDatoStrategy/SommerjobbStartOgSluttDatoStrategy.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/startOgSluttDatoStrategy/SommerjobbStartOgSluttDatoStrategy.java
@@ -1,27 +1,32 @@
 package no.nav.tag.tiltaksgjennomforing.avtale.startOgSluttDatoStrategy;
 
-import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
+import java.time.LocalDate;
 import no.nav.tag.tiltaksgjennomforing.exceptions.Feilkode;
 import no.nav.tag.tiltaksgjennomforing.exceptions.FeilkodeException;
-
-import java.time.LocalDate;
 
 public class SommerjobbStartOgSluttDatoStrategy implements StartOgSluttDatoStrategy {
     @Override
     public void sjekkStartOgSluttDato(LocalDate startDato, LocalDate sluttDato, boolean erGodkjentForEtterregistrering, boolean erAvtaleInngått) {
         StartOgSluttDatoStrategy.super.sjekkStartOgSluttDato(startDato, sluttDato, erGodkjentForEtterregistrering, erAvtaleInngått);
         if (startDato != null) {
-            if (startDato.isBefore(LocalDate.of(2021, 6, 1)) ) {
-                throw new FeilkodeException(Feilkode.SOMMERJOBB_FOR_TIDLIG);
-            }
-            if (startDato.isAfter(LocalDate.of(2021, 8, 31))) {
-                throw new FeilkodeException(Feilkode.SOMMERJOBB_FOR_SENT);
-            }
+                if (startDato.isBefore(LocalDate.of(startDato.getYear(), 6, 1)) ) {
+                    throw new FeilkodeException(Feilkode.SOMMERJOBB_FOR_TIDLIG);
+                }
+                if (startDato.isAfter(LocalDate.of(startDato.getYear(), 8, 31))) {
+                    throw new FeilkodeException(Feilkode.SOMMERJOBB_FOR_SENT);
+                }
         }
-        if (startDato != null && sluttDato != null) {
-            if (startDato.plusWeeks(4).isBefore(sluttDato)) {
-                throw new FeilkodeException(Feilkode.SOMMERJOBB_FOR_LANG_VARIGHET);
-            }
+      if (startDato != null && sluttDato != null) {
+        if (startDato.plusWeeks(4).isBefore(sluttDato)) {
+          throw new FeilkodeException(Feilkode.SOMMERJOBB_FOR_LANG_VARIGHET);
+        }else{
+          if (sluttDato.isBefore(LocalDate.of(sluttDato.getYear(), 6, 1)) ) {
+            throw new FeilkodeException(Feilkode.SOMMERJOBB_FOR_TIDLIG);
+          }
+          if (sluttDato.isAfter(LocalDate.of(sluttDato.getYear(), 8, 31))) {
+            throw new FeilkodeException(Feilkode.SOMMERJOBB_FOR_SENT);
+          }
         }
+      }
     }
 }
