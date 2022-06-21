f3396a3768952baa58cd55dd97efebc5029261d6
Mattias Lundmark <lundmark.mattias@gmail.com>
2022-06-21 14:23:11 +0200

    Strategy for å lagre inkluderingstilskudd og liste av de

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/InkluderingsTiltakStrategy.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/InkluderingsTiltakStrategy.java
@@ -8,14 +8,12 @@ public class InkluderingsTiltakStrategy extends BaseAvtaleInnholdStrategy {
     public InkluderingsTiltakStrategy(AvtaleInnhold avtaleInnhold){
         super(avtaleInnhold);
     }
-/*
+
     @Override
     public void endre(EndreAvtale nyAvtale) {
-        avtaleInnhold.setMentorFornavn(nyAvtale.getMentorFornavn());
-        avtaleInnhold.setMentorEtternavn(nyAvtale.getMentorEtternavn());
-        avtaleInnhold.setMentorOppgaver(nyAvtale.getMentorOppgaver());
-        avtaleInnhold.setMentorAntallTimer(nyAvtale.getMentorAntallTimer());
-        avtaleInnhold.setMentorTimelonn(nyAvtale.getMentorTimelonn());
+        avtaleInnhold.getInkluderingstilskudd().clear();
+        avtaleInnhold.getInkluderingstilskudd().addAll(nyAvtale.getInkluderingstilskudd());
+        avtaleInnhold.getInkluderingstilskudd().forEach(i -> i.setAvtaleInnhold(avtaleInnhold));
         super.endre(nyAvtale);
     }
 
@@ -23,13 +21,8 @@ public class InkluderingsTiltakStrategy extends BaseAvtaleInnholdStrategy {
     public Map<String, Object> alleFelterSomMåFyllesUt() {
         var alleFelter = new HashMap<String, Object>();
         alleFelter.putAll(super.alleFelterSomMåFyllesUt());
-        alleFelter.put(AvtaleInnhold.Fields.mentorFornavn, avtaleInnhold.getMentorFornavn());
-        alleFelter.put(AvtaleInnhold.Fields.mentorEtternavn, avtaleInnhold.getMentorEtternavn());
-        alleFelter.put(AvtaleInnhold.Fields.mentorOppgaver, avtaleInnhold.getMentorOppgaver());
-        alleFelter.put(AvtaleInnhold.Fields.mentorAntallTimer, avtaleInnhold.getMentorAntallTimer());
-        alleFelter.put(AvtaleInnhold.Fields.mentorTimelonn, avtaleInnhold.getMentorTimelonn());
+        alleFelter.put(AvtaleInnhold.Fields.inkluderingstilskudd, avtaleInnhold.getInkluderingstilskudd());
         return alleFelter;
     }
 
- */
 }
