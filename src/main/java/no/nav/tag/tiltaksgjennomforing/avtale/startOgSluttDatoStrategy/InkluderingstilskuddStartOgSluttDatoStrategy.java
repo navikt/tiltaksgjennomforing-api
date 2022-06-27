8184df4934152f0909bd216fd1b5e01f5bddab3f
Sindre Dahl Løken <sindre.loken@nav.no>
2022-06-27 10:03:08 +0200

    Egen feilkode for for lang varighet på inkluderingstilskudd

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/startOgSluttDatoStrategy/InkluderingstilskuddStartOgSluttDatoStrategy.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/startOgSluttDatoStrategy/InkluderingstilskuddStartOgSluttDatoStrategy.java
@@ -1,15 +1,16 @@
 package no.nav.tag.tiltaksgjennomforing.avtale.startOgSluttDatoStrategy;
 
-import no.nav.tag.tiltaksgjennomforing.exceptions.VarighetForLangMentorException;
+import no.nav.tag.tiltaksgjennomforing.exceptions.Feilkode;
+import no.nav.tag.tiltaksgjennomforing.exceptions.FeilkodeException;
 
 import java.time.LocalDate;
 
-public class InkluderingstilskuddStartOgSluttDatoStrategy implements StartOgSluttDatoStrategy  {
+public class InkluderingstilskuddStartOgSluttDatoStrategy implements StartOgSluttDatoStrategy {
     @Override
     public void sjekkStartOgSluttDato(LocalDate startDato, LocalDate sluttDato, boolean erGodkjentForEtterregistrering, boolean erAvtaleInngått) {
         StartOgSluttDatoStrategy.super.sjekkStartOgSluttDato(startDato, sluttDato, erGodkjentForEtterregistrering, erAvtaleInngått);
         if (startDato != null && sluttDato != null && startDato.plusMonths(12).isBefore(sluttDato)) {
-            throw new VarighetForLangMentorException();
+            throw new FeilkodeException(Feilkode.VARIGHET_FOR_LANG_INKLUDERINGSTILSKUDD);
         }
     }
 }
