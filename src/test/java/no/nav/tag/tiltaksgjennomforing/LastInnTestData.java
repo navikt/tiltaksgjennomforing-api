14a29053ab26c63f3a3eae146c6a7a14a262c8a1
millad <milladd@gmail.com>
2022-07-01 10:21:19 +0200

    lagt til en mentor avtale i LastInnTestData
    Co-authored-by: Magnus Rom <magnus.skalberg.rom@nav.no>

--- src/test/java/no/nav/tag/tiltaksgjennomforing/LastInnTestData.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/LastInnTestData.java
@@ -25,6 +25,7 @@ public class LastInnTestData implements ApplicationListener<ApplicationReadyEven
 
         log.info("Laster testdata");
         avtaleRepository.save(TestData.enArbeidstreningAvtale());
+        avtaleRepository.save(TestData.enMentorAvtale());
         avtaleRepository.save(TestData.enAvtaleMedAltUtfylt());
         avtaleRepository.save(TestData.enAvtaleMedAltUtfyltGodkjentAvVeileder());
         avtaleRepository.save(TestData.enAvtaleMedFlereVersjoner());
