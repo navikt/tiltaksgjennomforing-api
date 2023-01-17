72e71ee69ddb8aa4acafa0bc5656c513e6fbfd41
Mattias Lundmark <lundmark.mattias@gmail.com>
2023-01-17 12:50:29 +0100

    Sender alle hendelser til Den Store Topicen

--- src/main/java/no/nav/tag/tiltaksgjennomforing/datadeling/AvtaleHendelseController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/datadeling/AvtaleHendelseController.java
@@ -1,80 +1,49 @@
 package no.nav.tag.tiltaksgjennomforing.datadeling;
 
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.github.victools.jsonschema.generator.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.security.token.support.core.api.ProtectedWithClaims;
 import no.nav.security.token.support.core.api.Unprotected;
 import no.nav.tag.tiltaksgjennomforing.autorisasjon.TokenUtils;
-import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
-import no.nav.tag.tiltaksgjennomforing.avtale.AvtaleRepository;
-import no.nav.tag.tiltaksgjennomforing.avtale.Fnr;
-import no.nav.tag.tiltaksgjennomforing.exceptions.RessursFinnesIkkeException;
+import no.nav.tag.tiltaksgjennomforing.autorisasjon.UtviklerTilgangProperties;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
-@RequestMapping("/internal/avtale-hendelse")
+@RequestMapping("/utvikler-admin/avtale-hendelse")
 @RequiredArgsConstructor
-@ProtectedWithClaims(issuer = "aad")
+//@ProtectedWithClaims(issuer = "aad")
+@Unprotected
 @Slf4j
 public class AvtaleHendelseController {
 
-    private final AvtaleMeldingEntitetRepository avtaleMeldingEntitetRepository;
-    private final AvtaleRepository avtaleRepository;
-
+    private final UtviklerTilgangProperties utviklerTilgangProperties;
     private final TokenUtils tokenUtils;
-
+    private final AvtaleHendelseService avtaleHendelseService;
     private void sjekkTilgang() {
-        if (!tokenUtils.harAdRolle("access_as_application")) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        if (!tokenUtils.harAdGruppe(utviklerTilgangProperties.getGruppeTilgang())) {
+            //throw new HttpClientErrorException(HttpStatus.FORBIDDEN);
         }
     }
 
-    @PostMapping("/fnr")
-    public List<String> hentSisteHendelseForFnr(@RequestBody AvtaleMeldingForFnr meldingForFnr) {
+    @PostMapping("dry-send-melding-alle-avtaler")
+    @Transactional
+    public void drySendMeldingAlleAvtaler() {
+        log.info("DRY - Sender alle avtaler som hendelsemeldinger på topic");
         sjekkTilgang();
-        List<String> hendelser = new ArrayList<>();
-        if(Fnr.erGyldigFnr(meldingForFnr.fnr)) {
-            List<Avtale> alleAvtalerForDeltaker = avtaleRepository.findAllByDeltakerFnr(new Fnr(meldingForFnr.fnr));
-            alleAvtalerForDeltaker.forEach(avtale -> {
-                List<AvtaleMeldingEntitet> avtaleMeldingEntiteter = avtaleMeldingEntitetRepository.findAllByAvtaleId(avtale.getId());
-                AvtaleMeldingEntitet avtaleMeldingEntitet = avtaleMeldingEntiteter.stream().max(Comparator.comparing(melding -> melding.getTidspunkt())).orElseGet(null);
-                if(avtaleMeldingEntitet != null) {
-                    hendelser.add(avtaleMeldingEntitet.getJson());
-                }
-            });
-        }
-
-        return hendelser;
-    }
-
-    @GetMapping("/{avtaleId}")
-    public String hentSisteHendelse(@PathVariable("avtaleId") UUID avtaleId) {
-        List<AvtaleMeldingEntitet> avtaleMeldingEntiteter = avtaleMeldingEntitetRepository.findAllByAvtaleId(avtaleId);
-        AvtaleMeldingEntitet avtaleMeldingEntitet = avtaleMeldingEntiteter.stream().max(Comparator.comparing(melding -> melding.getTidspunkt())).orElseThrow(RessursFinnesIkkeException::new);
-
-        return avtaleMeldingEntitet.getJson();
-    }
-
-    @GetMapping("/skjema")
-    @Unprotected
-    public String hentJsonSkjema() {
-        SchemaGeneratorConfigBuilder configBuilder = new SchemaGeneratorConfigBuilder(SchemaVersion.DRAFT_2020_12, OptionPreset.PLAIN_JSON)
-                .without(Option.DEFINITIONS_FOR_ALL_OBJECTS);
-        SchemaGeneratorConfig config = configBuilder.build();
-        SchemaGenerator generator = new SchemaGenerator(config);
-        JsonNode jsonSchema = generator.generateSchema(AvtaleMelding.class);
-
-        return jsonSchema.toPrettyString();
+        avtaleHendelseService.sendAvtaleHendelseMeldingPåAlleAvtalerDRYRun();
     }
 
 
-    private record AvtaleMeldingForFnr(String fnr) { }
-
+    @PostMapping("send-melding-alle-avtaler")
+    @Transactional
+    public void sendMeldingAlleAvtaler() {
+        log.info("Sender alle avtaler som hendelsemeldinger på topic");
+        sjekkTilgang();
+        avtaleHendelseService.sendAvtaleHendelseMeldingPåAlleAvtaler();
+    }
 }
---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/datadeling/AvtaleHendelseService.java
@@ -0,0 +1,94 @@
+package no.nav.tag.tiltaksgjennomforing.datadeling;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
+import no.nav.tag.tiltaksgjennomforing.avtale.AvtaleRepository;
+import no.nav.tag.tiltaksgjennomforing.avtale.HendelseType;
+import no.nav.tag.tiltaksgjennomforing.utils.Now;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AvtaleHendelseService {
+
+    private final AvtaleRepository avtaleRepository;
+    private final AvtaleMeldingEntitetRepository avtaleMeldingEntitetRepository;
+
+    private final ObjectMapper objectMapper;
+
+    @Async
+    public void sendAvtaleHendelseMeldingPåAlleAvtaler() {
+        AtomicInteger antallSendt = new AtomicInteger();
+
+        log.info("Henter ALLE avtaler for å lage patchehendelsemeldinger");
+        List<Avtale> alleAvtaler = avtaleRepository.findAll(); // /o\ \o/
+        log.info("Alle avtaler er hentet, looper og sender hendelsemeldinger");
+
+        alleAvtaler.forEach(avtale -> {
+            if(skalSendes(avtale)) {
+                lagMelding(avtale);
+                antallSendt.getAndIncrement();
+            }
+        });
+        log.info("Sendt totalt {} antall hendelsemeldinger", antallSendt.get());
+    }
+
+    @Async
+    public void sendAvtaleHendelseMeldingPåAlleAvtalerDRYRun() {
+        AtomicInteger antallSendt = new AtomicInteger();
+
+        log.info("DRY RUN - Henter ALLE avtaler for å lage patchehendelsemeldinger");
+        List<Avtale> alleAvtaler = avtaleRepository.findAll(); // /o\ \o/
+        log.info("DRY RUN - Alle avtaler er hentet, looper og sender hendelsemeldinger");
+
+        alleAvtaler.forEach(avtale -> {
+            if(skalSendes(avtale)) {
+                lagMeldingDRYRun(avtale);
+                antallSendt.getAndIncrement();
+            }
+        });
+        log.info("DRY RUN - Sendt totalt {} antall hendelsemeldinger", antallSendt.get());
+    }
+
+    // Skal alt sendes egentlig?
+    private boolean skalSendes(Avtale avtale) {
+        return true;
+    }
+
+    private void lagMelding(Avtale avtale) {
+        var melding = AvtaleMelding.create(avtale, avtale.getGjeldendeInnhold(), null, AvtaleHendelseUtførtAvRolle.SYSTEM, HendelseType.STATUSENDRING);
+        UUID meldingId = UUID.randomUUID();
+        LocalDateTime tidspunkt = Now.localDateTime();
+        try {
+            String meldingSomString = objectMapper.writeValueAsString(melding);
+            AvtaleMeldingEntitet entitet = new AvtaleMeldingEntitet(meldingId, avtale.getId(), tidspunkt, HendelseType.STATUSENDRING, avtale.statusSomEnum(), meldingSomString);
+            avtaleMeldingEntitetRepository.save(entitet);
+        } catch (JsonProcessingException e) {
+            log.error("Feil ved parsing av AvtaleHendelseMelding til json for avtale med id: {}", avtale.getId());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void lagMeldingDRYRun(Avtale avtale) {
+        var melding = AvtaleMelding.create(avtale, avtale.getGjeldendeInnhold(), null, AvtaleHendelseUtførtAvRolle.SYSTEM, HendelseType.STATUSENDRING);
+        try {
+            String meldingSomString = objectMapper.writeValueAsString(melding);
+            if(meldingSomString.length() != 0) {
+                log.info("Melding ble generert");
+            }
+        } catch (JsonProcessingException e) {
+            log.error("Feil ved parsing av AvtaleHendelseMelding til json for avtale med id: {}", avtale.getId());
+            throw new RuntimeException(e);
+        }
+    }
+}
