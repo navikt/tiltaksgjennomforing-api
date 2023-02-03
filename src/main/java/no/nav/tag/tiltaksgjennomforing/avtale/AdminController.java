456ecdb139a51e78c962c926d75b221d60d9f0f0
Sindre Dahl Løken <sindre.loken@nav.no>
2023-02-03 15:43:34 +0100

    kode som filtrerer bort annullert, avbrutt og avtaler som er kortere enn 12mnd

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AdminController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AdminController.java
@@ -32,30 +32,49 @@ public class AdminController {
     @PostMapping("/reberegn-mangler-dato-for-redusert-prosent/{migreringsDato}")
     @Transactional
     public void reberegnVarigLønnstilskuddSomIkkeHarRedusertDato(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate migreringsDato) {
-        // 1. Generer dato for redusert prosent og sumRedusert
-        List<Avtale> varigeLønnstilskudd = avtaleRepository.findAllByTiltakstypeAndGjeldendeInnhold_DatoForRedusertProsentNullAndGjeldendeInnhold_AvtaleInngåttNotNull(Tiltakstype.VARIG_LONNSTILSKUDD);
-        log.info("Fant {} varige lønnstilskudd avtaler som mangler redusert prosent til fiksing.", varigeLønnstilskudd.size());
-        varigeLønnstilskudd.forEach(avtale -> {
-            if (avtale.getGjeldendeInnhold().getLonnstilskuddProsent() > 67) {
-                avtale.reUtregnRedusert();
-                avtale.nyeTilskuddsperioderEtterMigreringFraArena(migreringsDato, false);
-                avtaleRepository.save(avtale);
-            }
-        });
-    }
-
-    @PostMapping("/reberegn-mangler-dato-for-redusert-prosent-dry-run/{migreringsDato}")
-    public void reberegnVarigLønnstilskuddSomIkkeHarRedusertDatoDryRun(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate migreringsDato) {
+        log.info("Starter jobb for å fikse manglende redusert prosent og redusert sum");
         // 1. Generer dato for redusert prosent og sumRedusert
         List<Avtale> varigeLønnstilskudd = avtaleRepository.findAllByTiltakstypeAndGjeldendeInnhold_DatoForRedusertProsentNullAndGjeldendeInnhold_AvtaleInngåttNotNull(Tiltakstype.VARIG_LONNSTILSKUDD);
         log.info("Fant {} varige lønnstilskudd avtaler som mangler redusert prosent til fiksing.", varigeLønnstilskudd.size());
         AtomicInteger antallUnder67 = new AtomicInteger();
         varigeLønnstilskudd.forEach(avtale -> {
-            if (avtale.getGjeldendeInnhold().getLonnstilskuddProsent() > 67) {
+            LocalDate startDato = avtale.getGjeldendeInnhold().getStartDato();
+            LocalDate sluttDato = avtale.getGjeldendeInnhold().getSluttDato();
+            if (avtale.getGjeldendeInnhold().getLonnstilskuddProsent() > 67
+                    && startDato.isBefore(sluttDato.minusMonths(12))
+                    && avtale.getAnnullertTidspunkt() == null
+                    && avtale.getAvbruttGrunn() == null
+                    && avtale.getGjeldendeInnhold().getSumLonnstilskudd() != null) {
+
+                avtale.reUtregnRedusert();
+                avtale.nyeTilskuddsperioderEtterMigreringFraArena(migreringsDato, false);
+                avtaleRepository.save(avtale);
                 antallUnder67.getAndIncrement();
             }
         });
-        log.info("Fant {} avtaler som vil bli kjørt fiksing av redusert sum og sats på", antallUnder67.get());
+        log.info("Ferdig kjørt reberegning av fiks for manglende redusert prosent og redusert sum på {} avtaler", antallUnder67);
+    }
+
+    @PostMapping("/reberegn-mangler-dato-for-redusert-prosent-dry-run/{migreringsDato}")
+    public void reberegnVarigLønnstilskuddSomIkkeHarRedusertDatoDryRun(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate migreringsDato) {
+        log.info("DRY-RUN: Starter DRY-RUN jobb for å fikse manglende redusert prosent og redusert sum");
+        // 1. Generer dato for redusert prosent og sumRedusert
+        List<Avtale> varigeLønnstilskudd = avtaleRepository.findAllByTiltakstypeAndGjeldendeInnhold_DatoForRedusertProsentNullAndGjeldendeInnhold_AvtaleInngåttNotNull(Tiltakstype.VARIG_LONNSTILSKUDD);
+        log.info("DRY-RUN: Fant {} varige lønnstilskudd avtaler som mangler redusert prosent til fiksing.", varigeLønnstilskudd.size());
+        AtomicInteger antallUnder67 = new AtomicInteger();
+        varigeLønnstilskudd.forEach(avtale -> {
+            LocalDate startDato = avtale.getGjeldendeInnhold().getStartDato();
+            LocalDate sluttDato = avtale.getGjeldendeInnhold().getSluttDato();
+
+            if (avtale.getGjeldendeInnhold().getLonnstilskuddProsent() > 67
+                    && startDato.isBefore(sluttDato.minusMonths(12))
+                    && avtale.getAnnullertTidspunkt() == null
+                    && avtale.getAvbruttGrunn() == null
+                    && avtale.getGjeldendeInnhold().getSumLonnstilskudd() != null) {
+                antallUnder67.getAndIncrement();
+            }
+        });
+        log.info("DRY-RUN: Fant {} avtaler som vil bli kjørt fiksing av redusert sum og sats på", antallUnder67.get());
     }
 
 
