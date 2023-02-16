193e3386968f1088545a3b0434ac34b8425dacc7
Sindre Dahl Løken <sindre.loken@nav.no>
2023-02-16 15:33:01 +0100

    endrer loggmelding i statusendring jobb slik at den viser riktig hvilken status avtalen hadde

--- src/main/java/no/nav/tag/tiltaksgjennomforing/datadeling/AvtaleHendelseStatusendringJobb.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/datadeling/AvtaleHendelseStatusendringJobb.java
@@ -53,7 +53,7 @@ public class AvtaleHendelseStatusendringJobb {
                     AvtaleMeldingEntitet entitet = new AvtaleMeldingEntitet(UUID.randomUUID(), avtaleId, tidspunkt, HendelseType.STATUSENDRING, avtale.statusSomEnum(), meldingSomString);
                     avtaleMeldingEntitetRepository.save(entitet);
                     log.info("Avtale med id {} har byttet status til {}, siste melding har status {}, så sender melding med den nye statusen på topic {}",
-                            avtale.getId(), avtale.statusSomEnum(), entitet.getAvtaleStatus(), Topics.AVTALE_HENDELSE);
+                            avtale.getId(), avtale.statusSomEnum(), avtaleMeldingEntitet.getAvtaleStatus(), Topics.AVTALE_HENDELSE);
                     antallNyeMeldinger++;
                 } catch (JsonProcessingException e) {
                     log.error("Feil ved parsing av AvtaleHendelseMelding i statusendringjobb til json for hendelse med avtaleId {}", avtaleMelding.getAvtaleId());
