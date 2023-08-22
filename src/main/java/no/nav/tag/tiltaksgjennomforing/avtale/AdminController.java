05d2cf07ca493c261ee4bec735a5ca34e28269e4
Sindre Dahl Løken <sindre.loken@nav.no>
2023-08-22 12:23:44 +0200

    forbedret logging

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AdminController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AdminController.java
@@ -135,15 +135,17 @@ public class AdminController {
     @Transactional
     public void annullerOgGenererBehandletIArenaPerioder(@PathVariable("avtaleId") UUID avtaleId, @PathVariable("dato") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dato) {
         sjekkTilgang();
-        log.info("Annullerer behandlet i arena perioder på avtale {}", avtaleId);
+        log.info("Annullerer tilskuddsperioder med sluttdato før {} på avtale {} og lager nye med status behandlet i arena", dato, avtaleId);
         Avtale avtale = avtaleRepository.findById(avtaleId).orElseThrow(RessursFinnesIkkeException::new);
         List<TilskuddPeriode> tilskuddsperioder = tilskuddPeriodeRepository.findAllByAvtaleAndSluttDatoBefore(avtale, dato);
+        log.info("Fant {} tilskuddsperioder som skal annulleres og genereres på nytt med behandlet i arena status", tilskuddsperioder.size());
 
         tilskuddsperioder.stream().toList().forEach(tp -> {
             avtale.annullerTilskuddsperiode(tp);
             avtale.lagNyBehandletIArenaTilskuddsperiodeFraAnnullertPeriode(tp);
         });
 
+        log.info("Avtale {} har nå {} perioder med status behandlet i arena", avtale, avtale.getTilskuddPeriode().stream().filter(tp -> tp.getStatus() == TilskuddPeriodeStatus.BEHANDLET_I_ARENA).count());
         avtaleRepository.save(avtale);
     }
 
