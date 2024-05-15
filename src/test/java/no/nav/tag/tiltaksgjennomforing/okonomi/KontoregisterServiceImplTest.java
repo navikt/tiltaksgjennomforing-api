92effb450237f4a9bbe83b455f53a03e37bbed23
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-05-15 10:03:36 +0200

    CorrelationIdSupplier må sette en id
    Ellers feiler tester

--- src/test/java/no/nav/tag/tiltaksgjennomforing/okonomi/KontoregisterServiceImplTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/okonomi/KontoregisterServiceImplTest.java
@@ -6,6 +6,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import no.nav.tag.tiltaksgjennomforing.Miljø;
 import no.nav.tag.tiltaksgjennomforing.exceptions.KontoregisterFantIkkeBedriftFeilException;
 import no.nav.tag.tiltaksgjennomforing.exceptions.KontoregisterFeilException;
+import no.nav.tag.tiltaksgjennomforing.infrastruktur.CorrelationIdSupplier;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,11 +16,21 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles({ Miljø.LOCAL, "wiremock"})
+@ActiveProfiles({Miljø.LOCAL, "wiremock"})
 @DirtiesContext
 public class KontoregisterServiceImplTest {
     @Autowired
     private KontoregisterService KontoregisterService;
+
+    @BeforeAll
+    public static void beforeTests() {
+        CorrelationIdSupplier.generateToken();
+    }
+
+    @AfterAll
+    public static void afterTests() {
+        CorrelationIdSupplier.remove();
+    }
 
     @Test
     public void hentKontonummer__skal_returnere_verdi_fra_kall() {
@@ -27,13 +40,13 @@ public class KontoregisterServiceImplTest {
 
     @Test
     public void hentKontonummer__skal_returnere_fant_ikke_bedrift_feilmelding() {
-        assertThatThrownBy(() ->  KontoregisterService.hentKontonummer("111234567"))
-            .isInstanceOf(KontoregisterFantIkkeBedriftFeilException.class);
+        assertThatThrownBy(() -> KontoregisterService.hentKontonummer("111234567"))
+                .isInstanceOf(KontoregisterFantIkkeBedriftFeilException.class);
     }
 
     @Test
     public void hentKontonummer__skal_returnere_feilmelding() {
-        assertThatThrownBy(() ->  KontoregisterService.hentKontonummer("777333333"))
-            .isInstanceOf(KontoregisterFeilException.class);
+        assertThatThrownBy(() -> KontoregisterService.hentKontonummer("777333333"))
+                .isInstanceOf(KontoregisterFeilException.class);
     }
 }
