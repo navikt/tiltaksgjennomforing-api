b9ae5201d3c0f8e935793a615964106984ab9342
MagnusRom <magnus.skalleberg.rom@nav.no>
2024-06-04 13:06:43 +0200

    lagt til felter i vtaoStrategy

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/VtaoStrategy.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/VtaoStrategy.java
@@ -4,4 +4,11 @@ public class VtaoStrategy extends BaseAvtaleInnholdStrategy {
     public VtaoStrategy(AvtaleInnhold avtaleInnhold) {
         super(avtaleInnhold);
     }
+
+    @Override
+    public void endre(EndreAvtale nyAvtale) {
+        avtaleInnhold.setStillingstittel(nyAvtale.getStillingstittel());
+        avtaleInnhold.setArbeidsgiverKontonummer(nyAvtale.getArbeidsgiverKontonummer());
+        super.endre(nyAvtale);
+    }
 }
