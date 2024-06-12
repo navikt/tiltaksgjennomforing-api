1ee215cb2b9e844c428424bf8afbe649ab874a49
Eirik Vigeland <eirik.vigeland@nav.no>
2024-06-12 12:55:35 +0200

    fix. Justert flere Spring Kafka instillinger

--- src/main/java/no/nav/tag/tiltaksgjennomforing/infrastruktur/kafka/DvhMeldingKafkaConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/infrastruktur/kafka/DvhMeldingKafkaConfiguration.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.tag.tiltaksgjennomforing.datavarehus.AvroTiltakHendelse;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,13 +21,6 @@ import java.util.Map;
 @EnableKafka
 public class DvhMeldingKafkaConfiguration {
 
-    @Value("${KAFKA_SCHEMA_REGISTRY:#{null}}}")
-    private String schemaRegistryUrl;
-    @Value("${KAFKA_SCHEMA_REGISTRY_USER:#{null}}}")
-    private String schemaRegistryUser;
-    @Value("${KAFKA_SCHEMA_REGISTRY_PASSWORD:#{null}}}")
-    private String schemaRegistryPassword;
-
     @Bean
     public KafkaTemplate<String, AvroTiltakHendelse> dvhMeldingKafkaTemplate(KafkaProperties kafkaProperties) {
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(producerConfigs(kafkaProperties)));
@@ -39,9 +31,6 @@ public class DvhMeldingKafkaConfiguration {
 
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        props.put("schema.registry.url", schemaRegistryUrl);
-        props.put("basic.auth.credentials.source", "USER_INFO");
-        props.put("basic.auth.user.info", schemaRegistryUser + ":" + schemaRegistryPassword);
 
         return props;
     }
