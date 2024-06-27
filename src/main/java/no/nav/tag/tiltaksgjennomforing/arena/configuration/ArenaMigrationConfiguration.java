e417e5fa83c40d44586193fc98894617af12dcca
Eirik Vigeland <eirik.vigeland@nav.no>
2024-06-27 15:52:46 +0200

    fix. EnableAsync er allerede satt på main

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/configuration/ArenaMigrationConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/configuration/ArenaMigrationConfiguration.java
@@ -2,12 +2,10 @@ package no.nav.tag.tiltaksgjennomforing.arena.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
-@EnableAsync
 @Configuration
 public class ArenaMigrationConfiguration {
 
