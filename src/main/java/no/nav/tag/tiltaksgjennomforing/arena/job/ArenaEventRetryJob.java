18ba578d8eaa6773b1d508f6367a812ca624dd0e
Eirik Vigeland <eirik.vigeland@nav.no>
2024-06-27 07:57:12 +0200

    fix. Fjernet unødvendig logging

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/job/ArenaEventRetryJob.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/job/ArenaEventRetryJob.java
@@ -1,6 +1,5 @@
 package no.nav.tag.tiltaksgjennomforing.arena.job;
 
-import lombok.extern.slf4j.Slf4j;
 import no.nav.tag.tiltaksgjennomforing.arena.models.event.ArenaEvent;
 import no.nav.tag.tiltaksgjennomforing.arena.service.ArenaEventRetryService;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Slf4j
 @Component
 public class ArenaEventRetryJob {
     private final ArenaEventRetryService arenaEventRetryService;
@@ -19,7 +17,6 @@ public class ArenaEventRetryJob {
 
     @Scheduled(fixedDelay = 60000)
     public void updateArenaEventStatus() {
-        log.info("ArenaEventRetryJob startet");
         List<ArenaEvent> arenaEvents = arenaEventRetryService.getRetryEvents();
 
         if (!arenaEvents.isEmpty()) {
