05fcce5494bb883626314521b7ac325aa9df3de4
Eirik Vigeland <eirik.vigeland@nav.no>
2024-09-17 10:50:05 +0200

    feat. Kall til aktivitetsplan ACL for å veksle inn deltaker-id

---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/client/AktivitetArenaAclTranslationAktivitetKategori.java
@@ -0,0 +1,7 @@
+package no.nav.tag.tiltaksgjennomforing.arena.client;
+
+public enum AktivitetArenaAclTranslationAktivitetKategori {
+    TILTAKSAKTIVITET,
+    UTDANNINGSAKTIVITET,
+    GRUPPEAKTIVITET,
+}
