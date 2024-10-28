8cbff3fd00cd5c39decaedabe5c7d32f10d598ec
Eirik Vigeland <eirik.vigeland@nav.no>
2024-10-28 12:54:50 +0100

    fix. fjernet unødvendig import

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/events/AvtaleForlenget.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/events/AvtaleForlenget.java
@@ -3,7 +3,6 @@ package no.nav.tag.tiltaksgjennomforing.avtale.events;
 import lombok.Value;
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
 import no.nav.tag.tiltaksgjennomforing.avtale.NavIdent;
-import org.w3c.dom.events.Event;
 
 @Value
 public class AvtaleForlenget {
