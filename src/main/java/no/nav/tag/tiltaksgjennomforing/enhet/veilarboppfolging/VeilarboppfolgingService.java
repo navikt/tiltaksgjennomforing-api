7989c7015e32276178893ad764482f311a4aad52
Eirik Vigeland <eirik.vigeland@nav.no>
2024-10-30 12:40:15 +0100

    log. oppdatert loggmelding for sjekk av innsatsbehov fra veilarboppfolging

--- src/main/java/no/nav/tag/tiltaksgjennomforing/enhet/veilarboppfolging/VeilarboppfolgingService.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/enhet/veilarboppfolging/VeilarboppfolgingService.java
@@ -1,5 +1,6 @@
 package no.nav.tag.tiltaksgjennomforing.enhet.veilarboppfolging;
 
+import com.google.common.hash.Hashing;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
 import no.nav.tag.tiltaksgjennomforing.enhet.Formidlingsgruppe;
@@ -8,6 +9,8 @@ import no.nav.tag.tiltaksgjennomforing.enhet.Oppfølgingsstatus;
 import no.nav.tag.tiltaksgjennomforing.exceptions.Feilkode;
 import no.nav.tag.tiltaksgjennomforing.exceptions.FeilkodeException;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Service
@@ -22,6 +25,12 @@ public class VeilarboppfolgingService {
     public Oppfølgingsstatus hentOppfolgingsstatus(String fnr) {
         try {
             HentOppfolgingsstatusRespons respons = client.hentOppfolgingsstatus(new HentOppfolgingsstatusRequest(fnr));
+
+            log.info("Hentet servicegruppe {} og formidlingsgruppe {} for id {}",
+                respons.servicegruppe(),
+                respons.formidlingsgruppe(),
+                Hashing.sha256().hashString(fnr + VeilarboppfolgingService.class.getName(), StandardCharsets.UTF_8)
+            );
 
             return new Oppfølgingsstatus(
                 Formidlingsgruppe.parse(respons.formidlingsgruppe()),
