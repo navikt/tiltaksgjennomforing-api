e1213e26a828dd739c03102b6caa81c18005c55d
Eirik Vigeland <eirik.vigeland@nav.no>
2024-11-07 09:39:41 +0100

    feat. egen kafka consumer for å slippe å måtte resette group-id for hele applikasjonen (#1198)

---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/configuration/ArenaKafkaConsumerConfig.java
@@ -0,0 +1,33 @@
+package no.nav.tag.tiltaksgjennomforing.arena.configuration;
+
+import no.nav.tag.tiltaksgjennomforing.Miljø;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@Profile({ Miljø.DEV_FSS, Miljø.PROD_FSS })
+public class ArenaKafkaConsumerConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> arenaContainerFactory(KafkaProperties kafkaProperties) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory(kafkaProperties));
+        return factory;
+    }
+
+    private ConsumerFactory<String, String> consumerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> props = kafkaProperties.buildConsumerProperties(null);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "tiltaksgjennomforing-api");
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+}
