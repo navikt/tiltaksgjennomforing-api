5f75a5fb5adf0dc25ac4a37c7a3acc4382ab865b
Eirik Vigeland <eirik.vigeland@nav.no>
2024-11-20 13:30:37 +0100

    fix. korrekt navn på q0-topic

--- src/main/java/no/nav/tag/tiltaksgjennomforing/infrastruktur/kafka/Topics.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/infrastruktur/kafka/Topics.java
@@ -1,5 +1,5 @@
 package no.nav.tag.tiltaksgjennomforing.infrastruktur.kafka;
 
 public class Topics {
-    public static final String AVTALE_HENDELSE_COMPACT = "arbeidsgiver.tiltak-avtale-hendelse-compact-q0";
+    public static final String AVTALE_HENDELSE_COMPACT = "team-tiltak.tiltak-avtale-hendelse-q0";
 }
