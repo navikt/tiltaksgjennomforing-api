50e871dec78ec27d098d65d779005f55f7b6d4e1
Eirik Vigeland <eirik.vigeland@nav.no>
2024-11-20 10:54:56 +0100

    kun avtale-melding kafka fra q0 - ingen andre meldinger

--- src/main/java/no/nav/tag/tiltaksgjennomforing/infrastruktur/auditing/AuditKafkaLogger.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/infrastruktur/auditing/AuditKafkaLogger.java
@@ -1,38 +1,14 @@
 package no.nav.tag.tiltaksgjennomforing.infrastruktur.auditing;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.tag.tiltaksgjennomforing.infrastruktur.kafka.Topics;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @ConditionalOnProperty("tiltaksgjennomforing.kafka.enabled")
 public class AuditKafkaLogger implements AuditLogger {
-    private final KafkaTemplate<String, String> auditKafkaTemplate;
-    private final ObjectMapper mapper = new ObjectMapper()
-            .registerModule(new JavaTimeModule())
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-    public AuditKafkaLogger(KafkaTemplate<String, String> kafkaTemplate) {
-        this.auditKafkaTemplate = kafkaTemplate;
-    }
 
     @Override
-    public void logg(AuditEntry event) {
-        try {
-            auditKafkaTemplate.send(Topics.AUDIT_HENDELSE, mapper.writeValueAsString(event)).whenComplete((result, ex) -> {
-                if (ex != null) {
-                    log.error("Audit-hendelse kunne ikke sendes til Kafka topic {}", Topics.AUDIT_HENDELSE, ex);
-                }
-            });
-        } catch (JsonProcessingException ex) {
-            log.error("Audit-hendelse kunne ikke serialiseres til Kafkamelding", ex);
-        }
-    }
+    public void logg(AuditEntry event) {}
 }
--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/SmsProducer.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/kafka/SmsProducer.java
@@ -1,41 +1,14 @@
 package no.nav.tag.tiltaksgjennomforing.varsel.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.tag.tiltaksgjennomforing.infrastruktur.kafka.Topics;
 import no.nav.tag.tiltaksgjennomforing.varsel.Sms;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @ConditionalOnProperty("tiltaksgjennomforing.kafka.enabled")
 @Component
 @Slf4j
 public class SmsProducer {
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final ObjectMapper objectMapper;
-
-    public SmsProducer(
-        KafkaTemplate<String, String> kafkaTemplate,
-        ObjectMapper objectMapper
-    ) {
-        this.kafkaTemplate = kafkaTemplate;
-        this.objectMapper = objectMapper;
-    }
-
-    public void sendSmsVarselMeldingTilKafka(Sms sms) throws JsonProcessingException {
-        String melding = objectMapper.writeValueAsString(sms);
-        kafkaTemplate.send(Topics.TILTAK_SMS, sms.getSmsVarselId().toString(), melding).whenComplete((result, ex) -> {
-            if (ex != null) {
-                log.warn("Sms med id={} kunne ikke sendes til Kafka topic", sms.getSmsVarselId());
-            } else {
-                log.info(
-                    "Sms med id={} sendt på Kafka topic {}",
-                    sms.getSmsVarselId(),
-                    result.getProducerRecord().topic()
-                );
-            }
-        });
-    }
+    public void sendSmsVarselMeldingTilKafka(Sms sms) throws JsonProcessingException {}
 }
