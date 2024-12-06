e9de1f4f7ad7ce5625ccc6954c33a455cee49421
Eirik Vigeland <eirik.vigeland@nav.no>
2024-12-06 10:49:28 +0100

    fix. la til Profil for ArenaKafkaConsumerConfig

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/configuration/ArenaKafkaConsumerConfig.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/configuration/ArenaKafkaConsumerConfig.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @Configuration
 @EnableKafka
-@Profile({ Miljø.LOCAL, Miljø.DEV_FSS, Miljø.PROD_FSS })
+@Profile({ Miljø.LOCAL, Miljø.DEV_FSS_Q0, Miljø.DEV_FSS, Miljø.PROD_FSS })
 public class ArenaKafkaConsumerConfig {
 
     @Bean
