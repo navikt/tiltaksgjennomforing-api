25554bd7cab72f4d86cea3cace7f92b1f89e533e
Eirik Vigeland <eirik.vigeland@nav.no>
2024-12-12 10:29:14 +0100

    feat. stopp ArenaAgreementJob midlertidig

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/job/ArenaAgreementJob.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/job/ArenaAgreementJob.java
@@ -1,13 +1,11 @@
 package no.nav.tag.tiltaksgjennomforing.arena.job;
 
 import no.nav.tag.tiltaksgjennomforing.Miljø;
-import no.nav.tag.tiltaksgjennomforing.arena.models.migration.ArenaAgreementAggregate;
 import no.nav.tag.tiltaksgjennomforing.arena.service.ArenaAgreementService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -21,11 +19,11 @@ public class ArenaAgreementJob {
 
     @Scheduled(fixedRate =  30, timeUnit = TimeUnit.SECONDS)
     public void run() {
-        List<ArenaAgreementAggregate> arenaAgreements = arenaAgreementService.getArenaAgreementsForProcessing();
-
-        if (!arenaAgreements.isEmpty()) {
-            arenaAgreementService.processAgreements(arenaAgreements);
-        }
+//        List<ArenaAgreementAggregate> arenaAgreements = arenaAgreementService.getArenaAgreementsForProcessing();
+//
+//        if (!arenaAgreements.isEmpty()) {
+//            arenaAgreementService.processAgreements(arenaAgreements);
+//        }
     }
 
 }
