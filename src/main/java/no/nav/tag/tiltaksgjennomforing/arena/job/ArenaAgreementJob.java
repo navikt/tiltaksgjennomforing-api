e24a14a6fa2ee1288eda06aec5543202f7a780e7
Eirik Vigeland <eirik.vigeland@nav.no>
2024-12-13 13:16:48 +0100

    feat. id på arena_agreement_migration som lagres ved start av prossesering

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/job/ArenaAgreementJob.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/job/ArenaAgreementJob.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -21,7 +22,7 @@ public class ArenaAgreementJob {
 
     @Scheduled(fixedRate =  30, timeUnit = TimeUnit.SECONDS)
     public void run() {
-        List<ArenaAgreementAggregate> arenaAgreements = arenaAgreementService.getArenaAgreementsForProcessing();
+        Map<UUID, ArenaAgreementAggregate> arenaAgreements = arenaAgreementService.getArenaAgreementsForProcessing();
 
         if (!arenaAgreements.isEmpty()) {
             arenaAgreementService.processAgreements(arenaAgreements);
