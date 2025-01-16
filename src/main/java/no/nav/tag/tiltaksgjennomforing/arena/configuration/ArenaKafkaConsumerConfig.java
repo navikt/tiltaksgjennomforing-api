ce8d1caa63484ff7ba7b6d24c62d8418978caa63
Eirik Vigeland <eirik.vigeland@nav.no>
2025-01-16 19:59:59 +0100

    feat. reseatt group-id for arena for ny gjennomkjøring

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/configuration/ArenaKafkaConsumerConfig.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/configuration/ArenaKafkaConsumerConfig.java
@@ -27,7 +27,7 @@ public class ArenaKafkaConsumerConfig {
 
     private ConsumerFactory<String, String> consumerFactory(KafkaProperties kafkaProperties) {
         Map<String, Object> props = kafkaProperties.buildConsumerProperties(null);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "tiltaksgjennomforing-api-5");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "tiltaksgjennomforing-api-6");
         return new DefaultKafkaConsumerFactory<>(props);
     }
 }
