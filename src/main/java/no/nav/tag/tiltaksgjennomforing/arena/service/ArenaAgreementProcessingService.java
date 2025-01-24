2185eb5b8eddb62f40be9b825736a1391fcb5b3b
Eirik Vigeland <eirik.vigeland@nav.no>
2025-01-24 11:53:42 +0100

    fix. midlertidig fiks for de 6 bedriftene som failet

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/service/ArenaAgreementProcessingService.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/service/ArenaAgreementProcessingService.java
@@ -283,9 +283,18 @@ public class ArenaAgreementProcessingService {
 
         Avtale avtale = Avtale.opprett(opprettAvtale, Avtaleopphav.ARENA);
 
-        Organisasjon org = eregService.hentVirksomhet(avtale.getBedriftNr());
-        avtale.leggTilBedriftNavn(org.getBedriftNavn());
-        avtale.leggTilDeltakerNavn(hentNavnFraPdlRespons(personalData));
+        if ("100004368".equals(avtale.getBedriftNr().asString())) {
+            avtale.leggTilBedriftNavn("FÖRSÄKRINGSKASSAN");
+            try {
+                avtale.leggTilDeltakerNavn(hentNavnFraPdlRespons(personalData));
+            } catch (Exception e) {
+                log.error("Feil ved henting av navn fra PDL", e);
+            }
+        } else {
+            Organisasjon org = eregService.hentVirksomhet(avtale.getBedriftNr());
+            avtale.leggTilBedriftNavn(org.getBedriftNavn());
+            avtale.leggTilDeltakerNavn(hentNavnFraPdlRespons(personalData));
+        }
 
         getGeoEnhetFromNorg2(personalData).ifPresent((norg2GeoResponse) -> {
             avtale.setEnhetGeografisk(norg2GeoResponse.getEnhetNr());
--- src/main/java/no/nav/tag/tiltaksgjennomforing/orgenhet/EregService.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/orgenhet/EregService.java
@@ -8,10 +8,19 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EregService {
+    private List<String> arenaMigreringBedriftNr = List.of(
+        "912006751",
+        "921327072",
+        "925586951",
+        "925586129",
+        "925076937"
+    );
+
     private final EregProperties eregProperties;
     private final RestTemplate restTemplate = new RestTemplate();
 
@@ -28,7 +37,7 @@ public class EregService {
             if ("Organisasjonsledd".equals(eregEnhet.type())) {
                 throw new EnhetErOrganisasjonsleddException();
             }
-            if (!eregEnhet.erAktiv()) {
+            if (!eregEnhet.erAktiv() && !arenaMigreringBedriftNr.contains(bedriftNr.asString())) {
                 throw new EnhetErSlettetException();
             }
             return eregEnhet.konverterTilDomeneObjekt();
