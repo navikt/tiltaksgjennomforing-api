b46bed3de9d2a043077d8bf200ef4e646bea6f6a
Eirik Vigeland <eirik.vigeland@nav.no>
2025-02-20 10:41:44 +0100

    fix. fikset json serialisering av diskresjonskode

--- src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/Diskresjonskode.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/Diskresjonskode.java
@@ -1,5 +1,11 @@
 package no.nav.tag.tiltaksgjennomforing.autorisasjon;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+
 public enum Diskresjonskode {
     STRENGT_FORTROLIG_UTLAND,
     STRENGT_FORTROLIG,
@@ -12,7 +18,14 @@ public enum Diskresjonskode {
             case "STRENGT_FORTROLIG" -> STRENGT_FORTROLIG;
             case "FORTROLIG" -> FORTROLIG;
             case "UGRADERT" -> UGRADERT;
-            default -> throw new IllegalArgumentException("Ukjent diskresjonskode: " + str);
+            case null, default -> UGRADERT;
         };
+    }
+
+    public static class DiskresjonskodeDeserializer extends JsonDeserializer<Diskresjonskode> {
+        @Override
+        public Diskresjonskode deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return Diskresjonskode.parse(p.getValueAsString());
+        }
     }
 }
--- src/main/java/no/nav/tag/tiltaksgjennomforing/persondata/Adressebeskyttelse.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/persondata/Adressebeskyttelse.java
@@ -1,10 +1,13 @@
 package no.nav.tag.tiltaksgjennomforing.persondata;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Value;
 import no.nav.tag.tiltaksgjennomforing.autorisasjon.Diskresjonskode;
 
 @Value
 public class Adressebeskyttelse {
     public static final Adressebeskyttelse INGEN_BESKYTTELSE = new Adressebeskyttelse(Diskresjonskode.UGRADERT);
+
+    @JsonDeserialize(using = Diskresjonskode.DiskresjonskodeDeserializer.class)
     private final Diskresjonskode gradering;
 }
