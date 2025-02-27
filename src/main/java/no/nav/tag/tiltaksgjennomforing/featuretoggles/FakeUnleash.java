9519558e467eba959e9b866011abecf50bfb66bf
Eirik Vigeland <eirik.vigeland@nav.no>
2025-02-27 08:16:27 +0100

    fix. Fikset FakeUnleash til å kunne kjøre uten request-scope
    Problemet oppstod ved testing av jobber eller kafka lokalt,
    hvor kallet til unleash ikke ble gjort via http-request.
    Da vil kall mot feature toggles faile.
    
    Denne endringen gjør det mulig å kjøre unleash utenfor
    et request-scope.

---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/featuretoggles/FakeUnleash.java
@@ -0,0 +1,97 @@
+package no.nav.tag.tiltaksgjennomforing.featuretoggles;
+
+import io.getunleash.UnleashContext;
+import io.getunleash.Variant;
+import jakarta.servlet.http.HttpServletRequest;
+import no.nav.tag.tiltaksgjennomforing.Miljø;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.BiPredicate;
+
+public final class FakeUnleash extends io.getunleash.FakeUnleash {
+    private final Set<String> miljo;
+
+    public FakeUnleash(String... miljo) {
+        super();
+        this.miljo = Set.of(miljo);
+    }
+
+    @Override
+    public boolean isEnabled(String toggleName, UnleashContext context) {
+        updateTogglesFromRequest();
+        return super.isEnabled(toggleName, false);
+    }
+
+    @Override
+    public boolean isEnabled(String toggleName, boolean defaultSetting) {
+        updateTogglesFromRequest();
+        return super.isEnabled(toggleName, defaultSetting);
+    }
+
+    @Override
+    public boolean isEnabled(String toggleName, BiPredicate<String, UnleashContext> fallbackAction) {
+        updateTogglesFromRequest();
+        return super.isEnabled(toggleName, fallbackAction);
+    }
+
+    @Override
+    public Variant getVariant(String toggleName, Variant defaultValue) {
+        updateTogglesFromRequest();
+        return super.getVariant(toggleName, defaultValue);
+    }
+
+    @Override
+    public List<String> getFeatureToggleNames() {
+        updateTogglesFromRequest();
+        return super.getFeatureToggleNames();
+    }
+
+    private void updateTogglesFromRequest() {
+        if (miljo.contains(Miljø.TEST)) {
+            return;
+        }
+
+        Optional<HttpServletRequest> requestOpt = resolveRequest();
+        if (requestOpt.isEmpty()) {
+            this.enableAll();
+            return;
+        }
+
+        List<String> headers = Optional.ofNullable(requestOpt.get().getHeader("features"))
+            .map(feature -> List.of(feature.split(",")))
+            .orElse(Collections.emptyList());
+
+        Optional<String> first = headers.stream().findFirst();
+        if (first.map("enabled"::equals).orElse(false)) {
+            this.enableAllExcept(getToggleExclusions(headers));
+        } else if (first.map("disabled"::equals).orElse(false)) {
+            this.disableAllExcept(getToggleExclusions(headers));
+        } else {
+            this.disableAll();
+        }
+    }
+
+    private static String[] getToggleExclusions(List<String> headers) {
+        return headers.stream()
+            .map(header -> header.startsWith("!") ? header.substring(1) : null)
+            .filter(Objects::nonNull)
+            .toList()
+            .toArray(new String[0]);
+    }
+
+    private static Optional<HttpServletRequest> resolveRequest() {
+        try {
+            return Optional.of(
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest()
+            );
+        } catch (IllegalStateException e) {
+            return Optional.empty();
+        }
+    }
+}
--- src/main/java/no/nav/tag/tiltaksgjennomforing/featuretoggles/FeatureToggleConfig.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/featuretoggles/FeatureToggleConfig.java
@@ -4,20 +4,13 @@ package no.nav.tag.tiltaksgjennomforing.featuretoggles;
 import io.getunleash.DefaultUnleash;
 import io.getunleash.Unleash;
 import io.getunleash.util.UnleashConfig;
-import jakarta.servlet.http.HttpServletRequest;
 import no.nav.tag.tiltaksgjennomforing.Miljø;
 import no.nav.tag.tiltaksgjennomforing.featuretoggles.enhet.ByEnhetStrategy;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.context.annotation.RequestScope;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 
 @Configuration
@@ -49,41 +42,15 @@ public class FeatureToggleConfig {
     }
 
     @Bean
-    @RequestScope
     @Profile({ Miljø.LOCAL, Miljø.DOCKER_COMPOSE, Miljø.DEV_GCP_LABS })
-    public Unleash unleashMock(@Autowired HttpServletRequest request) {
-        FakeFakeUnleash fakeUnleash = new FakeFakeUnleash();
-
-        List<String> headers = Optional.ofNullable(request.getHeader("features"))
-            .map(feature -> List.of(feature.split(",")))
-            .orElse(Collections.emptyList());
-
-        Optional<String> first = headers.stream().findFirst();
-        if (first.map("enabled"::equals).orElse(false)) {
-            fakeUnleash.enableAll();
-            headers.forEach(header -> {
-                if (header.startsWith("!")) {
-                    fakeUnleash.disable(header.substring(1));
-                }
-            });
-        } else if(first.map("disabled"::equals).orElse(false)) {
-            fakeUnleash.disableAll();
-            headers.forEach(header -> {
-                if (header.startsWith("!")) {
-                    fakeUnleash.enable(header.substring(1));
-                }
-            });
-        } else {
-            fakeUnleash.disableAll();
-        }
-
-        return fakeUnleash;
+    public Unleash unleashMock() {
+        return new FakeUnleash();
     }
 
     @Bean
     @Profile(Miljø.TEST)
     public Unleash unleashTestMock() {
-        FakeFakeUnleash fakeUnleash = new FakeFakeUnleash();
+        FakeUnleash fakeUnleash = new FakeUnleash(Miljø.TEST);
         fakeUnleash.enable(FeatureToggle.SMS_TIL_MOBILNUMMER.getToggleNavn());
         return fakeUnleash;
     }
