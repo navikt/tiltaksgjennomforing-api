ab63dd977c3df3c8721503c69f01521a47dd5b5a
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2025-02-28 12:35:16 +0100

    PR-fikser

--- src/main/java/no/nav/tag/tiltaksgjennomforing/dev/DevController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/dev/DevController.java
@@ -12,7 +12,6 @@ import no.nav.tag.tiltaksgjennomforing.avtale.NavIdent;
 import no.nav.tag.tiltaksgjennomforing.avtale.TilskuddsperiodeConfig;
 import no.nav.tag.tiltaksgjennomforing.avtale.Tiltakstype;
 import no.nav.tag.tiltaksgjennomforing.utils.Now;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,31 +21,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
 import java.util.Optional;
 
 @Profile({Miljø.DEV_FSS, Miljø.LOCAL, Miljø.DOCKER_COMPOSE})
 @RestController
-@RequestMapping("/dev-api/")
+@RequestMapping("/dev/")
 // Controlleren er begrenset til testmiljøer, men som en ekstra sikring tillater vi kun
 // at veiledere utfører disse handlingene.
 @ProtectedWithClaims(issuer = "aad")
 public class DevController {
-
-    private final String miljo;
     private final AvtaleRepository avtaleRepository;
     private final TilskuddsperiodeConfig tilskuddsperiodeConfig;
     private final InnloggingService innloggingService;
 
-    public DevController(@Value("${MILJO:}") String miljo, AvtaleRepository avtaleRepository, TilskuddsperiodeConfig tilskuddsperiodeConfig, InnloggingService innloggingService) {
-        this.miljo = miljo;
+    public DevController(AvtaleRepository avtaleRepository, TilskuddsperiodeConfig tilskuddsperiodeConfig, InnloggingService innloggingService) {
         this.avtaleRepository = avtaleRepository;
         this.tilskuddsperiodeConfig = tilskuddsperiodeConfig;
         this.innloggingService = innloggingService;
     }
 
-    @PutMapping("godkjenn/{avtaleNr}")
-    public ResponseEntity<String> godkjennAvtale(@PathVariable("avtaleNr") Integer avtaleNr, @RequestBody Godkjenning godkjenning) {
+    @PutMapping("/avtale/{avtaleNr}/godkjenn")
+    ResponseEntity<String> godkjennAvtale(
+            @PathVariable("avtaleNr") Integer avtaleNr,
+            @RequestBody GodkjennAvtaleRequest godkjennAvtaleRequest
+    ) {
         Optional<Avtale> kanskjeAvtale = avtaleRepository.findByAvtaleNr(avtaleNr);
 
         if (kanskjeAvtale.isEmpty()) {
@@ -70,14 +68,14 @@ public class DevController {
             avtale.godkjennForVeileder(avtale.getVeilederNavIdent());
         }
         if (!avtale.erAvtaleInngått()) {
-            avtale.godkjennTilskuddsperiode(godkjenning.beslutterIdent(), godkjenning.kostnadssted());
+            avtale.godkjennTilskuddsperiode(godkjennAvtaleRequest.beslutterIdent(), godkjennAvtaleRequest.kostnadssted());
         }
         avtaleRepository.save(avtale);
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("familierelasjon/{avtaleNr}")
-    public void familierelasjon(@PathVariable("avtaleNr") Integer avtaleNr) {
+    @PutMapping("/avtale/{avtaleNr}/familierelasjon")
+    void familierelasjon(@PathVariable("avtaleNr") Integer avtaleNr) {
         Avtale avtale = avtaleRepository.findByAvtaleNr(avtaleNr).orElseThrow();
         EndreAvtale endring = EndreAvtale.fraAvtale(avtale);
         endring.setFamilietilknytningForklaring(null);
@@ -92,15 +90,15 @@ public class DevController {
         avtaleRepository.save(avtale);
     }
 
-    @PutMapping("etterregistrering/{avtaleNr}")
-    public void etterregistrering(@PathVariable("avtaleNr") Integer avtaleNr) {
+    @PutMapping("/avtale/{avtaleNr}/etterregistrering")
+    void etterregistrering(@PathVariable("avtaleNr") Integer avtaleNr) {
         Avtale avtale = avtaleRepository.findByAvtaleNr(avtaleNr).orElseThrow();
         avtale.togglegodkjennEtterregistrering(hentVeileder());
         avtaleRepository.save(avtale);
     }
 
-    @PutMapping("ny-oppfolging-dato/{avtaleNr}")
-    public void nyOppfolgingDato(@PathVariable("avtaleNr") Integer avtaleNr) {
+    @PutMapping("/avtale/{avtaleNr}/ny-oppfolging-dato")
+    void nyOppfolgingDato(@PathVariable("avtaleNr") Integer avtaleNr) {
         Avtale avtale = avtaleRepository.findByAvtaleNr(avtaleNr).orElseThrow();
         if (avtale.getTiltakstype().equals(Tiltakstype.VTAO)) {
             avtale.setKreverOppfolgingFom(Now.localDate().minusDays(1));
@@ -109,14 +107,14 @@ public class DevController {
         }
     }
 
-    @PostMapping("opprett-avtale")
-    public ResponseEntity<String> opprettAvtale(@RequestBody Opprettelse avtale) {
+    @PostMapping("/avtale")
+    ResponseEntity<String> opprettAvtale(@RequestBody OpprettAvtaleRequest avtale) {
         var lagretAvtale = avtaleRepository.save(Avtale.opprett(
-                avtale.getOpprett(), Avtaleopphav.VEILEDER, hentVeileder()));
+                avtale.opprett(), Avtaleopphav.VEILEDER, hentVeileder()));
         lagretAvtale.endreAvtale(Now.instant(),
-                avtale.getEndre(), Avtalerolle.VEILEDER, tilskuddsperiodeConfig.getTiltakstyper());
+                avtale.endre(), Avtalerolle.VEILEDER, tilskuddsperiodeConfig.getTiltakstyper());
         avtaleRepository.save(lagretAvtale);
-        return ResponseEntity.ok().body("\"" + lagretAvtale.getId().toString() + "\"");
+        return ResponseEntity.ok().body(lagretAvtale.getId().toString());
     }
 
     private NavIdent hentVeileder() {
--- src/main/java/no/nav/tag/tiltaksgjennomforing/dev/GodkjennAvtaleRequest.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/dev/GodkjennAvtaleRequest.java
@@ -2,7 +2,7 @@ package no.nav.tag.tiltaksgjennomforing.dev;
 
 import no.nav.tag.tiltaksgjennomforing.avtale.NavIdent;
 
-public record Godkjenning(
+record GodkjennAvtaleRequest(
         NavIdent beslutterIdent,
         String kostnadssted
 ) {
