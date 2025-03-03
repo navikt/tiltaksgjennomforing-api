c769d9ca734ffa0567f8685e05da9ee0b94b4439
Eirik Vigeland <eirik.vigeland@nav.no>
2025-03-03 16:21:43 +0100

    feat. forbedring av labs h2 migreringsscript

--- after-migrate-labs.js
+++ after-migrate-labs.js
@@ -12,6 +12,24 @@ const fs = require('node:fs');
 
 const TWENTY_FOUR_HOURS_IN_MS = 24 * 60 * 60 * 1000;
 
+const getProgressBar = (total) => {
+    return (current) => {
+        if (current % 100 !== 0) {
+            return;
+        }
+
+        const progress = current/total < 0.95 ? current/total : 1;
+        const barWidth = 30;
+        const filledWidth = Math.floor(progress * barWidth);
+        const emptyWidth = barWidth - filledWidth;
+        const progressBar = '█'.repeat(filledWidth) + '▒'.repeat(emptyWidth);
+
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(`[${progressBar}] ${Math.round(progress * 100)}%`);
+    };
+};
+
 const replaceDatetime = (str) => {
     const date = new Date(str.replace('TIMETAMP ', '').replaceAll('\'', ''));
     const diff = date.setHours(0,0,0,0) - new Date().setHours(0,0,0,0);
@@ -37,28 +55,34 @@ const replaceDate = (str) => {
 };
 
 try {
-    let file = fs.readFileSync('./dump.sql', 'utf8');
+    console.log('\n **** 🚀 LABS MIGRERING 🚀 **** \n');
+    const file = fs.readFileSync('./dump.sql', 'utf8');
 
     const sisteLinjeMedFlyway = file.split('\n')
         .map((linje, idx) => linje.includes('flyway_schema_history_s_idx') ? idx : -1)
         .filter((idx) => idx !== -1)[0] || -1;
 
-    file = file.split('\n')
-        .filter((_, idx) => idx > sisteLinjeMedFlyway)
-        .join('\n');
+    const fileArr = file.split('\n').filter((_, idx) => idx > sisteLinjeMedFlyway);
+    const drawProgressBar = getProgressBar(fileArr.length);
+    const output = fs.createWriteStream('./src/main/resources/db/callbacks/labs/afterMigrate__sql-dump.sql');
 
-    [...file.matchAll(/TIMESTAMP '20\d{2}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d*)?'/g)].forEach((match) => {
-        file = file.replace(match[0], replaceDatetime(match[0]));
+    fileArr.forEach((line, idx) => {
+        [...line.matchAll(/TIMESTAMP '20\d{2}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d*)?'/g)].forEach((match) => {
+            line = line.replace(match[0], replaceDatetime(match[0]));
+        });
+        [...line.matchAll(/DATE '20\d{2}-\d{2}-\d{2}'/g)].forEach((match) => {
+            line = line.replace(match[0], replaceDate(match[0]));
+        });
+
+        line.replaceAll(/MEMORY /g, '');
+        line.replaceAll(/"PUBLIC"\./g, '');
+
+        output.write(line + '\n');
+        drawProgressBar(idx);
     });
 
-    [...file.matchAll(/DATE '20\d{2}-\d{2}-\d{2}'/g)].forEach((match) => {
-        file = file.replace(match[0], replaceDate(match[0]));
-    });
-
-    file = file.replaceAll(/MEMORY /g, '');
-    file = file.replaceAll(/"PUBLIC"\./g, '');
-
-    fs.writeFileSync('./src/main/resources/db/callbacks/labs/afterMigrate__sql-dump.sql', file);
+    output.end();
+    console.log('\n\n✅  Ferdig!')
 } catch (err) {
     console.error(err);
 }
