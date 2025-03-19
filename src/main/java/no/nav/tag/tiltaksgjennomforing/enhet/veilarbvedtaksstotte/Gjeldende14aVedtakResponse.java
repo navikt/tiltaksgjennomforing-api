182be71114f340217fe3d27ed167d3710e718b05
Sindre Dahl Løken <sindre.loken@nav.no>
2025-03-19 20:44:01 +0100

    ny service og klient for innsatsbehov

---
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/enhet/veilarbvedtaksstotte/Gjeldende14aVedtakResponse.java
@@ -0,0 +1,7 @@
+package no.nav.tag.tiltaksgjennomforing.enhet.veilarbvedtaksstotte;
+
+record Gjeldende14aVedtakResponse(
+    Innsatsgruppe innsatsgruppe,
+    String hovedmal,
+    String fattetDato
+) {}
