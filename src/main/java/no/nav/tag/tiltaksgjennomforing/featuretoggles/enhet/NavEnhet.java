80104d7d2977649b479334b9d950fc0923307738
Eirik Vigeland <eirik.vigeland@nav.no>
2025-03-21 08:53:55 +0100

    fix. fjernet ubrukt kode i NavEnhet.java

--- src/main/java/no/nav/tag/tiltaksgjennomforing/featuretoggles/enhet/NavEnhet.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/featuretoggles/enhet/NavEnhet.java
@@ -1,17 +1,9 @@
 package no.nav.tag.tiltaksgjennomforing.featuretoggles.enhet;
 
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value
-@EqualsAndHashCode(of = "verdi")
 public class NavEnhet {
-    public static final NavEnhet NAV_VIKAFOSSEN = new NavEnhet("2103", "Nav Vikafossen");
-
     String verdi;
     String navn;
-
-    public boolean equalsEnhetsnummer(String verdi) {
-        return this.equals(new NavEnhet(verdi, null));
-    }
 }
