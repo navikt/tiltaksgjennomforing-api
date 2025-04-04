77d808e40cc37c837817db35a7d766be18070566
Eirik Vigeland <eirik.vigeland@nav.no>
2025-04-04 11:49:28 +0200

    feat. admin-endepunkt for å sende siste melding på en avtale til aktivitetsplanen på nytt

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/client/hendelse/HendelseAktivietsplanIdRequest.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/client/hendelse/HendelseAktivietsplanIdRequest.java
@@ -3,7 +3,6 @@ package no.nav.tag.tiltaksgjennomforing.arena.client.hendelse;
 import java.util.UUID;
 
 public record HendelseAktivietsplanIdRequest(
-    UUID avtaleId,
     UUID aktivitetsplanId,
     boolean resendSisteMelding
 ) {}
