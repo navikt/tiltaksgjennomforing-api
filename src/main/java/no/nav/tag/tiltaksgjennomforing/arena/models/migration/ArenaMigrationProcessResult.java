2cecf414faf219dc009603e04bc25e9efb24a068
Eirik Vigeland <eirik.vigeland@nav.no>
2025-05-09 08:52:24 +0200

    feat. lagre feilmeldinger ved migrering i databasen
    Lagre feilmeldinger ved migrering i databasen. Da blir det enklere ved oppslag
    å se hva som har skjedd og å dra ut statisikk i motsetning til å hente fra loggene
    i tillegg kan det lagres flere typer feilmelding ved å bruke string
    istedenfor å bruke ArenaAgreementAction. Actions var aldri ment til bruk
    for feilmeldinger.

--- src/main/java/no/nav/tag/tiltaksgjennomforing/arena/models/migration/ArenaMigrationProcessResult.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/arena/models/migration/ArenaMigrationProcessResult.java
@@ -4,6 +4,6 @@ import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
 
 public sealed interface ArenaMigrationProcessResult {
     record Ignored() implements ArenaMigrationProcessResult {}
-    record Failed(ArenaMigrationAction action) implements ArenaMigrationProcessResult {}
+    record Failed(String error) implements ArenaMigrationProcessResult {}
     record Completed(ArenaMigrationAction action, Avtale avtale) implements ArenaMigrationProcessResult {}
 }
