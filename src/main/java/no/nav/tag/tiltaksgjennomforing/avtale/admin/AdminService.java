cc7a4dcb233cb227a6772231f6eca5f65987e06e
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2025-05-13 13:12:24 +0200

    Ikke opprett avtalekrav-hendelse hvis det fins fra før

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/admin/AdminService.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/admin/AdminService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
 import no.nav.tag.tiltaksgjennomforing.avtale.AvtaleRepository;
+import no.nav.tag.tiltaksgjennomforing.avtale.Avtalerolle;
 import no.nav.tag.tiltaksgjennomforing.avtale.HendelseType;
 import no.nav.tag.tiltaksgjennomforing.avtale.Identifikator;
 import no.nav.tag.tiltaksgjennomforing.avtale.Status;
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -30,24 +32,52 @@ public class AdminService {
     @Transactional
     public void oppdaterteAvtalekrav() {
         AtomicInteger antallSendt = new AtomicInteger();
-        log.info("Oppdaterer avtalekrav ...");
+        AtomicInteger antallIgnorert = new AtomicInteger();
+
+        log.info("Oppdaterer avtalekrav...");
+
         var avtaler = avtaleRepository.findAllByGjeldendeInnhold_GodkjentAvArbeidsgiverNotNullAndStatusIn(
             List.of(Status.GJENNOMFØRES, Status.MANGLER_GODKJENNING, Status.AVSLUTTET)
         );
         log.info("Fant {} avtaler som det skal sendes varslinger på", avtaler.size());
-        List<Varsel> varsler = avtaler.stream().map(this::lagHendelse).peek((varsel) -> {
-            antallSendt.getAndIncrement();
-            if (antallSendt.get() % 100 == 0) {
-                log.info("Opprettet varsel for avtalekrav for {} avtaler", antallSendt.get());
-            }
-        }).toList();
 
-        varselRepository.saveAll(varsler);
-        log.info("lagret {} varsler", varsler.size());
+        List<Varsel> nyeVarsler = new ArrayList<>();
+
+        avtaler.forEach(avtale -> {
+            List<Varsel> arbeidsgiverVarsler = varselRepository.findAllByAvtaleIdAndMottaker(
+                avtale.getId(),
+                Avtalerolle.ARBEIDSGIVER
+            );
+            boolean harAlleredeAvtalekravVarsel = arbeidsgiverVarsler.stream()
+                .anyMatch(varsel -> varsel.getHendelseType().equals(HendelseType.OPPDATERTE_AVTALEKRAV));
+
+            if (harAlleredeAvtalekravVarsel) {
+                var ignorerte = antallIgnorert.getAndIncrement();
+                log.info(
+                    "Fant avtale som allerede har avtalekrav-varsel, ignorerer. Foreløpig {} ignorerte",
+                    ignorerte
+                );
+            } else {
+                var nyttVarselForAvtale = lagHendelse(avtale);
+                antallSendt.getAndIncrement();
+                if (antallSendt.get() % 100 == 0) {
+                    log.info("Opprettet varsel for avtalekrav for {} avtaler", antallSendt.get());
+                }
+                nyeVarsler.add(nyttVarselForAvtale);
+            }
+        });
+
+        varselRepository.saveAll(nyeVarsler);
+        log.info("Lagret {} varsler, hoppet over {} avtaler", nyeVarsler.size(), antallIgnorert.get());
     }
 
     private Varsel lagHendelse(Avtale avtale) {
-        VarselFactory factory = new VarselFactory(avtale, AvtaleHendelseUtførtAvRolle.SYSTEM, Identifikator.SYSTEM, HendelseType.OPPDATERTE_AVTALEKRAV);
+        VarselFactory factory = new VarselFactory(
+            avtale,
+            AvtaleHendelseUtførtAvRolle.SYSTEM,
+            Identifikator.SYSTEM,
+            HendelseType.OPPDATERTE_AVTALEKRAV
+        );
         return factory.arbeidsgiver();
     }
 }
