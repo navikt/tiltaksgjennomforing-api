a64129c8cb730c8ba4abdfbd2b26c684580ba078
MagnusRom <65652609+MagnusRom@users.noreply.github.com>
2025-06-26 11:45:30 +0200

    Shed lock (#1375)
    * lagt til shedlock
    
    * Lagt til Shedlock på Scheduled jobber og fjernet leaderpod checks
    
    Co-authored-by: eirik.vigeland@nav.no
    
    * fikset skrive feil og fjernet leaderElection
    
    * Update pom.xml
    
    Co-authored-by: Odd A <3494925+Oddsor@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Odd A <3494925+Oddsor@users.noreply.github.com>

--- src/main/java/no/nav/tag/tiltaksgjennomforing/TiltaksgjennomforingConfiguration.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/TiltaksgjennomforingConfiguration.java
@@ -1,14 +1,23 @@
 package no.nav.tag.tiltaksgjennomforing;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 class TiltaksgjennomforingConfiguration {
     @Bean
     public RestTemplate noAuthRestTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
+    }
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(dataSource);
     }
 }
