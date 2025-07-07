f68176ca24d5aa769f673509c45190c02a3eac8c
Eirik Vigeland <eirik.vigeland@nav.no>
2025-07-07 09:54:25 +0200

    feat. sortering på tilskuddsperioder for veileder (#1324)
    * feat. sortering på tilskuddsperioder for veileder
    
    * fix. oppdatert sortering
    
    * Endrert til Sort.by for å kunne bruke JpaSort.unsafe
    * Bruke JpaSort.unsafe for å kunne lage spesfikk sortering med sql-case for
    sortering av tilskuddsperiode status og oppfølgingsvarsel.

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleRepository.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleRepository.java
@@ -235,6 +235,7 @@ public interface AvtaleRepository extends JpaRepository<Avtale, UUID>, JpaSpecif
         value = """
             SELECT a
             FROM Avtale a
+            LEFT OUTER JOIN TilskuddPeriode t ON t.id = a.gjeldendeTilskuddsperiode.id
             WHERE a.feilregistrert = FALSE AND
                   (:ufordelt = FALSE OR a.veilederNavIdent IS NULL) AND
                   (:ufordelt = TRUE OR :veilederNavIdent IS NULL OR a.veilederNavIdent = :veilederNavIdent) AND
