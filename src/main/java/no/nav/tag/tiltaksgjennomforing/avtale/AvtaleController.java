a0ad4070932435ad8373eba23a466e41cb629481
Eirik Vigeland <eirik.vigeland@nav.no>
2025-07-30 11:08:08 +0200

    Revert "fix. instant og tidssoner for filtersøk (#1397)"
    This reverts commit 62e41a6ef2dd6d6059b144bfbcf5d33ce699aff3.

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleController.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleController.java
@@ -166,7 +166,7 @@ public class AvtaleController {
         }
 
         filterSok.setAntallGangerSokt(filterSok.getAntallGangerSokt() + 1);
-        filterSok.setSistSoktTidspunkt(Now.instant());
+        filterSok.setSistSoktTidspunkt(Now.localDateTime());
         filterSokRepository.save(filterSok);
         AvtaleQueryParameter avtalePredicate = filterSok.getAvtalePredicate();
 
@@ -216,7 +216,7 @@ public class AvtaleController {
         FilterSok filterSokiDb = filterSokRepository.findFilterSokBySokId(queryParametre.generateHash()).orElse(null);
         if (filterSokiDb != null) {
             filterSokiDb.setAntallGangerSokt(filterSokiDb.getAntallGangerSokt() + 1);
-            filterSokiDb.setSistSoktTidspunkt(Now.instant());
+            filterSokiDb.setSistSoktTidspunkt(Now.localDateTime());
             filterSokRepository.save(filterSokiDb);
             if (!filterSokiDb.erLik(queryParametre)) {
                 log.error("Kollisjon i søkId: {}", filterSokiDb.getSokId());
--- src/main/resources/db/migration/common/V136__tidssoner_filter_sok.sql
+++
@@ -1,2 +0,0 @@
-alter table filter_sok alter column sist_sokt_tidspunkt type timestamp with time zone
-    using sist_sokt_tidspunkt at time zone 'Europe/Oslo';
