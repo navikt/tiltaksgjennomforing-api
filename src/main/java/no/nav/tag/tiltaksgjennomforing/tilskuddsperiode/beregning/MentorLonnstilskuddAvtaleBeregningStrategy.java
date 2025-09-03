b354a1cba7f5f40470b718f4b3676c8d91fbd4d1
Hoang Mickel <mickel.hoang@bekk.no>
2025-09-03 15:18:39 +0200

    feat. beregning av satser og månedslonn

--- src/main/java/no/nav/tag/tiltaksgjennomforing/tilskuddsperiode/beregning/MentorLonnstilskuddAvtaleBeregningStrategy.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/tilskuddsperiode/beregning/MentorLonnstilskuddAvtaleBeregningStrategy.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 import static no.nav.tag.tiltaksgjennomforing.utils.Utils.fikseLøpenumre;
 
 public class MentorLonnstilskuddAvtaleBeregningStrategy extends GenerellLonnstilskuddAvtaleBeregningStrategy {
+    int MAKS_PROSENT = 100;
 
     @Override
     public boolean nødvendigeFelterErUtfylt(Avtale avtale) {
@@ -39,22 +40,11 @@ public class MentorLonnstilskuddAvtaleBeregningStrategy extends GenerellLonnstil
         }
         AvtaleInnhold innhold = avtale.getGjeldendeInnhold();
 
-        int prosent = 100; //innhold.getLonnstilskuddProsent();
         LocalDate start = innhold.getStartDato();
         LocalDate slutt = innhold.getSluttDato();
         LocalDate redusertFra = innhold.getDatoForRedusertProsent();
 
-
-        BigDecimal timerPerUke = BigDecimal.valueOf(innhold.getMentorAntallTimer());
-        BigDecimal timelonn = BigDecimal.valueOf(innhold.getMentorTimelonn());
-        BigDecimal brutto = timerPerUke.multiply(timelonn);
-
-        Integer månedligTilskudd = beregnMånedligTilskudd(brutto, prosent);
-        Integer månedligTilskuddRedusert = null;
-        if (redusertFra != null) {
-            int redusertProsent = Math.max(0, prosent - 10);
-            månedligTilskuddRedusert = beregnMånedligTilskudd(brutto, redusertProsent);
-        }
+        Integer månedligTilskudd = beregnMånedligTilskudd(innhold.getMentorAntallTimer(), innhold.getMentorTimelonn());
 
         List<TilskuddPeriode> perioder = beregnTilskuddsperioderForAvtale(
             UUID.randomUUID(),
@@ -62,9 +52,9 @@ public class MentorLonnstilskuddAvtaleBeregningStrategy extends GenerellLonnstil
             månedligTilskudd,
             start,
             slutt,
-            prosent,
+            MAKS_PROSENT,
             redusertFra,
-            månedligTilskuddRedusert
+            månedligTilskudd
         );
         perioder.forEach(p -> p.setAvtale(avtale));
         perioder.forEach(p -> p.setEnhet(innhold.getEnhetKostnadssted()));
@@ -74,82 +64,17 @@ public class MentorLonnstilskuddAvtaleBeregningStrategy extends GenerellLonnstil
     }
 
     @Override
-    public List<TilskuddPeriode> hentTilskuddsperioderForPeriode(Avtale avtale, LocalDate startDato, LocalDate sluttDato) {
-        if (startDato == null || sluttDato == null || startDato.isAfter(sluttDato) || !nødvendigeFelterErUtfylt(avtale)) {
-            return Collections.emptyList();
-        }
-        AvtaleInnhold innhold = avtale.getGjeldendeInnhold();
-
-        int prosent = 100; //innhold.getLonnstilskuddProsent();
-        LocalDate redusertFra = innhold.getDatoForRedusertProsent();
-
-        BigDecimal brutto = BigDecimal
-            .valueOf(innhold.getMentorAntallTimer())
-            .multiply(BigDecimal.valueOf(innhold.getMentorTimelonn()));
-
-        Integer månedligTilskudd = beregnMånedligTilskudd(brutto, prosent);
-        Integer månedligTilskuddRedusert = null;
-        if (redusertFra != null) {
-            int redusertProsent = Math.max(0, prosent - 10);
-            månedligTilskuddRedusert = beregnMånedligTilskudd(brutto, redusertProsent);
-        }
-
-        List<TilskuddPeriode> perioder = beregnTilskuddsperioderForAvtale(
-            UUID.randomUUID(),
-            avtale.getTiltakstype(),
-            månedligTilskudd,
-            startDato,
-            sluttDato,
-            prosent,
-            redusertFra,
-            månedligTilskuddRedusert
-        );
-        perioder.forEach(p -> p.setAvtale(avtale));
-        perioder.forEach(p -> p.setEnhet(innhold.getEnhetKostnadssted()));
-        perioder.forEach(p -> p.setEnhetsnavn(innhold.getEnhetsnavnKostnadssted()));
-        return perioder;
-    }
-
-    @Override
     public Integer beregnTilskuddsbeløpForPeriode(Avtale avtale, LocalDate startDato, LocalDate sluttDato) {
         if (!nødvendigeFelterErUtfylt(avtale) || startDato == null || sluttDato == null) {
             return null;
         }
         AvtaleInnhold innhold = avtale.getGjeldendeInnhold();
-
-        BigDecimal brutto = BigDecimal
-            .valueOf(innhold.getMentorAntallTimer())
-            .multiply(BigDecimal.valueOf(innhold.getMentorTimelonn()));
-
-        int prosent = 100; //innhold.getLonnstilskuddProsent();
-        Integer månedligTilskudd = beregnMånedligTilskudd(brutto, prosent);
-
-        LocalDate redusertFra = innhold.getDatoForRedusertProsent();
-        Integer månedligTilskuddRedusert = null;
-        if (redusertFra != null) {
-            int redusertProsent = Math.max(0, prosent - 10);
-            månedligTilskuddRedusert = beregnMånedligTilskudd(brutto, redusertProsent);
-        }
-
-
-        if (redusertFra == null || sluttDato.isBefore(redusertFra)) {
-            return LonnstilskuddAvtaleBeregningStrategy.beløpForPeriode(startDato, sluttDato, månedligTilskudd);
-        } else {
-
-            if (månedligTilskuddRedusert == null) {
-                return null;
-            }
-            return LonnstilskuddAvtaleBeregningStrategy.beløpForPeriode(startDato, sluttDato, månedligTilskuddRedusert);
-        }
+        int månedsLonn = beregnMånedligTilskudd(innhold.getMentorAntallTimer(), innhold.getMentorTimelonn());
+        return LonnstilskuddAvtaleBeregningStrategy.beløpForPeriode(startDato, sluttDato, månedsLonn);
     }
 
-    private Integer beregnMånedligTilskudd(BigDecimal bruttoGrunnlag, int prosent) {
-        if (prosent <= 0) {
-            return 0;
-        }
-        BigDecimal tilskudd = bruttoGrunnlag
-            .multiply(BigDecimal.valueOf(prosent))
-            .divide(BigDecimal.valueOf(100), 0, RoundingMode.HALF_UP);
-        return tilskudd.intValue();
+    private Integer beregnMånedligTilskudd(double antalTimerIMåned, double timelonn) {
+        BigDecimal månedslonnBeregningFraTimerMedTimelonn = BigDecimal.valueOf(antalTimerIMåned).multiply(BigDecimal.valueOf(timelonn));
+        return månedslonnBeregningFraTimerMedTimelonn.setScale(0, RoundingMode.HALF_UP).intValue();
     }
 }
