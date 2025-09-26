298ecbf23d36b66fac25066c1f0cd78cbd958ca2
Hoang Mickel <mickel.hoang@bekk.no>
2025-09-26 12:59:24 +0200

    fix. test med endreavtale trenger featuretoggle boolean for mentor
    Co-authored-by: Magnus Rom <magnus.skalleberg.rom@nav.no>
    Co-authored-by: Leif T <leif.thorbjornsen@nav.no>

--- src/test/java/no/nav/tag/tiltaksgjennomforing/avtale/MentorAvtaleBeregningStrategyTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/avtale/MentorAvtaleBeregningStrategyTest.java
@@ -1,6 +1,7 @@
 package no.nav.tag.tiltaksgjennomforing.avtale;
 
 import no.bekk.bekkopen.person.FodselsnummerValidator;
+import no.nav.tag.tiltaksgjennomforing.featuretoggles.FeatureToggle;
 import no.nav.tag.tiltaksgjennomforing.utils.Now;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 public class MentorAvtaleBeregningStrategyTest {
 
@@ -25,9 +27,10 @@ public class MentorAvtaleBeregningStrategyTest {
 
     @Test
     public void en_mentor_avtale_beregning_se_at_otpSats_blir_lagret_i_avtale_innhold_etter_beregning() {
-        Now.fixedDate(LocalDate.of(2021, 3, 1));
-        LocalDate fra = LocalDate.of(2021, 3, 1);
-        LocalDate til = LocalDate.of(2021, 3, 31);
+        when(TestData.featureToggleService.isEnabled(FeatureToggle.MENTOR_TILSKUDD)).thenReturn(true);
+        Now.fixedDate(LocalDate.of(2025, 3, 1));
+        LocalDate fra = LocalDate.of(2025, 3, 1);
+        LocalDate til = LocalDate.of(2025, 3, 31);
         final int antallTimer = 8;
         final int timelonn = 100;
         final int feriepengerBelop = 18;
@@ -43,7 +46,7 @@ public class MentorAvtaleBeregningStrategyTest {
         endreAvtale.setArbeidsgiveravgift(BigDecimal.valueOf(0.012));
         endreAvtale.setMentorAntallTimer(8.0);
         endreAvtale.setMentorTimelonn(100);
-        avtale.endreAvtale(endreAvtale, Avtalerolle.VEILEDER);
+        avtale.endreAvtale(endreAvtale, Avtalerolle.VEILEDER, null, TestData.featureToggleService.isEnabled(FeatureToggle.MENTOR_TILSKUDD));
 
         assertThat(avtale.getGjeldendeInnhold().getFeriepengerBelop()).isEqualTo(feriepengerBelop);
         assertThat(avtale.getGjeldendeInnhold().getOtpBelop()).isEqualTo(forventetOptBelop);
