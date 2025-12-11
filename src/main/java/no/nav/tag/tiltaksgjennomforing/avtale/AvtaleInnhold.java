9d7d4c9846608838cc517ae1343a75ff7a525075
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2025-12-11 11:46:36 +0100

    Fjern ubrukt import

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleInnhold.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleInnhold.java
@@ -30,7 +30,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
