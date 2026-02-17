7560b3ca1aa0203e33977c5a4b69e86d5f4370ef
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-02-17 14:32:19 +0100

    Legg til "kreverOppfolging" på Tiltakstype
    Endrer også "skalBesluttes" til å være en exhaustive
    switch-case, som gjør at vi tvinges til å ta stilling
    til om tiltakstypen skal besluttes/følges opp.
    
    # Conflicts:
    #	src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/Tiltakstype.java

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/Tiltakstype.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/Tiltakstype.java
@@ -50,10 +50,27 @@ public enum Tiltakstype {
     public boolean isMentor() { return this == Tiltakstype.MENTOR; }
 
     public boolean skalBesluttes() {
-        return this == Tiltakstype.SOMMERJOBB ||
-            this == Tiltakstype.VARIG_LONNSTILSKUDD ||
-            this == Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD ||
-            this == Tiltakstype.VTAO ||
-            this == Tiltakstype.MENTOR;
+        switch (this) {
+            case ARBEIDSTRENING, INKLUDERINGSTILSKUDD -> {
+                return false;
+            }
+            case MIDLERTIDIG_LONNSTILSKUDD, VARIG_LONNSTILSKUDD, VTAO, SOMMERJOBB, MENTOR -> {
+                return true;
+            }
+            case null -> throw new IllegalStateException("Tiltakstype kan ikke være null");
+        }
+    }
+
+    public boolean kreverOppfolging() {
+        switch (this) {
+            case MIDLERTIDIG_LONNSTILSKUDD, VARIG_LONNSTILSKUDD, MENTOR, ARBEIDSTRENING, INKLUDERINGSTILSKUDD,
+                 SOMMERJOBB -> {
+                return false;
+            }
+            case VTAO -> {
+                return true;
+            }
+            case null -> throw new IllegalStateException("Tiltakstype kan ikke være null");
+        }
     }
 }
