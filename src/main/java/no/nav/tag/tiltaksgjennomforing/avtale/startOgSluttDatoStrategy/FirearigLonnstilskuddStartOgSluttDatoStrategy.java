4c05c872bc8c1296ab434e88844dc8fd884382e2
MagnusRom <65652609+MagnusRom@users.noreply.github.com>
2026-02-26 09:49:32 +0100

    Lagt til strategyer for at deltaker må være under 30 ved oppstart og … (#1516)
    * Lagt til strategyer for at deltaker må være under 30 ved oppstart og at avtalen ikke kan vare lengere en 4 år
    
    * lagt til ny periode sjekk på 4 år

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/startOgSluttDatoStrategy/FirearigLonnstilskuddStartOgSluttDatoStrategy.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/startOgSluttDatoStrategy/FirearigLonnstilskuddStartOgSluttDatoStrategy.java
@@ -1,5 +1,28 @@
 package no.nav.tag.tiltaksgjennomforing.avtale.startOgSluttDatoStrategy;
 
+import no.nav.tag.tiltaksgjennomforing.avtale.Fnr;
+import no.nav.tag.tiltaksgjennomforing.exceptions.Feilkode;
+import no.nav.tag.tiltaksgjennomforing.exceptions.FeilkodeException;
+
+import java.time.LocalDate;
+import java.time.Period;
+
 public class FirearigLonnstilskuddStartOgSluttDatoStrategy implements StartOgSluttDatoStrategy {
 
+    @Override
+    public void sjekkStartOgSluttDato(LocalDate startDato, LocalDate sluttDato, boolean erGodkjentForEtterregistrering, boolean erAvtaleInngått, Fnr deltakerFnr) {
+        StartOgSluttDatoStrategy.super.sjekkStartOgSluttDato(startDato, sluttDato, erGodkjentForEtterregistrering, erAvtaleInngått, deltakerFnr);
+        if (startDato == null){
+            return;
+        }
+        if (deltakerFnr != null && deltakerFnr.erOver30årFraOppstartDato(startDato)) {
+            throw new FeilkodeException(Feilkode.FIREARIG_LONNSTILSKUDD_FOR_GAMMEL_FRA_OPPSTARTDATO);
+        }
+        if (sluttDato == null){
+            return;
+        }
+        if (Period.between(startDato, sluttDato).getYears() >= 4) {
+            throw new FeilkodeException(Feilkode.FIREARIG_LONNSTILSKUDD_FOR_LANG_VARIGHET);
+        }
     }
+}
