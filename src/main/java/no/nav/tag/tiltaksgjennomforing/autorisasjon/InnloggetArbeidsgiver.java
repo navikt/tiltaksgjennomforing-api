44148327bc5e1f0bdf5f3764845b2887ab7e1218
Sindre Dahl Løken <sindre.loken@nav.no>
2026-03-04 15:44:55 +0100

    mer wip
    # Conflicts:
    #	src/test/java/no/nav/tag/tiltaksgjennomforing/avtale/TestData.java

--- src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/InnloggetArbeidsgiver.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/autorisasjon/InnloggetArbeidsgiver.java
@@ -2,6 +2,7 @@ package no.nav.tag.tiltaksgjennomforing.autorisasjon;
 
 import lombok.Value;
 import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.model.AltinnReportee;
+import no.nav.tag.tiltaksgjennomforing.autorisasjon.altinntilgangsstyring.AltinnTilgangerResponse;
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtalerolle;
 import no.nav.tag.tiltaksgjennomforing.avtale.BedriftNr;
 import no.nav.tag.tiltaksgjennomforing.avtale.Fnr;
@@ -16,6 +17,7 @@ public class InnloggetArbeidsgiver implements InnloggetBruker {
     Fnr identifikator;
     Set<AltinnReportee> altinnOrganisasjoner;
     Map<BedriftNr, Collection<Tiltakstype>> tilganger;
+    AltinnTilgangerResponse altinn3Tilganger;
     Avtalerolle rolle = Avtalerolle.ARBEIDSGIVER;
     boolean erNavAnsatt = false;
 }
