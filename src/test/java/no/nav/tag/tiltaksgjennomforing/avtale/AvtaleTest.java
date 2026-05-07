d42c142e062d7075c9dbe5692252c85c3d2cf4f7
Eirik Vigeland <eirik.vigeland@nav.no>
2026-05-07 14:16:09 +0200

    fix. fikset opp i tester som failet
    Fikset opp i tester som failet etter at det nye feltet
    ble åpnet for flere lønnstilskudd

--- src/test/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/avtale/AvtaleTest.java
@@ -787,7 +787,8 @@ public class AvtaleTest {
                 AvtaleInnhold.Fields.arbeidsgiveravgift,
                 AvtaleInnhold.Fields.tilrettelegging,
                 AvtaleInnhold.Fields.oppfolging,
-                AvtaleInnhold.Fields.harFamilietilknytning
+                AvtaleInnhold.Fields.harFamilietilknytning,
+                AvtaleInnhold.Fields.lonnstilskuddFormaal
         );
 
         Avtale avtale = Avtale.opprett(new OpprettAvtale(TestData.etFodselsnummer(), TestData.etBedriftNr(), Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD), Avtaleopphav.VEILEDER, TestData.enNavIdent());
