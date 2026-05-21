091ea8ec3f1555818f2c2a9df35a3a60c507ea71
d142796 <millad.dagdoni@nav.no>
2026-05-21 12:54:26 +0200

    forenklet en del. fjernet unødvendige exceptions

--- src/main/java/no/nav/tag/tiltaksgjennomforing/postadresse/exception/RegoppslagSecurityException.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/postadresse/exception/RegoppslagSecurityException.java
@@ -1,6 +1,6 @@
 package no.nav.tag.tiltaksgjennomforing.postadresse.exception;
 
-public class RegoppslagSecurityException extends RegoppslagFunctionalException {
+public class RegoppslagSecurityException extends RuntimeException {
 	public RegoppslagSecurityException(String message) {
 		super(message);
 	}
--- src/main/java/no/nav/tag/tiltaksgjennomforing/postadresse/exception/RegoppslagTechnicalException.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/postadresse/exception/RegoppslagTechnicalException.java
@@ -2,10 +2,6 @@ package no.nav.tag.tiltaksgjennomforing.postadresse.exception;
 
 
 public class RegoppslagTechnicalException extends RuntimeException {
-	public RegoppslagTechnicalException(String message) {
-		super(message);
-	}
-
 	public RegoppslagTechnicalException(String message, Throwable cause) {
 		super(message, cause);
 	}
