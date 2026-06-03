875cb2c878efd2d0e581d56f362fcbf1f0c96576
Eirik Vigeland <eirik.vigeland@nav.no>
2026-06-03 08:12:05 +0200

    feat. Oppdater kostnadssted automatisk ved godkjenning av tilskuddsperiode (#1597)
    Beslutter oppgir nå enhet ved godkjenning, og kostnadssted oppdateres
    automatisk på alle tilskuddsperioder i samme operasjon. Validering og
    Norg2-oppslag flyttes fra Avtale til Beslutter, slik at domenelaget
    ikke trenger enhet som parameter.
    
    - Fjerner separat endepunkt og flyt for å endre kostnadssted (AvtaleController,
     Veileder.oppdatereKostnadssted, EndreKostnadsstedRequest)
    - Refaktorerer oppdatereKostnadsstedForTilskuddsperioder til å ta enhet/enhetsnavn
     direkte og oppdatere alle perioder (ikke bare de åpne)
    - Fjerner enhet-parameter fra TilskuddPeriode.godkjenn og Avtale.godkjennTilskuddsperiode
    - Fjerner ubrukt Feilkode.TILSKUDDSPERIODE_ER_IKKE_SATT
    
    ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    -
    
    Kort oppsummert: Det separate 'endre kostnadssted'-endepunktet er fjernet, og kostnadssted settes nå automatisk (fra Norg2) når beslutter godkjenner tilskuddsperioden. Signaturer er forenklet og validering
    er løftet opp til Beslutter-laget.

--- src/main/java/no/nav/tag/tiltaksgjennomforing/dev/GodkjennAvtaleRequest.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/dev/GodkjennAvtaleRequest.java
@@ -3,7 +3,5 @@ package no.nav.tag.tiltaksgjennomforing.dev;
 import no.nav.tag.tiltaksgjennomforing.avtale.NavIdent;
 
 record GodkjennAvtaleRequest(
-        NavIdent beslutterIdent,
-        String kostnadssted
-) {
-}
+        NavIdent beslutterIdent
+) {}
