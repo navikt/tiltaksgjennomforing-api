db4c3749a02b741702cb0fac64e69d4a34f5cc8b
d142796 <millad.dagdoni@nav.no>
2026-06-17 14:46:23 +0200

    fjernet ubrukt import i Veileder.java

--- src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/Veileder.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/avtale/Veileder.java
@@ -39,7 +39,6 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
