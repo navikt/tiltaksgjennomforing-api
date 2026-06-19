6c7de086183d7b8383c62a359a8c437623d8242c
Sindre Dahl Løken <sindre.loken@nav.no>
2026-06-19 15:44:22 +0200

    mer riktig brukt

--- src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/NotifikasjonHendelseLytter.java
+++ src/main/java/no/nav/tag/tiltaksgjennomforing/varsel/notifikasjon/NotifikasjonHendelseLytter.java
@@ -1,11 +1,12 @@
 package no.nav.tag.tiltaksgjennomforing.varsel.notifikasjon;
 
-import io.getunleash.Unleash;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.tag.tiltaksgjennomforing.avtale.Avtale;
 import no.nav.tag.tiltaksgjennomforing.avtale.HendelseType;
 import no.nav.tag.tiltaksgjennomforing.avtale.events.RefusjonKlar;
+import no.nav.tag.tiltaksgjennomforing.featuretoggles.FeatureToggle;
+import no.nav.tag.tiltaksgjennomforing.featuretoggles.FeatureToggleService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -19,21 +20,29 @@ public class NotifikasjonHendelseLytter {
     private final ArbeidsgiverNotifikasjonRepository arbeidsgiverNotifikasjonRepository;
     private final NotifikasjonService notifikasjonService;
     private final NotifikasjonParser parser;
-    private final Unleash unleash;
+    private final FeatureToggleService featureToggleService;
 
     private void opprettOgSendNyBeskjed(Avtale avtale, HendelseType hendelseType, NotifikasjonTekst tekst) {
-        final ArbeidsgiverNotifikasjon notifikasjon = ArbeidsgiverNotifikasjon.nyHendelse(avtale,
-                hendelseType, notifikasjonService, parser);
+        final ArbeidsgiverNotifikasjon notifikasjon = ArbeidsgiverNotifikasjon.nyHendelse(
+            avtale,
+            hendelseType, notifikasjonService, parser
+        );
         arbeidsgiverNotifikasjonRepository.save(notifikasjon);
-        notifikasjonService.opprettNyBeskjed(notifikasjon,
-                NotifikasjonMerkelapp.getMerkelapp(avtale.getTiltakstype().getBeskrivelse()),
-                tekst);
+        notifikasjonService.opprettNyBeskjed(
+            notifikasjon,
+            NotifikasjonMerkelapp.getMerkelapp(avtale.getTiltakstype().getBeskrivelse()),
+            tekst
+        );
     }
 
     @EventListener
     public void avtaleKlarForRefusjon(RefusjonKlar event) {
-        if (unleash.isEnabled("refusjon-klar-i-tiltak-notifikasjon")) return;
-        opprettOgSendNyBeskjed(event.getAvtale(), HendelseType.REFUSJON_KLAR,
-                NotifikasjonTekst.TILTAK_AVTALE_KLAR_REFUSJON);
+        if (featureToggleService.isEnabled(FeatureToggle.REFUSJON_KLAR_I_TILTAK_NOTIFIKASJON)) {
+            return;
+        }
+        opprettOgSendNyBeskjed(
+            event.getAvtale(), HendelseType.REFUSJON_KLAR,
+            NotifikasjonTekst.TILTAK_AVTALE_KLAR_REFUSJON
+        );
     }
 }
