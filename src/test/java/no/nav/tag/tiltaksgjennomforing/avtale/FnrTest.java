ea25c310b81ed7236920e963620ec6cd7f22da5b
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-09-04 20:02:14 +0200

    Bruke helsyntetiske fødselsnumre
    Beveger oss vekk fra test-fødselsnumre som er gyldige,
    fordi dette fører med seg risiko for at et fnr kan bli
    tildelt en person i ettertid selv om de ikke var det i
    opprettelsesøyeblikket.

--- src/test/java/no/nav/tag/tiltaksgjennomforing/avtale/FnrTest.java
+++ src/test/java/no/nav/tag/tiltaksgjennomforing/avtale/FnrTest.java
@@ -73,12 +73,12 @@ public class FnrTest {
 
     @Test
     public void testFnr1() {
-        Now.fixedDate(LocalDate.of(2021, 12, 20));
-        Fnr fnrOver16 = new Fnr("29110976648");
+        Now.fixedDate(LocalDate.of(2013, 12, 20));
+        Fnr fnrOver16 = new Fnr("13459803674");
         assertThat(fnrOver16.erUnder16år()).isTrue();
         assertThat(fnrOver16.erOver30år()).isFalse();
 
-        fnrOver16 = Fnr.fraDb("29110976648");
+        fnrOver16 = Fnr.fraDb("13459803674");
         assertThat(fnrOver16.erUnder16år()).isTrue();
         assertThat(fnrOver16.erOver30år()).isFalse();
 
@@ -88,11 +88,11 @@ public class FnrTest {
     @Test
     public void testFnr2() {
         Now.fixedDate(LocalDate.of(2021, 12, 20));
-        Fnr fnr = new Fnr("19109613897");
+        Fnr fnr = new Fnr("27479635477");
         assertThat(fnr.erUnder16år()).isFalse();
         assertThat(fnr.erOver30år()).isFalse();
 
-        fnr = Fnr.fraDb("19109613897");
+        fnr = Fnr.fraDb("27479635477");
         assertThat(fnr.erUnder16år()).isFalse();
         assertThat(fnr.erOver30år()).isFalse();
 
@@ -101,12 +101,12 @@ public class FnrTest {
 
     @Test
     public void testFnr3() {
-        Now.fixedDate(LocalDate.of(2021, 12, 20));
-        Fnr fnr = new Fnr("25128626630");
+        Now.fixedDate(LocalDate.of(2026, 12, 20));
+        Fnr fnr = new Fnr("27479635477");
         assertThat(fnr.erOver30år()).isTrue();
         assertThat(fnr.erUnder16år()).isFalse();
 
-        fnr = Fnr.fraDb("25128626630");
+        fnr = Fnr.fraDb("27479635477");
         assertThat(fnr.erOver30år()).isTrue();
         assertThat(fnr.erUnder16år()).isFalse();
 
@@ -115,12 +115,12 @@ public class FnrTest {
 
     @Test
     public void testFnr4() {
-        Now.fixedDate(LocalDate.of(2021, 12, 20));
-        Fnr fnr = new Fnr("23029149054");
+        Now.fixedDate(LocalDate.of(1984, 12, 20));
+        Fnr fnr = new Fnr("25506715337");
         assertThat(fnr.erOver30årFørsteJanuar()).isFalse();
         assertThat(fnr.erUnder16år()).isFalse();
 
-        fnr = Fnr.fraDb("23029149054");
+        fnr = Fnr.fraDb("25506715337");
         assertThat(fnr.erOver30årFørsteJanuar()).isFalse();
         assertThat(fnr.erUnder16år()).isFalse();
 
@@ -129,14 +129,14 @@ public class FnrTest {
 
     @Test
     public void testFnr5() {
-        Now.fixedDate(LocalDate.of(2021, 12, 20));
-        LocalDate startDato = LocalDate.of(2022, 1, 5);
+        Now.fixedDate(LocalDate.of(2024, 12, 20));
+        LocalDate startDato = LocalDate.of(2025, 1, 5);
 
-        Fnr fnr = new Fnr("23029149054");
+        Fnr fnr = new Fnr("17509432406");
         assertThat(fnr.erOver30årFørsteJanuar()).isFalse();
         assertThat(fnr.erOver30årFraOppstartDato(startDato)).isTrue();
 
-        fnr = Fnr.fraDb("23029149054");
+        fnr = Fnr.fraDb("17509432406");
         assertThat(fnr.erOver30årFørsteJanuar()).isFalse();
         assertThat(fnr.erOver30årFraOppstartDato(startDato)).isTrue();
 
@@ -146,11 +146,12 @@ public class FnrTest {
     @Test
     public void testDnr1() {
         Now.fixedDate(LocalDate.of(2023, 11, 1));
-        Fnr fnr = new Fnr("49120799125");
+        Fnr fnr = new Fnr("52512536379");
+
         assertThat(fnr.erUnder16år()).isTrue();
         assertThat(fnr.erOver30år()).isFalse();
 
-        fnr = Fnr.fraDb("49120799125");
+        fnr = Fnr.fraDb("52512536379");
         assertThat(fnr.erUnder16år()).isTrue();
         assertThat(fnr.erOver30år()).isFalse();
 
@@ -160,11 +161,12 @@ public class FnrTest {
     @Test
     public void testDnr2() {
         Now.fixedDate(LocalDate.of(2023, 12, 1));
-        Fnr fnr = new Fnr("64090099076");
+        Fnr fnr = new Fnr("58519743742");
+
         assertThat(fnr.erUnder16år()).isFalse();
         assertThat(fnr.erOver30år()).isFalse();
 
-        fnr = Fnr.fraDb("64090099076");
+        fnr = Fnr.fraDb("58519743742");
         assertThat(fnr.erUnder16år()).isFalse();
         assertThat(fnr.erOver30år()).isFalse();
 
@@ -206,7 +208,7 @@ public class FnrTest {
         assertThat(Fnr.fraDb("00000000000").hashCode()).isEqualTo(new Fnr("00000000000").hashCode());
         assertThat(new Fnr("12345678910").hashCode()).isEqualTo(Fnr.fraDb("12345678910").hashCode());
 
-        assertThat(Fnr.fraDb("01015808566")).isEqualTo(new Fnr("01015808566"));
+        assertThat(Fnr.fraDb("29462114573")).isEqualTo(new Fnr("29462114573"));
     }
 
     @Test
@@ -218,7 +220,8 @@ public class FnrTest {
         assertThat(fnr.erOver67ÅrFraSluttDato(Now.localDate().plusDays(1))).isTrue();
         assertThat(fnr.erOver67ÅrFraSluttDato(Now.localDate().minusDays(1))).isFalse();
 
-        fnr = Fnr.fraDb("01015808566");
+        Now.fixedDate(LocalDate.of(1997, 7, 13));
+        fnr = Fnr.fraDb("13473036007");
         assertThat(fnr.erOver67ÅrFraSluttDato(Now.localDate())).isTrue();
         assertThat(fnr.erOver67ÅrFraSluttDato(Now.localDate().plusDays(1))).isTrue();
         assertThat(fnr.erOver67ÅrFraSluttDato(Now.localDate().minusDays(1))).isFalse();
